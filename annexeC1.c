89600fc4a2a47d1eb4ef8a32418d86df9bd624cb
BINDAT <136336341+BINDAT@users.noreply.github.com>
2023-10-04 20:30:38 +0200

    Add files via upload

---
+++ annexeC1.c
@@ -0,0 +1,8 @@
+#include <stdio.h> /*inclure des biblios ou ref*/
+#include <stdlib.h> /*inclure des biblios ou ref*/
+
+int main() /*place une zone de fonction*/
+{
+    printf("Coucou je suis en C !\n");/*a afficher*/
+    return(0); /*retourne la fonction avec 0*/
+}
