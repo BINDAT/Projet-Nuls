406cc10dcc8dea9d7d14d5a70d3f0c71b75aff15
BINDAT <136336341+BINDAT@users.noreply.github.com>
2023-10-15 17:22:45 +0200

    Add files via upload

---
+++ Pour les nul C/annexeC2.c
@@ -0,0 +1,9 @@
+/*Auteur : Max
+Programme affichant un message*/
+#include <stdio.h> //Requis pour utilser puts()
+#include <stdlib.h>
+int main(){
+    printf("hello world!\n");
+    puts("Salut, Humain");
+    return 0;
+}
