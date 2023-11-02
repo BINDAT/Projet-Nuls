d5bed80b2c1d010f5ef18790567bdcde0cf45fd6
BINDAT <maximerigaud66@gmail.com>
2023-11-02 15:57:19 +0100

    Create ex0415.c

---
+++ Pour les nul C/ex0415.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    puts("\"Bonjour,\" dit le corbeau, \"que vous me semblez joli !\""); //sequence d'echappement  pour les guillemets grace a \"
+    return 0;
+}
