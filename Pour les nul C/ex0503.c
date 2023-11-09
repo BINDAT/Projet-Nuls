3c2e68dcb7153cbdfce4659f571b5d94d1aba9ab
BINDAT <maximerigaud66@gmail.com>
2023-11-09 10:10:31 +0100

    Create ex0503.c

---
+++ Pour les nul C/ex0503.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    printf("La valeur %2.1f est un flottant. \n",98.6); //affiche un nombre decimal
+    printf("%d \n",127); //affiche un entier
+    printf("%1.2f \n",3.1415926535); //affiche un nombre decimal avec limite d'affichage
+    printf("%d \n",122013);//affiche un entier
+    printf("%1.1f \n",0.00008); //affiche un nombre decimal avec limite d'affichage
+    return(0);
+}
