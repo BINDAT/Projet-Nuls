47775ec6ec473490b8122806f09f3e52c0082a9e
BINDAT <maximerigaud66@gmail.com>
2023-11-09 10:10:28 +0100

    Create ex0502.c

---
+++ Pour les nul C/ex0502.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    printf("%d \n",127); //affiche un entier
+    printf("%f \n",3.1415926535); //affiche un nombre decimal
+    printf("%d \n",122013);//affiche un entier
+    printf("%f \n",0.00008); //affiche un nombre decimal
+    return(0);
+}
