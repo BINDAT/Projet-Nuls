ef88ba46d622cb95c1b96aaa1cdcd429dd5700bc
BINDAT <maximerigaud66@gmail.com>
2023-11-09 10:10:26 +0100

    Create ex0501.c

---
+++ Pour les nul C/ex0501.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    printf("La valeur %d est un entier. \n", 986); //affiche un entier
+    printf("La valeur %f est un flottant. \n,",98.6); //affiche un nombre decimal
+    return(0);
+}
