93e6b14c281b44048307bef36b3c9daed2c1e901
BINDAT <maximerigaud66@gmail.com>
2023-11-16 00:07:59 +0100

    Create ex0603.c

---
+++ Pour les nul C/ex0603.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    //definition des usages
+    char c;
+    int i;
+    float f;
+    double d;
+    //application des usages
+    c = 'a';
+    i = 1;
+    f = 19.0;
+    d = 20000.009;
+    //affichage test
+    printf("%c\n%d\n%f\n%f\n",c,i,f,d);
+    return(0);
+}
+/*
+Ici dans la definition des usages des var ils est defini les var pour les utilisers et leur attribues de reglage exemple INT, FLOAT.
+A l'application des usage une valeur leur est attribuer en fonctions des attribues de reglage INT, FLOAT.
+Est Par la suite dans la partie affichage test les var pour afficher le result pour chacune
+cette solution est mono ligne pour l'affichage.
+*/
