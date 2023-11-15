73e627f793ed214d5ad53f8da13e1eee503fa7ff
BINDAT <maximerigaud66@gmail.com>
2023-11-16 00:07:53 +0100

    Create ex0601.c

---
+++ Pour les nul C/ex0601.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+    int x; //def de la var x attribue int
+    x = 5; //def de la val de la var x
+    printf("La valeur de la variable x est %d \n", x); //ici le result sera afficher en recherche de la var x
+    return(0);
+}
+/* 
+La déclaration de la var est obligatoire sans la definir il est pas possible de lui definir une valeur ou de l'utiliser.
+Un peu comme la constance.
+Observation : la constance à l'air d'être toujours def en exter en comparaison la var def en inter.
+*/
