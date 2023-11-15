6f7551f36380a455f355d99146da7569116d2304
BINDAT <maximerigaud66@gmail.com>
2023-11-16 00:08:04 +0100

    Create ex0605.c

---
+++ Pour les nul C/ex0605.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int blorf;
+    blorf = 22;
+    printf("La valeur de blorf est %i\n", blorf);
+    printf("La valeur de blorf est +16 est %i\n", blorf+16);
+    printf("La valeur de blorf puissance deux est %i\n", blorf*blorf);
+    return(0); 
+}
