4e1859ac7275cbfcf0e1e46af430c56c6bdce4ec
BINDAT <maximerigaud66@gmail.com>
2023-11-16 19:08:16 +0100

    Create ex0606.c

---
+++ Pour les nul C/ex0606.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define GLORKUS 16
+int main(){
+    int blorf;
+    blorf = 22;
+    printf("La valeur de blorf est %i\n", blorf);
+    printf("La valeur de blorf est +16 est %i\n", blorf+GLORKUS);
+    printf("La valeur de blorf puissance deux est %i\n", blorf*blorf);
+    return(0); 
+}
