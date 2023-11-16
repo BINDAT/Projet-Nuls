7aa7cc93411ae39405388a94b370bfd7bc8529a7
BINDAT <maximerigaud66@gmail.com>
2023-11-16 19:08:20 +0100

    Create ex0608.c

---
+++ Pour les nul C/ex0608.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+int ananias, azarias, misael; //crea multiple de variable
+ananias =701; //attribution individuelle des valeurs des variables
+azarias =709;
+misael =719;
+printf("Ananias est %d\nAzarias est %d\nMisael est %d\n",ananias,azarias,misael);//affichage en liste des valeur des variableS.
+return(0);
+}
