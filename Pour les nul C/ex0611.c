3f886f585ee4a576f9d3c94828f50048baa19f47
BINDAT <maximerigaud66@gmail.com>
2023-11-16 19:08:27 +0100

    Create ex0611.c

---
+++ Pour les nul C/ex0611.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+int npremier; //crea de variable
+npremier = 701; //prise de var sur la val 1
+printf("Ananias vaut %d\n", npremier);
+npremier = 709; //prise de var sur la val 2
+printf("Azarias vaut %d\n", npremier);
+npremier = 719;
+printf("Misael vaut %d\n",npremier);
+return(0);
+}
+/*
+Observation : Les valeurs s'interchange comme des cartouches sans changer la machine est affiches les result demander.
+Var A + Val 1
+Affichage sur machine de la Val 1
+puis apres
+Var A + Val 2
+Affichage sur machine de la Val 2
+Et ceci avec autant de val
+*/
