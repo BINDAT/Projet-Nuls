61949e9126e08f0ba3f58334dcfcb6b296c12992
BINDAT <maximerigaud66@gmail.com>
2023-11-16 19:08:24 +0100

    Create ex0610.c

---
+++ Pour les nul C/ex0610.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+int ananias, azarias, misael; //crea multiple de variable
+ananias, azarias, misael = (701,709,719);
+printf("Ananias est %d\n Azarias est %d\n Misael est %d\n",ananias,azarias,misael);//affichage en liste des valeur des variableS.
+return(0);
+}
