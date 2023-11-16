1e5e8a55e28620eeaf18c87c9b2a949525563c2a
BINDAT <maximerigaud66@gmail.com>
2023-11-16 19:08:29 +0100

    Create ex0612.c

---
+++ Pour les nul C/ex0612.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+int a, b, c; //crea multiple de variable
+a = 5; //attribution individuelle des valeurs des variables
+b = 7;
+c = a+b; //var qui fait appel aux autres var comme valeur et operant
+printf("Variable c=%d\n",c);
+return(0);
+}
