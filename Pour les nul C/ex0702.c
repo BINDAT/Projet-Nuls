e930c17788c41a5cfd41d37535e62d8e943172ec
BINDAT <maximerigaud66@gmail.com>
2023-11-18 21:38:45 +0100

    Update ex0702.c

--- Pour les nul C/ex0702.c
+++ Pour les nul C/ex0702.c
@@ -2,8 +2,11 @@
 int main()
 {
     int c;
-    printf("Je vais recevoir un caractere: ");
+    printf("Je vais recevoir un caractere:");
     c = getchar();
-    printf("J'ai obtenu le caractere %d.",c);
+    printf("J'ai obtenu le caractere '%d'.",c);
     return(0);
 }
+/*
+Affiche la valeur de un caractere en ASCII.
+*/
