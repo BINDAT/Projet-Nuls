6ffc9a13f0beeb5521cdfb2f0c6225beb389366a
BINDAT <maximerigaud66@gmail.com>
2023-11-21 15:52:07 +0100

    Create ex0703.c

---
+++ Pour les nul C/ex0703.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int c;
+    printf("Je vais recevoir un caractere: ");
+    c = getc(stdin);
+    printf("J'ai obtenu le caractere '%c'.",c);
+    return(0);
+}
