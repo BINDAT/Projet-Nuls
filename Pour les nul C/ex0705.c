f2c1696496883bb17f21a9cd0c295c280684cd7d
BINDAT <maximerigaud66@gmail.com>
2023-11-23 18:42:52 +0100

    Create ex0705.c

---
+++ Pour les nul C/ex0705.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+    int ch;
+    printf("Frappez Entree : ");
+    getchar();
+    ch = 'H';
+    putchar(ch);
+    ch = 'i';
+    putchar(ch);
+    putchar('!');
+    return(0);
+}
+/*
+Observation : ce programme affiche Hi! grace au valeurs stocker dans ch et putchar qui permet de modifier ch grace a leurs parametres.
+*/
