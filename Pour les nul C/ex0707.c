a4daeae68c1f460c69f59380e8e364bdc841f357
BINDAT <maximerigaud66@gmail.com>
2023-11-23 18:42:56 +0100

    Create ex0707.c

---
+++ Pour les nul C/ex0707.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char a,b,c,d;
+a = 'W';
+b = a +24;
+c =  b +8;
+d ='\n';
+printf("%c%c%c%c",a,b,c,d);
+return (0);
+}
+/*
+Observation : affiche le message Wow grace a la premiere lettre puis des modification avec les vars
+*/ 
