7f29af5f45df1b07167858a49af27697884aa36b
BINDAT <maximerigaud66@gmail.com>
2023-11-26 10:55:04 +0100

    Create ex0709.c

---
+++ Pour les nul C/ex0709.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    char a,b,c,d;
+printf("Frappez Entree :");
+getchar();
+a = 'W';
+putchar(a);
+b = a+24;
+putchar(b);
+c = a+32;
+putchar(c);
+d ='\n';
+putchar(d);
+return (0);
+}
+/*
+Affiche un message grace au putchar et les var qui modifie les characteres
+*/
