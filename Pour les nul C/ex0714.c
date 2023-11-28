f0fb51305f73e3381e34e9f1ab1f529b962e1b26
BINDAT <maximerigaud66@gmail.com>
2023-11-28 13:47:49 +0100

    Create ex0714.c

---
+++ Pour les nul C/ex0714.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int fav;
+    printf("Saissisez votre chiffre favori : ");
+    scanf("%d",&fav);
+    printf("%d est mon nombre favori aussi ! \n",fav);
+    return(0);
+}
+/*
+Observation : ici nous determinont une var int fav qui sert a stocker se qui ecrit dans le scanf 
+et ressorti ensuite dans le printf %d servent a determiner un entier.
+*/
