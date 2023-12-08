8ea7e9fde5c7ff5fd7a7dfbfbdae14ca8ffe88d3
BINDAT <maximerigaud66@gmail.com>
2023-12-08 11:40:40 +0100

    Create ex0808.c

---
+++ Pour les nul C/ex0808.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a;
+    a = 5;
+    if(a==-3)
+    {printf("%d egale a %d\n",a, -3);}
+    return(0);
+}
+/*
+Observation : a n'est pas egale a -3  cela n'affiche rien
+*/
