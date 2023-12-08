b94e06443433ad248dad02e105535132baee0efb
BINDAT <maximerigaud66@gmail.com>
2023-12-08 11:40:34 +0100

    Create ex0807.c

---
+++ Pour les nul C/ex0807.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a;
+    a = 5;
+    if(a=-3)
+    {printf("%d egale a %d\n",a, -3);}
+    return(0);
+}
+/*
+Observation : ici la variable est mal affecter se qui n'affiche pas le bon resultat
+*/
