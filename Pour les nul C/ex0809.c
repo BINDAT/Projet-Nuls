f1d5ee581fb66940f61d5ee8b96367040515d0ec
BINDAT <maximerigaud66@gmail.com>
2023-12-08 11:40:45 +0100

    Create ex0809.c

---
+++ Pour les nul C/ex0809.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    a = 5;
+    b = -3;
+    if(a==b);
+    {printf("%d egale a %d\n",a,b);}
+    return(0);
+}
+/*
+Observation : a n'est pas egale a b  cela n'affiche rien sauf ci ; bloque l'instruction if en instruction vide ceci ; egale une instruction vide
+*/
