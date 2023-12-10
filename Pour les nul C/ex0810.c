2f3a568d9ceb674c5bc9bd5a6581dade0d210e05
BINDAT <maximerigaud66@gmail.com>
2023-12-10 14:20:25 +0100

    Create ex0810.c

---
+++ Pour les nul C/ex0810.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    a = 6;
+    b = a-2;
+    if(a>b){
+        printf("%d est plus grand que %d\n",a,b);
+        }
+    else{
+        printf("%d n'est pas plus grand que %d\n",a,b);
+    };
+    return(0);
+}
+/*
+Observation : Ici nous retrouvont une condition if qui afficheras si a et plus grand que b ou plus petit avec else mais pas de condition if.
+supplemmentaire
+*/
