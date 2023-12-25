ebaffecd77138a7c554819b8ba5b2ad2a58387b9
BINDAT <maximerigaud66@gmail.com>
2023-12-25 15:03:38 +0100

    Create ex0817.c

---
+++ Pour les nul C/ex0817.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int code;
+    printf("Indiquez le code d'erreur (1-3)");
+    scanf("%d",&code);
+    switch(code)
+    {
+        case 1:
+        puts("Erreur disque, vous n'y pouvez rien.");
+        break;
+        case 2:
+        break;
+        puts("Format invalide, applez votre avocat.");
+        case 3:
+        break;
+        puts("Nom de fichier incorrect, spank it.");
+        default:
+        puts("Haha, ni 1, ni 2, ni 3 ?");
+    }
+    return(0);
+}
+/*
+Observation : ici le scanf prend l'entree du clavier pour la var code qui sert au swicht avec les differentes case que l'ont peut voirs
+comme des conditions et break qui et un point d'arret sur la case.
+*/
