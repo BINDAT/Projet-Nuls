1e62bbae055870f2bf319ec48b7b7c677a5e35e5
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-01-16 15:26:30 +0100

    Create ex0903.c

---
+++ Pour les nul C/ex0903.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int cmptr;
+
+    for(cmptr = - 5; cmptr < 6; cmptr = cmptr+1)
+    {
+        printf("%d\n", cmptr);
+    }
+}
+/*
+Obersavation : compte  a partir de -5 et ajoute +1 jusqu'a atteindre 5 printf ajoute la possibiliter de voir le resultat de test
+*/
