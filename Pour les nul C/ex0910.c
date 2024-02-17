c55b7eb9e5136504766210cdb151aa9cfb73eb36
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-02-17 12:13:48 +0100

    Update ex0910.c

--- Pour les nul C/ex0910.c
+++ Pour les nul C/ex0910.c
@@ -10,3 +10,18 @@ int main() {
    putchar('\n');
    return (0);
 }
+
+/* Correction */
+
+#include <stdio.h>
+
+int main() {
+   char alphabet;
+
+   for(alphabet = 'z'; alphabet >= 'a'; alphabet--)
+   {
+      printf("%c", alphabet);
+   }
+   putchar('\n');
+   return 0;
+}
