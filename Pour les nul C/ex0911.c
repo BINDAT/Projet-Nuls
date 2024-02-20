e6ffbe51cbf08edc0c338a62d18676939d997d0d
BINDAT <maximerigaud66@gmail.com>
2024-02-20 16:51:21 +0100

    Create ex0911.c

---
+++ Pour les nul C/ex0911.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int alpha, code;
+
+    for (alpha ='A'; alpha <='G'; alpha+alpha+1)
+    {
+        for(code=1; code<=7; code=code+1)
+        {
+            printf("%c%d\t",alpha,code);
+        }
+        putchar('\n');
+    }
+    return(0);
+}
+/*
+Obersavion : creer des boucles infini avec de A à G et de chiffre de 1 à 7
+*/
