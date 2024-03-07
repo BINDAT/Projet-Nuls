ad4dcfc941da9c3ecb51448f8cd404eaf24301a0
BINDAT <maximerigaud66@gmail.com>
2024-03-07 16:40:09 +0100

    Create ex0914.c

---
+++ Pour les nul C/ex0914.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+
+    x = 13;
+    while(x < 10)
+    {
+        puts("Le saviez vous ?");
+        x = x + 1;
+    }
+    return(0);
+}
+/*
+Observation : erreur du a une mauvaise instruction de la valeur x et la boucle inferieure
+*/
