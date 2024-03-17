927b89809a29cab696574873a7ddedb17474ff9a
BINDAT <maximerigaud66@gmail.com>
2024-03-17 16:57:30 +0100

    Create ex0916.c

---
+++ Pour les nul C/ex0916.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main ()
+{
+    int fibo, nacci;
+    //bloc d'iniatialiason des var et leurs val
+    fibo = 0;
+    nacci = 1;
+    
+    do //bloc de calcul avec while <300
+    {
+        printf("%d ", fibo);
+        fibo = fibo+nacci;
+        printf("%d ", nacci);
+        nacci = nacci+fibo;
+    } while (nacci < 300 );
+    putchar('\n');
+    return(0);
+}
+
+/*
+Observation : fait apparaitre une suite de nombre grace à deux var et leurs val en sur une ligne d'affichage
+*/
