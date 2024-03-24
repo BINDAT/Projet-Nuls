2b738405dc8d95991912ad5d64f18915c090f7bd
BINDAT <maximerigaud66@gmail.com>
2024-03-24 15:26:54 +0100

    Create ex0918.c

---
+++ Pour les nul C/ex0918.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+
+    for (x=0; x=10; x=x+1)
+    {
+        puts("Vous chercher quelque chose ?");
+    }
+    return(0);
+}
+/*
+Observation : Ici ont a une boucle infini à cause du x=10 dans le for pour eviter cela il faut ajouter ou remplacer par <
+La boucle est infini car toujours vrais
+*/
