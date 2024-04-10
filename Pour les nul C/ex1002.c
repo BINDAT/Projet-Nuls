375fa8056ffdd459a0f8cf1de87efbe17f70ad62
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-04-10 18:25:50 +0200

    Create ex1002.c

---
+++ Pour les nul C/ex1002.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/* Fonction du prototype */
+void prompt();
+void busy();
+int main()
+{
+busy(); /*Boucle de fonction busy()*/
+{
+    int loop;
+    char input[32];
+
+    loop =0;
+    while (loop < 5)
+    {
+        prompt();
+        fgets(input, 31, stdin);
+        loop = loop +1;
+    }
+    return(0);   
+}
+}
+/*Fonction de definition pour busy()*/
+void busy()
+{
+    printf("Appel de la fonction busy !\n");
+}
+/* Fonction de definition pour prompt() */
+void prompt() 
+{
+    printf("C:\\DOS>");
+}
+/*
+Obersavtion : Ce programme permet de saisir 5 fois de suites des commandes. (A etudier)
+Contient une boucle while avec une var loop implementer dans ses parametre et une restriction sur 5.
+La var loop a pour valeur 0 et dans le while elle augmente de 1 jusqu'à temps quelle atteind le nombre de la restrction de while.
+La var input permet une saisie maximale de 32 charactere actuellement.
+Est le fgets de la boucle permet une interaction (A etudier) avec input sur 31 charactere en prenant une saisie avec stdin.
+Les void reste a etudier.
+Cela en restant a l'interieur de la fonction busy qui est appeller par la suite par sa fonction de definition qui vas chercher la boucle busy directement 
+*/
