7ad829d41b90e262c0a64b5b5529a4d6a7f2ab63
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-05-04 22:03:24 +0200

    Update ex1009.c
    Repérage commenter du code

--- Pour les nul C/ex1009.c
+++ Pour les nul C/ex1009.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-float convertir(float f);
+float convertir(float f); //var float f definition globale
 
-int main()
+int main() //bloc d'instuction primaire
 {
- float temp_f, temp_c;
+ float temp_f, temp_c; //var interne au bloc primaire
 
  printf("Temperature en Fahrenheit: ");
  scanf("%f", temp_f);
  temp_c = convertir(temp_f);
  printf("%.1fF vaut %.1fC\n", temp_f, temp_c);
- return(0);
+ return(0); //retourne tout
 }
 
-float convertir(float f)
+float convertir(float f) //Detail la val de la var float f globale bloc secondaire
 {
- float t;
+ float t; //la var t var interne au bloc secondaire
  
- t=(f - 32) /1.8;
- return(t);
+ t=(f - 32) /1.8; //la valeur et l'operation de la var t
+ return(t); //retourne a la var t
 }
