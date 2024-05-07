5ec04c7147b8135a25da9d3f369dbeef1b4d12db
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-05-07 17:28:40 +0200

    Update Pour les nul C

---
+++ Pour les nul C/ex1011.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+float convertir(float f); //fonction convertir var float f definition globale
+
+int main() //bloc d'instuction primaire
+{
+ float temp_f, temp_c; //var interne au bloc primaire
+
+ printf("Temperature en Fahrenheit: ");
+ scanf("%f", &temp_f); //note à moi même ne pas oublier de mettre la mémoire & au temp_f pour rappel scanf prend l'entrer du clavier.
+ printf("%.1fF vaut %.1fC\n",temp_c, convertir(temp_f));//point de conversion et affichage
+ return(0); //retourne tout
+}
+
+float convertir(float f) //Instruction de convertir et Detail la val de la var float f globale bloc secondaire
+{
+ return(f-32/1.8); //retourne a la val.
+}
+
+/*
+Observation : ce code convertie une temperature en Fahrenheit en celsius. la fonction convertir reprend les float et avec et fait les opérations
+pour enfaire une conversation. Une fois cela fait elle renvoit la valeur dans le main ou dans la fonction convertie en temp_c (à étudier).
+*/
