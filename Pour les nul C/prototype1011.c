981266234370e65ea3973a1d853b4caa6d5abb91
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-05-08 21:38:22 +0200

    Create prototype1011.c

---
+++ Pour les nul C/prototype1011.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+  
+  float temp_f, temp_c;
+  temp_c = temp_f 32;
+  temp_f = 32/1.8;
+
+int main() //bloc d'instuction primaire
+{
+
+  printf("Temperature en Fahrenheit: ");
+  scanf("%f", &temp_f); //note à moi même ne pas oublier de mettre la mémoire & au temp_f pour rappel scanf prend l'entrer du clavier.
+  printf("%.1fF vaut %.1fC\n",temp_c, convertir(temp_f));//point de conversion et affichage
+  return(0); //retourne tout
+
+}
+/*Dois faire la meme chose que le code en ex1011.c mais en prennant moins de place et en etant plus direct que ce dernier.*/
