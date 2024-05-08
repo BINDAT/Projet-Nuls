27086524869bcfe7c54cb96a0a4444e98f41926e
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-05-08 22:11:34 +0200

    Update prototype1011.c

--- Pour les nul C/prototype1011.c
+++ Pour les nul C/prototype1011.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
   
-  float temp_f, temp_c;
-  temp_c = temp_f;
-  temp_f = 32/1.8;
-
+  float convertir(float fahrenheit)
+  {
+    return(fahrenheit -32)/1.8;
+  }
 int main() //bloc d'instuction primaire
 {
-
+  float temp_f, temp_c;
+ 
   printf("Temperature en Fahrenheit: ");
   scanf("%f", &temp_f); //note à moi même ne pas oublier de mettre la mémoire & au temp_f pour rappel scanf prend l'entrer du clavier.
-  printf("%.1fF vaut %.1fC\n",temp_c, convertir(temp_f));//point de conversion et affichage
+  temp_c, convertir(temp_f);//point de conversion
+  printf("%.1fF vaut %.1fC\n", temp_f, temp_c); 
   return(0); //retourne tout
 
 }
