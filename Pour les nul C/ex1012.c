8137e61dae9d5a4ff0941f9cf6ae880242ee5b5f
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-05-11 19:00:38 +0200

    Update ex1012.c
    repérage du code

--- Pour les nul C/ex1012.c
+++ Pour les nul C/ex1012.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include <stdio.h> //librairie
 
-void limiter(int stop);
+void limiter(int stop); //fonction void limiter
 
-int main()
+int main()//fonction main
 {
+//instructions du main
   int s;
 
 printf("Indiquez une valeur pour stopper (0-100):");
@@ -11,8 +12,10 @@ scanf("%d", &s);
 limiter(s);
 return(0)
 }
+//fonction de définition de void limiter
 void limiter(int stop)
 {
+  //instructions du void limiter
   int x;
   for(x=0; x<=100; x=x+1)
   {
