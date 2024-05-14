1be3d061cc8a01aff426f488b885d396d6ddffdd
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-05-14 21:11:01 +0200

    Update ex0703.c
    Marquage.

--- Pour les nul C/ex0703.c
+++ Pour les nul C/ex0703.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int c;
-    printf("Je vais recevoir un caractere: ");
-    c = getc(stdin);
-    printf("J'ai obtenu le caractere '%c'.",c);
+    int c; //var c
+    printf("Je vais recevoir un caractere: "); //affiche un message.
+    c = getc(stdin); //prend l'entree du clavier et le stock dans la var c.
+    printf("J'ai obtenu le caractere '%c'.",c); //affiche le message avec le caractere tapper au clavier. 
     return(0);
 }
