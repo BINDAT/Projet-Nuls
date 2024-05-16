2442f1b3ef6f26279214a93fd379501ce7dfd5a5
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-05-16 21:08:26 +0200

    Update ex0606.c

--- Pour les nul C/ex0606.c
+++ Pour les nul C/ex0606.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#define GLORKUS 16
+#include <stdio.h> //librairie
+#define GLORKUS 16 //définition d'une constante.
 int main(){
-    int blorf;
-    blorf = 22;
+    int blorf; //variable
+    blorf = 22; //valeur de la variable
     printf("La valeur de blorf est %i\n", blorf);
     printf("La valeur de blorf est +16 est %i\n", blorf+GLORKUS);
     printf("La valeur de blorf puissance deux est %i\n", blorf*blorf);
