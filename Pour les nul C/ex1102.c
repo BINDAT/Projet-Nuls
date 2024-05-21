302c290de2b4dc37cf16e81f22601536be7e5e98
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-05-21 18:55:07 +0200

    Create ex1102.c

---
+++ Pour les nul C/ex1102.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    x= 0;
+    while (x<=10)
+    {
+        printf("Ne marchez pas dans ma pelouse !\n");
+        x++;     
+    }
+    return(0);
+}
