d995c88809c9525205da1286cf38b783ef4b9e05
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-05-24 18:49:41 +0200

    Update ex1103.c

--- Pour les nul C/ex1103.c
+++ Pour les nul C/ex1103.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-    int compteur; 
+    int compteur; //var compteur.
 
-    for(compteur =-5; compteur <5; compteur++)
+    for(compteur =-5; compteur <5; compteur++) //boucle for pour le comptage.
     {
         printf("%d ", compteur);
     }
-    for(; compteur >=5; compteur--)
+    for(; compteur >=5; compteur--)// boucle for avec instruction ; vide pour la continuiter avec la premiere boicle for. boucle for pour le décomptage.
     {
         printf("%d ",compteur);
     }
