8407ca788d45b309ec45836d3ac3cf6b44788e63
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-05-27 15:46:42 +0200

    Create ex1107.c

---
+++ ex1107.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define VALEUR 5 //ici pas de ; sinon erreur. def de constance.
+
+int main()
+{
+    int a;
+
+    printf("Modulus %d:\n", VALEUR);
+    for(a=0; a<30; a++); //val a dans para de la boucle for.
+        printf("%d %% %d = %d\n",a,VALEUR,a%VALEUR); //prend la val de la constance et celle de a dans la boucle pour faire un modulo.
+    return(0);
+}
