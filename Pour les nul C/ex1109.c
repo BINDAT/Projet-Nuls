8c765fb86168b602e39f042f4479c19fd38c7722
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-05-30 19:37:38 +0200

    Update ex1109.c

--- Pour les nul C/ex1109.c
+++ Pour les nul C/ex1109.c
@@ -4,15 +4,15 @@ int main()
 {
     float alpha;
 
-    alpha = 501;
+    alpha = 501;//etat de la première val de la var
     printf("alpha = %.1f\n",alpha);
-    alpha = alpha+99;
+    alpha = alpha+99; //incrémentation avec auto affectation.
     printf("alpha = %.1f\n",alpha);
-    alpha = alpha-250;
+    alpha = alpha-250; //décrémentation avec auto affectation
     printf("alpha = %.1f\n",alpha);
-    alpha = alpha/82;
+    alpha = alpha/82; //division avec auto affectation.
     printf("alpha = %.1f\n",alpha);
-    alpha = alpha*4.3;
+    alpha = alpha*4.3;//multiplication avec un float en auto affectation.
     printf("alpha = %.1f\n",alpha);
     return(0);
 }
