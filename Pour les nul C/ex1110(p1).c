da163dd82434dae2e05fd5ab3596e5e5ba1f7063
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-06-01 19:36:21 +0200

    Update Pour les nul C

---
+++ Pour les nul C/ex1110(p1).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+
+    while(numero = 5; numero <=100)
+    {
+        printf("%d\t",numero);
+        numero = numero + 5;
+    };
+    return(0);
+}
