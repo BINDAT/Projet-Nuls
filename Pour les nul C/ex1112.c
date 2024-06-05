a4a738a7411e78bc0c4ac31fe03c2897fc2a8bbe
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-06-05 19:32:41 +0200

    Update ex1112.c

--- Pour les nul C/ex1112.c
+++ Pour les nul C/ex1112.c
@@ -7,6 +7,6 @@ int main()
  printf("Indiquez un angle en degres : ");
  scanf("%f", &degres);
  radians = 0.0174532925*degres;
- printf*%.2f degres valent %.2f radians.\n", degres,radians);
+ printf("%.2f degres valent %.2f radians.\n", degres,radians);
  return(0);
 }
