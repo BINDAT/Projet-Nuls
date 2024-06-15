52053fa8894e44a2fcc19ea7a4581c02cd256dbe
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-06-15 20:37:49 +0200

    Update Pour les nul C

---
+++ Pour les nul C/ex1116.c
@@ -0,0 +1,23 @@
+#include <math.h>
+
+#define PI 1
+#define LONGUEURONDE 70
+#define PERIODE .1
+
+int main()
+{
+    float graph,s,x;
+
+    for(graph=0; graph<PI; graph+=PERIODE) //boucle qui reprend en paramètre PI et periode dans la var int a main graph
+    {
+        s=sin(graph); //graph donne une val a la var s
+        for(x=0; x< s*LONGUEURONDE; x++) //ici la var LONGUEURONDE et en paramètre avec x et s
+            putchar('*');
+        putchar('\n');
+    }
+return(0);
+}
+
+/*
+Observation : Fait une forme de dessin (Courbe) à partir des valeurs dans les variables et grâce au putchar ('*')
+*/
