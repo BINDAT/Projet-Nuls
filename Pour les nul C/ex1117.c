ea6b549771326a317463bb8b53eb63c641f30795
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-06-19 10:57:09 +0200

    Update ex1117.c

--- Pour les nul C/ex1117.c
+++ Pour les nul C/ex1117.c
@@ -6,7 +6,7 @@ int main()
 
 int r,a,b; // r sert pour le nombre aléatoire. a pour la boucle interne et b pour la restrictions.
 puts("100 chiffres au hasard");
-for (a=0; b<20; b++)
+for (a=0; b<20; b++)//La boucle gère les itérations avec la var b
 {
     {
         r=rand();//var qui prend des nombre aléatoire avec rand
