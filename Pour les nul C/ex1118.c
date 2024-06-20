bc9af4bd351512ca2f66eecd474026a5f3cc9dcf
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-06-20 18:40:01 +0200

    Create ex1118.c

---
+++ Pour les nul C/ex1118.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+int r,a,b; // r sert pour le nombre aléatoire. a pour la boucle interne et b pour la restrictions.
+
+for (a=0; b<20; b++)//La boucle gère les itérations avec la var b
+{
+    {
+        r=rand() %21;//var qui prend des nombre aléatoire avec rand le %21 détermine la limite à 20.
+        printf("%d\t",r);
+    }
+    putchar('\n');
+}
+return(0);
+}
+/*
+Observation : ce code affiche des nombres entre 0 et 20.
+Mais il dois être retravailler pour enlever b
+*/
