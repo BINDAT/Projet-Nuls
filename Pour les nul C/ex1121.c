9e7b7ddb8af2acc0fb3a3bd19abc7788ac869e0b
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-06-29 20:27:19 +0200

    Update ex1121.c

--- Pour les nul C/ex1121.c
+++ Pour les nul C/ex1121.c
@@ -4,17 +4,16 @@
 
 int main()
 {
-    int aleatoire,boucle,saisie;
+    int aleatoire,saisie;
     printf("Devinez la valeur aléatoire entre 0 et 9 : ");
     srand((unsigned)time(NULL));
     scanf("%d",&aleatoire);
-    for(boucle=0; boucle <9; boucle++);
-    {     
+         
             aleatoire=rand() %10;
             printf("%d\t",aleatoire);
         
         putchar('\n');
-    }
+    
     if(saisie == aleatoire){
         printf("Bravo la valeur aléatoire est bien %d", aleatoire);
     }
