69335ea2e0eca2ade171ed944ac0099df1bb4c7e
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-07-01 11:12:17 +0200

    Update Pour les nul C

---
+++ Pour les nul C/ex1121(p1).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int nombre_aleatoire(int min, int max);
+int main()
+{
+    int aleatoire,saisie;
+    printf("Devinez la valeur aléatoire entre 0 et 9 : ");
+    srand((unsigned)time(NULL)); //var var en srand assignée sur une val aléatoire sur avec time(NULL)
+    scanf("%d",&saisie);
+
+            aleatoire=nombre_aleatoire(0,9) //limite de 0 à 9.
+            printf("%d\t",aleatoire);
+
+        putchar('\n');
+
+    if(saisie == aleatoire){
+        printf("Bravo la valeur aléatoire est bien %d", aleatoire);
+    }
+    else{
+        printf("dommage la valeur aléatoire était %d",aleatoire);
+    }
+    return(0);
+}
+/*
+Observation : Ce code génère un chiffre entre 0 et 9 que la personne peut essayer de deviner.
+*/
