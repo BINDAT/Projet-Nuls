3195fe1c54d7e003e1aa147d4080a05fa8786768
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-07-05 22:48:39 +0200

    Update ex1122.c

--- Pour les nul C/ex1122.c
+++ Pour les nul C/ex1122.c
@@ -4,8 +4,8 @@
 
 int main()
 {
-int calculatrice= 20-5*2+42/6; //Var de calcul
-              printf("%d\t",calculatrice);
+int calculatrice= 20-5*2+42/6; //Var de calcul prend toute l'opération et fait même le calcul en une fois.
+              printf("%d\t",calculatrice); //affiche le résultat de la var
   
 return(0);
 }
