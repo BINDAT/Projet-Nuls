e985383ebfebcc0bb877104f1dfaaf3356455309
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-07-07 11:19:19 +0200

    Create ex1122(p1).c

---
+++ Pour les nul C/ex1122(p1).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int calcultrice;
+    int a = 20;
+    int b = -5;
+    int c = b*2;
+    int d = +42;
+    int e = d/6;
+    calcultrice = a,b,c,d,e;
+    printf("%d\n",calcultrice);
+
+}
