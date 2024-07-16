a81634160e163668bf4055787549cb67704997d0
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-07-16 15:31:27 +0200

    Create ex1124.c

---
+++ Pour les nul C/ex1124.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int nombre1, nombre2,nombre3,calculatrice;
+    nombre1 = 12;
+    nombre2 = 5;
+    nombre3 = 2;
+    calculatrice = (nombre1 - nombre2) * nombre3;
+    printf("%d",calculatrice);
+    return(0);
+}
