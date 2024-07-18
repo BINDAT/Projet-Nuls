07ea8c62d89ca39f464875bd8c7a5b33e9794fc9
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-07-18 13:21:59 +0200

    Update ex1124.c

--- Pour les nul C/ex1124.c
+++ Pour les nul C/ex1124.c
@@ -9,3 +9,7 @@ int main()
     calculatrice = (nombre1 - nombre2) * nombre3;//prioritée instaurée grâce au parenthèses pour le calcul.   printf("%d",calculatrice);
     return(0);
 }
+/*
+Observation : Ce code permet de faire un calcul avec les var regrouper dans la var calcultrice pour les opérations.
+Le printf affiche le résultat.
+*/
