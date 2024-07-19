881fc5c4d1581b39bceaa731d32cb8cde7d11a3d
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-07-19 11:45:44 +0200

    Create ex1201.c

---
+++ Pour les nul C/ex1201.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int scoremax1,scoremax2,scoremax3;
+    
+    printf("Votre meilleur score: ");
+    scanf("%d",&scoremax1);
+    printf("Second meilleur score: ");
+    scanf("%d",&scoremax2);
+    printf("Troisième meilleur score: ");
+    scanf("%d",&scoremax3);
+
+    puts("Voici vos meilleurs score");
+    printf("#1 %d\n", scoremax1);
+    printf("#2 %d\n", scoremax2);
+    printf("#3 %d\n", scoremax3);
+
+}
