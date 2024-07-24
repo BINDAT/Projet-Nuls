5c1e42d7b04f1897b73fe02037d456e843c73149
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-07-24 12:51:11 +0200

    Update ex1205.c

--- Pour les nul C/ex1205.c
+++ Pour les nul C/ex1205.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    int scoremax[10];
+    int scoremax[10]; //var avec limites d'élément tableau
     int x;
 
-    for (x=0; x<=9; x++)
+    for (x=0; x<=9; x++) //boucle d'entrée
     {
         printf("Votre score #%d : ",x+1);
         scanf("%d",&scoremax[x]);
     }
     puts("Voici vos meilleurs scores");
-    for (x= 0; x<=9; x++)
+    for (x= 0; x<=9; x++) //boucle de sortie
     {
         printf("#%d %d\n",x+1,scoremax[x]);
     }
