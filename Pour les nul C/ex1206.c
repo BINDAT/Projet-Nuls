0279c8bbebdd5c25426cd4c81328584ded4c4b90
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-07-25 18:03:23 +0200

    Update ex1206.c

--- Pour les nul C/ex1206.c
+++ Pour les nul C/ex1206.c
@@ -3,12 +3,16 @@
 
 int main()
 { 
-    float tabBourseFin [] = {};
+    
     float données [5] = {140450.06,14458.62,14539.14,14514.11,14452.06};
-    tabBourseFin = données;
-    for (int boucle =1; boucle <=5; boucle++)
+    float tabBourseFin [5];
+    for(int copy=0; copy<=4; copy++)
     {
-        printf("Jour %d: %f\n",boucle,tabBourseFin);
+    tabBourseFin[copy] = données[copy];
+    }
+    for (int boucle =0 ; boucle <=4; boucle++)
+    {
+        printf("Jour %d: %f\n",boucle+1,tabBourseFin[boucle]);
     }
     return(0);
 }
