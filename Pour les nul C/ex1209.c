42befaa5387dd149a5ed486620c4e87c1185cabb
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-07-29 14:23:35 +0200

    Create ex1209.c

---
+++ Pour les nul C/ex1209.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+    char prenom[16];
+
+    printf("Quel est votre prenom ? ");
+    fgets(prenom, 16, stdin);
+    printf("Ravi de vous rencontrer %s\n",prenom);
+    return(0);
+}
