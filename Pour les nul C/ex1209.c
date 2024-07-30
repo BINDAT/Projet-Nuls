79444adb6d09376bdf1634ec7338e8f6d21babec
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-07-30 16:51:26 +0200

    Update ex1209.c

--- Pour les nul C/ex1209.c
+++ Pour les nul C/ex1209.c
@@ -9,3 +9,6 @@ int main ()
     printf("Ravi de vous rencontrer %s\n",prenom);
     return(0);
 }
+/*
+Observation : Ici le code prend les entrer du clavier jusqu'à 16 caractères puis les mets dans la var prenom pour l'affichage.
+*/
