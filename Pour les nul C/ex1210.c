69897e75b04d49cb2dd04be068ff3c7dbf26d133
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-08-02 18:30:28 +0200

    Update ex1210.c

--- Pour les nul C/ex1210.c
+++ Pour les nul C/ex1210.c
@@ -11,3 +11,6 @@ int main ()
     printf("Bienvenu %s %s",nom,prenom);
     return(0);
 }
+/*
+Observation : Ce code affiche un message de bienvenu en prenant le nom et prenom de la personne grâce aux fgets et au var char prenom et nom.
+*/
