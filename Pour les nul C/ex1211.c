95f2eaf99461c11e91efe9ba5dfbb1fafb910d96
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-08-04 15:15:51 +0200

    Update ex1211.c

--- Pour les nul C/ex1211.c
+++ Pour les nul C/ex1211.c
@@ -4,9 +4,9 @@ int main ()
 {
     char prenom[16],nom[16];
     printf("Quel est votre prenom ? ");
-    scanf("%15s",prenom);
+    scanf("%s",prenom);
     printf("Quel est votre nom ? ");
-    scanf("%15s",nom);
+    scanf("%s",nom);
    
     printf("Bienvenu %s,%s",nom,prenom);
     return(0);
