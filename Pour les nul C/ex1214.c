1adc86ded06d3ef411d3841ecbf2c8b8497eb1b9
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-08-09 22:58:43 +0200

    Update ex1214.c

--- Pour les nul C/ex1214.c
+++ Pour les nul C/ex1214.c
@@ -22,3 +22,7 @@ int main()
         printf("%d\t", tabulles[x]);}
     //putchar('æ\nÆ');
 }
+/*
+Observation : ce code contient un tableau de 40 valeur qui est d'abord afficher en désordre puis ensuite afficher en ordre.
+la constante en #define TAILLE sert à déterminer la taille la sur 40 élément pour le tableau mais aussi les boucles avec la var x.
+*/
