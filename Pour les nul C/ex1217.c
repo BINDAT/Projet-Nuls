acd1ecc1b618099135b5d091959528bc9db4dff1
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-08-15 21:20:55 +0200

    Create ex1217.c

---
+++ Pour les nul C/ex1217.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ()
+{
+ char tictatoe[3][3];
+ int x,y;
+ /* Initialise la matrice */
+ for(x=0; x<3; x++)
+    for(y=0; y<3; y++)
+        tictatoe[x][y]= '.';
+    tictatoe[1][1] = 'X';
+
+/* Affiche le plateau */
+    puts("Une partie de  Tic-Tac-Toe ?");
+    for(x=0; x<3; x++);
+    {
+        for(y=0; y<3; y++)
+            printf("%c\t", tictatoe[x][y]);
+        putchar('\n');
+    }
+    return(0);
+}
