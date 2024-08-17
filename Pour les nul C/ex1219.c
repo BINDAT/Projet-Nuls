99bd66922bd8711a68fd866cba8ecdf2300245c7
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-08-17 16:01:54 +0200

    Create ex1219.c

---
+++ Pour les nul C/ex1219.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define TAILLE 3
+
+int main()
+{
+char president [TAILLE][8] = {
+"Sarkozy",
+"Coty",
+"Grevy"
+    };
+    int x,index;
+    
+    index = 0;
+    puts(president[x]);
+    return(0);  
+}
