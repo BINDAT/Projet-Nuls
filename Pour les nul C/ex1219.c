4681c0f86f55e37b59c34050aadbb2bed13cdaa9
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-08-20 18:36:05 +0200

    Update ex1219.c

--- Pour les nul C/ex1219.c
+++ Pour les nul C/ex1219.c
@@ -15,3 +15,7 @@ char president [TAILLE][8] = {
     puts(president[x]);
     return(0);  
 }
+
+/*
+Observation : Ce code contient une const TAILLE qui détermine la taille du tableau. le tableau president contient le nombre que la const TAILLE lui permet et sur un maximum de de caractères. la var x elle en fonction du nombre écrit chercheras l'élément demandé.
+*/
