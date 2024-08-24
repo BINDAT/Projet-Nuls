7756e2feef262c76a40754b5675e74724f98be76
BINDAT <maximerigaud66@gmail.com>
2024-08-24 15:02:49 +0200

    Create ex1110(p1).c

---
+++ Pour les nul C/annexe code c/ex1110(p1).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+
+    while(numero = 5; numero <=100)
+    {
+        printf("%d\t",numero);
+        numero = numero + 5;
+    };
+    return(0);
+}
+/*
+Observation : Compte de 5 en 5 jusqu'à 100 grâce au condition de la boucle for en paramètres.
+*/
