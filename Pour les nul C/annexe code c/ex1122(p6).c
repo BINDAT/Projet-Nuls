4d0ea9c4b8a11516b3325f02c45239470f1d38fe
BINDAT <maximerigaud66@gmail.com>
2024-08-24 15:02:27 +0200

    Create ex1122(p6).c

---
+++ Pour les nul C/annexe code c/ex1122(p6).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int calcultrice;
+    
+    calcultrice = 20;
+    calcultrice = calcultrice -5;
+    calcultrice = calcultrice *2;
+    calcultrice = calcultrice +42;
+    calcultrice = calcultrice /6;
+    printf("%d",calcultrice);
+    return(0);
+}    
+/*
+Solution : Cette solution propose une variable avec réaffectation à chaque opération sur le changement de valeur
+*/
