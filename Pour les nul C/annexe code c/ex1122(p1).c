a94677a044663912134320f3b76e26160e5fa37e
BINDAT <maximerigaud66@gmail.com>
2024-08-24 15:02:41 +0200

    Create ex1122(p1).c

---
+++ Pour les nul C/annexe code c/ex1122(p1).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int calcultrice;
+    int a = 20;
+    int b = -5;
+    int c = b*2;
+    int d = +42;
+    int e = d/6;
+    calcultrice = a,b,c,d,e;
+    printf("%d\n",calcultrice);
+    return(0);
+}
+/*
+Solution : Cette solution reprend plusieurs var avec leurs val dans une var calcultrice pour le calcul. Ici le préfixe int est présent.
+*/
