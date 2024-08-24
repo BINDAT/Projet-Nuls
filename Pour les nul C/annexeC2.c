4380393c0fb6d90ef2af29d83c6489105c4d591b
BINDAT <maximerigaud66@gmail.com>
2024-08-24 15:01:12 +0200

    Delete annexeC2.c

--- Pour les nul C/annexeC2.c
+++
@@ -1,9 +0,0 @@
-/*Auteur : Max
-Programme affichant un message*/
-#include <stdio.h> //Requis pour utilser puts()
-#include <stdlib.h>
-int main(){
-    printf("hello world!\n");
-    puts("Salut, Humain");
-    return 0;
-}
