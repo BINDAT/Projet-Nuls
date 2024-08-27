7430a4edf4a44ee16ea1739533a6765ad1ac201e
BINDAT <maximerigaud66@gmail.com>
2024-08-27 21:46:55 +0200

    Update ex1306.c

--- Pour les nul C/ex1306.c
+++ Pour les nul C/ex1306.c
@@ -4,15 +4,15 @@
 
 int main()
 {
-char phrase [100], majuscule[100], minscule[100];
-int curseur;
-printf("Entrez du texte");
-fgets (phrase, sizeof(phrase),stdin);
-for(curseur = 0; phrase[curseur] != '\O'; curseur++)
-{
-majuscule[curseur] = toupper(phrase[curseur]);
-minuscule[curseur] = tolower(phrase[curseur]);
-}
+    char phrase [100], majuscule[100], minuscule[100];
+    int curseur;
+        printf("Entrez du texte");
+        fgets (phrase, sizeof(phrase),stdin);
+    for(curseur = 0; phrase[curseur] != '\0'; curseur++)
+    {
+    majuscule[curseur] = toupper(phrase[curseur]);
+    minuscule[curseur] = tolower(phrase[curseur]);
+    }
 printf("En majuscule : %s",majuscule);
 printf("En minuscule : %s",minuscule);
 
