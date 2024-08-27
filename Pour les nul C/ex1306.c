6003cce25ff09943f7e27bfba9a709148f37ba2e
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-08-27 21:25:15 +0200

    Update ex1306.c

--- Pour les nul C/ex1306.c
+++ Pour les nul C/ex1306.c
@@ -7,7 +7,7 @@ int main()
 char phrase [100], majuscule[100], minscule[100];
 int curseur;
 printf("Entrez du texte");
-fgets (phrase; sizeof(phrase),stdin);
+fgets (phrase, sizeof(phrase),stdin);
 for(curseur = 0; phrase[curseur] != '\O'; curseur++)
 {
 majuscule[curseur] = toupper(phrase[curseur]);
