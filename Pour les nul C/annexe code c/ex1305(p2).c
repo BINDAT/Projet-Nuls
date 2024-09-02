d457bf509495c7103cadf434318ea0359140a469
BINDAT <maximerigaud66@gmail.com>
2024-09-02 21:45:59 +0200

    Create ex1305(p2).c

---
+++ Pour les nul C/annexe code c/ex1305(p2).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char reponse;
+
+    printf("Voulez-vous faire exploser la lune ? ");
+    scanf("%c", &reponse);
+
+    switch (reponse)
+    {
+    case 'O': 
+    case 'o':
+        puts("BOUM !");
+        break;
+    
+    case 'N': 
+    case 'n':
+        puts("La lune ne craint rien.");
+        break;
+    default:
+        puts("Cela n'est pas une réponse attendue.");
+        break;
+    }
+    
+}
+/*
+Observation : Ce code affiche les des messages à l'entrée de O ou N ou o et n mais affichera un message en else ppur l'erreur de saisie.
+*/
