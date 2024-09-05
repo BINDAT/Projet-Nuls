5e857d11eebf8b61af7096294689b5829545aea6
BINDAT <maximerigaud66@gmail.com>
2024-09-05 16:15:37 +0200

    Create ex1308.c

---
+++ Pour les nul C/ex1308.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char motpasse[] = "taco"; //notre mot de passe
+    char tabsaisie[15]; //notre entrée clavier du mot de passe
+    
+    printf("Le mot de passe ? ");
+    scanf("%s",tabsaisie); //Prend la saisie et met en mémoire dans la var tabsaisie.
+    
+    if (strcmp(tabsaisie,motpasse)==0)//compare les deux var des val de tabsaisie est motdepasse avec la var match et la fonction strcmp qui lui renvoie 0 quand les deux val des var correspondes.
+    
+        puts("Mot de passe correct");
+    else
+        puts("Mauvais mot de passe. Alertez la DGSE");
+
+    return(0);    
+}
