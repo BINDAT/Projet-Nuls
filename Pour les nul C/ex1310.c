f9358668ad9072918bf762acf294deebaebabab0
BINDAT <maximerigaud66@gmail.com>
2024-09-06 22:00:37 +0200

    Create ex1310.c

---
+++ Pour les nul C/ex1310.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char primo[40];
+    char secundo[20];
+    
+    printf("Indiquez votre prenom : ");
+    scanf("%s", primo); //prend la saisie et l'ajoute en valeur de primo.
+    printf("Et votre nom de famille : ");
+    scanf("%s", secundo); //prend la saisie et l'ajoute en valeur de secundo.
+    strcat(primo, secundo); //strcat prend deux valeurs pour en faire une seule.
+    printf("Ravi de vous voir, %s!\n", primo);
+ return(0);   
+}
