f51041e6cb887a6c246a321a75a52851768d63d3
BINDAT <maximerigaud66@gmail.com>
2024-09-08 17:07:48 +0200

    Create ex1312.c

---
+++ Pour les nul C/ex1312.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    float valfrac1 = 34.5;
+    float valfrac2 = 12.3456789;
+
+    printf("%%9.1f = %9.1f\n",valfrac1);
+    printf("%%8.1f = %8.1f\n",valfrac1);
+    printf("%%7.1f = %7.1f\n",valfrac1);
+    printf("%%6.1f = %6.1f\n",valfrac1);
+    printf("%%5.1f = %5.1f\n",valfrac1);
+    printf("%%4.1f = %4.1f\n",valfrac1);
+    printf("%%3.1f = %3.1f\n",valfrac1);
+    printf("%%2.1f = %2.1f\n",valfrac1);
+    printf("%%1.1f = %1.1f\n",valfrac1);
+    printf("%%9.1f = %9.1f\n",valfrac2);
+    printf("%%8.1f = %8.1f\n",valfrac2);
+    printf("%%7.1f = %7.1f\n",valfrac2);
+    printf("%%6.1f = %6.1f\n",valfrac2);
+    printf("%%5.1f = %5.1f\n",valfrac2);
+    printf("%%4.1f = %4.1f\n",valfrac2);
+    printf("%%3.1f = %3.1f\n",valfrac2);
+    printf("%%2.1f = %2.1f\n",valfrac2);
+    printf("%%1.1f = %1.1f\n",valfrac2);
+
+    return(0);
+}
