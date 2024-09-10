a760e715a05f2e630f934e6d6285a8c3899f82ec
BINDAT <maximerigaud66@gmail.com>
2024-09-10 22:20:08 +0200

    Update ex1314.c

--- Pour les nul C/ex1314.c
+++ Pour les nul C/ex1314.c
@@ -1,41 +1,24 @@
 #include <stdio.h>
 #include <ctype.h>
-char bloc_de_texte;
-
-int index, blanc, lettre,ponctuation;
+#include <string.h>
 
 int main()
 {   
-    printf("%%15s = %15s\n", "hello");
-    printf("%%14s = %14s\n", "hello");
-    printf("%%13s = %13s\n", "hello");
-    printf("%%12s = %12s\n", "hello");
-    printf("%%11s = %11s\n", "hello");
-    printf("%%10s = %10s\n", "hello");
-    printf("%%9s = %9s\n", "hello");
-    printf("%%8s = %8s\n", "hello");
-    printf("%%7s = %7s\n", "hello");
-    printf("%%6s = %6s\n", "hello");
-    printf("%%5s = %5s\n", "hello");
-    printf("%%4s = %4s\n", "hello");
-    index = 0;
+int char_count = 0;
+   
+   char_count = printf("%%15s = %15s\n", "hello");
+   char_count =  printf("%%14s = %14s\n", "hello");
+   char_count =  printf("%%13s = %13s\n", "hello");
+   char_count =  printf("%%12s = %12s\n", "hello");
+   char_count =  printf("%%11s = %11s\n", "hello");
+   char_count =  printf("%%10s = %10s\n", "hello");
+   char_count =  printf("%%9s = %9s\n", "hello");
+   char_count =  printf("%%8s = %8s\n", "hello");
+   char_count =  printf("%%7s = %7s\n", "hello");
+   char_count =  printf("%%6s = %6s\n", "hello");
+   char_count =  printf("%%5s = %5s\n", "hello");
+   char_count =  printf("%%4s = %4s\n", "hello");
     
-    while (bloc_de_texte(index))
-    {
-        if(isblank(bloc_de_texte(index)))
-        {
-            blanc++;
-        }
-        if (ispunct(bloc_de_texte(index)))
-        {
-            ponctuation++;
-        }
-        if (isalpha(bloc_de_texte(index)))
-        {
-            lettre++;
-        }
-        index++;
-    }
-    printf("%d Lettres de l'alphabet \n%d blancs \n%d signe de ponctuation", lettre,blanc,ponctuation);
+    printf("%d nombre de caractères capturés",char_count);
     return(0);
 }
