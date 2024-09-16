b0a8023eb3a3bdab2994f45213fedb0558cfc605
BINDAT <maximerigaud66@gmail.com>
2024-09-16 21:56:16 +0200

    Update ex1401.c

--- Pour les nul C/ex1401.c
+++ Pour les nul C/ex1401.c
@@ -2,18 +2,19 @@
 
 int main()
 {
-    struct joueur;
-    {
-        char nomj [32];
+    struct joueur {
+        char nomj[32];
         int scoremax;
     };
+    
     struct joueur xbox;
 
     printf("Nom du joueur : ");
-    scanf("%s", xbox.scoremax);
+    scanf("%s", xbox.nomj);
     printf("Meilleur score : ");
     scanf("%d", &xbox.scoremax);
 
     printf("Meilleur score de %s : %d\n", xbox.nomj, xbox.scoremax);
+    
     return(0);
 }
