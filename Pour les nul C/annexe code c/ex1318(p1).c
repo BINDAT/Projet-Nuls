a28591a70fcb62473031600bd7b78039f45fa69b
BINDAT <maximerigaud66@gmail.com>
2024-09-16 17:33:35 +0200

    Create ex1318(p1).c

---
+++ Pour les nul C/annexe code c/ex1318(p1).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char primo, secundo;
+
+    printf("Initiale de votre prénom : ");
+    primo = getchar();
+    getchar();
+    printf("Initiale du nom de famille : ");
+    secundo = getchar();
+    printf("Vos initiales son '%c' et '%c' \n", primo,secundo);
+    return(0);
+}
