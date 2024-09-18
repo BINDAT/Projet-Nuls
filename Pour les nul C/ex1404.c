dc39d658cb57a17816bc2a89b8bfe3bcfcd7c7a7
BINDAT <maximerigaud66@gmail.com>
2024-09-18 22:54:10 +0200

    Create ex1404.c

---
+++ Pour les nul C/ex1404.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    struct president
+    {
+        char nomPres[40];
+        int anneeNomi;
+    } pres1 = {
+        "George Washington", 1789
+    };
+    
+    printf("Le premier president est %s\n",pres1.nomPres);
+    printf("Il a pris ses fonctions en %d\n",pres1.anneeNomi);
+
+    return(0);
+}
