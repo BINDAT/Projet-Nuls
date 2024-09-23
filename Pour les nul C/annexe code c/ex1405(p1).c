34972461ebc3b46239585a3d588a2a390772fe49
BINDAT <maximerigaud66@gmail.com>
2024-09-23 21:44:27 +0200

    Create ex1405(p1).c

---
+++ Pour les nul C/annexe code c/ex1405(p1).c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    struct president
+    {
+        char nomPres[40];
+        int anneeNomi;
+    };
+    struct president pres1 =
+    {
+        "George Washington", 1789
+    };
+    struct president pres2 = 
+    {
+        "John Adams",1797
+    };
+    
+    printf("Le premier president est %s\n",pres1.nomPres);
+    printf("Il a pris ses fonctions en %d\n",pres1.anneeNomi);
+    printf("Le second président est %s\n",pres2.nomPres);
+    printf("Il a pris ses fonctions en %d\n",pres2.anneeNomi);
+    return(0);
+}
+/*
+Observation : Fait un bloc qui contient des informations qui sont affichées avec le bloc d'instruction d'affichage.
+*/
