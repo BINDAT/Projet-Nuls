47c766e4bb011cbaa8cf4c5df261e1256cb0d53a
BINDAT <maximerigaud66@gmail.com>
2024-09-24 21:49:39 +0200

    Create ex1406.c

---
+++ Pour les nul C/ex1406.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    struct scores
+    {
+        char nomj[32];
+        int score;
+    };
+    struct scores joueurs[4];
+    int x;
+
+    for (x=0; x<4; x++)
+    {
+        printf("Indiquez le joueur %d : ",x+1);
+        scanf("%s", joueurs[x].nomj);
+        printf("Indique son score : ");
+        scanf("%d",&joueurs[x].score);
+    }
+    
+    puts("Infos de joueur");
+    printf("#\tNom\tScore\n");
+    for (x=0;  x<4; x++)
+    {
+        printf("%d\t%s\t%5d\n",x+1, joueurs[x].nomj, joueurs[x].score);
+    }
+    return(0);
+}
