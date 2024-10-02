512cc1efe954532e8a3379cd2295453dc2b4bd25
BINDAT <maximerigaud66@gmail.com>
2024-10-02 22:03:58 +0200

    Create ex1501(p1).c

---
+++ Pour les nul C/annexe code c/ex1501(p1).c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    printf("Bienvenue, %s\n",argv[0]);//Renvoie en affichage le lien du dossier avec le message de bienvenue quand à 0 sinon renvoie null
+    return(0);
+}
