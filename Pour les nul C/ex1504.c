4ee8ff7a01f46f5604927094168db532bacf11b8
BINDAT <maximerigaud66@gmail.com>
2024-10-03 22:10:26 +0200

    Create ex1504.c

---
+++ Pour les nul C/ex1504.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+printf("invite$ %s\n", argv[0]);
+
+
+    for (argc = 0; argc <4; argc++)
+    {
+        printf("Arg#%d %s\n",argc+1);
+    }
+    
+    
+    return(0);
+}
