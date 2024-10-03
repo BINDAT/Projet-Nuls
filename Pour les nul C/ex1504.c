5a498c7fd1eb8989c2980a74dbfe8cb1a695eb59
BINDAT <maximerigaud66@gmail.com>
2024-10-03 22:26:43 +0200

    Update ex1504.c

--- Pour les nul C/ex1504.c
+++ Pour les nul C/ex1504.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (int argc, char *argv[])
 {
-printf("invite$ %s\n", argv[0]);
+    int curseur;
 
-
-    for (argc = 0; argc <4; argc++)
+    for (curseur =0; curseur < argc; curseur++)
     {
-        printf("Arg#%d %s\n",argc+1);
+        printf("%s ", argv[curseur]);
     }
-    
-    
+
+    for (curseur = 1; curseur < argc; curseur++)
+    {
+        printf("Arg#%d %s\n",curseur,argv[curseur]);
+    }
     return(0);
 }
