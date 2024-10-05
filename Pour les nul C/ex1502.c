65ac66e4a71224f07e367a5bd4c8faafc8090123
BINDAT <maximerigaud66@gmail.com>
2024-10-05 21:39:10 +0200

    Update ex1502.c

--- Pour les nul C/ex1502.c
+++ Pour les nul C/ex1502.c
@@ -5,3 +5,8 @@ int main (int argc, char *argv[])
     printf("Nombre d'arguments fourni = %d\n",argc);
     return(0);
 }
+
+/*
+Observation : ce code effectue un affichage sur le nombre d'argument.
+!ATTENTION! : Ce code dois être compiler, est ensuite executer dans un terminal.
+*/
