1a7d1583debc388aced809b30539104fb260a4ad
BINDAT <maximerigaud66@gmail.com>
2024-10-06 15:47:55 +0200

    Create ex1506.c

---
+++ Pour les nul C/ex1506.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    printf("Validez par Entree pour vider l'affiche !");
+    getchar();
+    //system("cls"); /* Windows only */
+    system("clear"); /* Mac - Unix */
+    puts("That's better");
+    return(0);
+}
