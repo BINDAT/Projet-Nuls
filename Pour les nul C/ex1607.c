31fc3738c71b40777763c53859077debbbaf6068
BINDAT <maximerigaud66@gmail.com>
2024-10-11 21:53:42 +0200

    Update ex1607.c

--- Pour les nul C/ex1607.c
+++ Pour les nul C/ex1607.c
@@ -15,3 +15,21 @@ int main()
     printf("Vous avez %d ans et mesurez %.1f.\n", age, toise);
     return(0);
 }
+
+void moitier(void)
+{
+    float a,h;
+
+    a=(float)age/2.0;
+    printf("La moitie de votre age est %.1f.\n", a);
+    h=toise/2.0;
+    printf("La moitie de votre taille est %.1f.\n",h);
+}
+
+void doubler(void)
+{
+    age*=2.0;
+    printf("Le double de votre age est %d.\n", age);
+    toise*=2.0;
+    printf("La moitie de votre taille est %.1f\n", toise);
+}
