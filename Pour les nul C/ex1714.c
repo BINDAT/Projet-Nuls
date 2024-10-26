1c4ec9de9a44617142b8858c57d5e9c74d3e902a
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-10-26 22:16:17 +0200

    Update ex1714.c

--- Pour les nul C/ex1714.c
+++ Pour les nul C/ex1714.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-char *binbin(int n);
+char *binbin(int n); //Création de la variable binbin et avec * pour prendre en entier avec en paramètre un entier du nom n
 
-int main()
+int main() //Fonction principale.
 {
-    int b,x;
+    int b,x; //création des variables entières de b et x
 
-    b = 0x11;
+    b = 0x11; //Attribution de la valeur en hexadécimale pour la variable b
 
-    for (x=0; x<8; x++)
+    for (x=0; x<8; x++) //Boucle d'affichage de ligne. 
     {
         printf("%s 0x%04x %4d\n",binbin(b),b,b);
         b<<=1;
@@ -16,16 +16,16 @@ int main()
     return(0);
 }
 
-char *binbin(int n)
+char *binbin(int n) //Réglage de binbin et de paramètre int n. 
 {
-    static char bin[17];
-    int x;
+    static char bin[17]; //Création de la valeur statique de bin au max de 17 caractères. 
+    int x; //Création de la variable x.
 
-    for (x = 0; x < 16; x++)
+    for (x = 0; x < 16; x++)//Boucle pour changer le nombre limite de nombre afficher en une ligne.
     {
-        bin[x] = n & 0x8000 ? '1' : '0';
+        bin[x] = n & 0x8000 ? '1' : '0'; //Code binaire avec hexadécimale
         n<<=1;
     }
-    bin[x] ='\0';
-    return(bin);
+    bin[x] ='\0';//Échappement de ligne.
+    return(bin);//Fin de la fonction bin.
 }
