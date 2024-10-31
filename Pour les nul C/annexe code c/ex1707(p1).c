4efca22998c303e9fb9138abb53b6e3d3aed82b8
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-10-31 22:12:10 +0100

    Update ex1707(p1).c

--- Pour les nul C/annexe code c/ex1707(p1).c
+++ Pour les nul C/annexe code c/ex1707(p1).c
@@ -2,36 +2,35 @@
 #include <stdio.h>
 #include <ctype.h>
 
-char saisie[448];
-int curseur = 0;
+char saisie[448];//variable pour la saisie
+int curseur = 0;//variable pour naviguer dans la variable saisie 
 
 
 /*Fonction principale*/
 int main(){ 
     
     printf("Saisissez du texte : ");
-    fgets(saisie,447,stdin);
+    fgets(saisie,447,stdin);//enregistre l'entrée du clavier en valeur pou la variable saisie 
     
-    while (saisie[curseur])
+    while (saisie[curseur])//boucle pour naviguer dans la saisie avec la variable curseur
     {    
-        if(islower(saisie[curseur])){
-            putchar(toupper(saisie[curseur]));
-            
+        if(islower(saisie[curseur])){//exécution normale quand tout est en minuscule
+            putchar(toupper(saisie[curseur]));//transforme tout en majuscule
             }
-        else if(isupper(saisie[curseur])){
+        else if(isupper(saisie[curseur])){//quand un ou plusieurs caractères sont en majuscule
             printf("Quelque caractère en majuscule trouvé : ");
             putchar(toupper(saisie[curseur]));
             }
-        else if(isspace(saisie[curseur])) {
+        else if(isspace(saisie[curseur])) {//pour ignorer les espaces
                 putchar(saisie[curseur]);
                 }
-        else
+        else//quand au moins un caractères n'est pas une lettre
             {
             printf("\nProblème ils y autre chose que des lettres.");
             break;
             }
         
-        curseur++;
+        curseur++;//incrémente la boucle
     }
     return(0);
 }
