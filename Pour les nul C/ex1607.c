17de83cbcf8d55c8fcee220c58bcd939117c56fe
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-11-02 21:44:08 +0100

    Update ex1607.c

--- Pour les nul C/ex1607.c
+++ Pour les nul C/ex1607.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 
-void moitier(void);
-void doubler(void);
+void moitier(void);//création de la fonction moitier
+void doubler(void);//création de la fonction doubler
 
-int age;
-float toise;
+int age; //var age
+float toise;//var toise
 
-int main()
+int main()//fonction principale
 {
     printf("Quel est votre age ? : ");
     scanf("%d", &age);
     printf("Est votre taille ? : ");
     scanf("%f", &toise);
-    moitier();
-    doubler();
+    moitier();//référence ou bloc d'instruction moitier
+    doubler();//référence au bloc d'instruction doubler
     printf("Vous avez %d ans et mesurez %.1f.\n", age, toise);
     return(0);
 }
 
 void moitier(void)
 {
-    float a,h;
+    float a,h; //var a et h
 
-    a=(float)age/2.0;
+    a=(float)age/2.0; //attribution des val de a avec age
     printf("La moitie de votre age est %.1f.\n", a);
-    h=toise/2.0;
+    h=toise/2.0; //attribution des val de h avec toise
     printf("La moitie de votre taille est %.1f.\n",h);
 }
 
 void doubler(void)
 {
-    age*=2.0;
+    age*=2.0; //attribution de val
     printf("Le double de votre age est %d.\n", age);
-    toise*=2.0;
+    toise*=2.0;//attribution de val
     printf("La moitie de votre taille est %.1f.\n", toise);
 }
