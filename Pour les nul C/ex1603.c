41fbe6f103e36548ca4e2b172ce9b15ca8e8a2f3
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-11-12 21:35:18 +0100

    Update ex1603.c

--- Pour les nul C/ex1603.c
+++ Pour les nul C/ex1603.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main()//fonction principale
 {
-    struct tentier
+    struct tentier//structure tentier
     {
-        int a;
-    }tentier;
-    tentier.a = 2;
+        int a;//var a
+    }tentier;//étiquette d'appelle au nom de fonction tentier
+    tentier.a = 2; //fonction tentier avec attribution de var a avec attribution sur elle de sa val
     printf("%d + %d = %d\n", tentier.a,tentier.a, tentier.a+tentier.a);
     return(0);
 }
