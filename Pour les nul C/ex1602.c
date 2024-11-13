203ed6a9b08a39244d99a3527ab0d64bdd142266
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-11-13 21:44:14 +0100

    Update ex1602.c

--- Pour les nul C/ex1602.c
+++ Pour les nul C/ex1602.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-typedef int tentier;
+typedef int tentier; //définition d'un nouveau type 'tentier' qui est équivalent à 'int'
 
-tentier main()
+tentier main()//la fonction principale retourne un 'tentier' (équivalent de 'int')
 {
-    tentier a = 2;
+    tentier a = 2;//déclaration de la variable 'a' de type 'tentier' et affectation de la valeur 2
 
-    printf("Tout le monde sais que ");
+    printf("Tout le monde sait que ");
 
     printf("%d + %d = %d\n", a, a, a+a);
     return(0);
