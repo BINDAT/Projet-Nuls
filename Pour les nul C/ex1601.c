2e9d78f1042f561017030818e18fb43a1e7f1006
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-11-14 21:39:34 +0100

    Update ex1601.c

--- Pour les nul C/ex1601.c
+++ Pour les nul C/ex1601.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int main ()
+int main ()//fonction principale
 {
-    int a, b;
-    float c;
+    int a, b;//création de var a et b
+    float c;//création de la c
 
     printf("Premier entier : ");
-    scanf("%d",&a);
+    scanf("%d",&a);//prend la saisit de clavier pour la mettre en val de la var a
     printf("Second entier : ");
-    scanf("%d",&b);
-    c = a/b;
+    scanf("%d",&b);//prend la saisit de clavier pour la mettre en val de la var b
+    c = a/b;//prend la val des var a et b 
     printf("%d/%d = %.2f\n",a,b,c);
     return(0);
 }
