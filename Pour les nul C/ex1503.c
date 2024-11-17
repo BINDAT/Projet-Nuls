5a89ce5f066e05f70ffa7a7b648fa4f50667f21f
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-11-17 17:47:53 +0100

    Update ex1503.c

--- Pour les nul C/ex1503.c
+++ Pour les nul C/ex1503.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+int main (int argc, char *argv[])//fonction principale avec en paramètres argc et *argv
 {
-    printf("Cet argument est = %s.\n",argv[0]);
+    printf("Cet argument est = %s.\n",argv[0]);//affiche argv
     return(0);
 }
+
+/*!Ce code dois être compiler pour être executer dans un terminal! Ce code afficheras tout les argument entrer dans le terminale*/
