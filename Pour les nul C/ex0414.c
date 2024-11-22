f528de1877496998df7ba2462fb8d2673b732e92
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-11-22 17:54:10 +0100

    Update ex0414.c

--- Pour les nul C/ex0414.c
+++ Pour les nul C/ex0414.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    printf("\"Bonjour,\" dit le corbeau, \"que vous me semblez joli !\"");
+    printf("\"Bonjour,\" dit le corbeau, \"que vous me semblez joli !\"");//affichage avec des caractères d'échappement pour afficher ceci "".
     return 0;
 }
