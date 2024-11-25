37abdf8ccd7254b6f1b2c05e9096395cee317696
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-11-25 14:20:36 +0100

    Update ex1202.c

--- Pour les nul C/ex1202.c
+++ Pour les nul C/ex1202.c
@@ -2,22 +2,22 @@
 
 int main()
 {
-    int scoremax1,scoremax2,scoremax3,scoremax4;
+    int scoremax1,scoremax2,scoremax3,scoremax4;//crées les vars scoremax
     
     printf("Votre meilleur score: ");
-    scanf("%d",&scoremax1);
+    scanf("%d",&scoremax1);//prend l'entrée de clavier et la mets dans scoremax1
     printf("Second meilleur score: ");
-    scanf("%d",&scoremax2);
+    scanf("%d",&scoremax2);//prend l'entrée de clavier et la mets dans scoremax2
     printf("Troisième meilleur score: ");
-    scanf("%d",&scoremax3);
+    scanf("%d",&scoremax3);//prend l'entrée de clavier et la mets dans scoremax3
     printf("Quatrième meilleur score: ");
-    scanf("%d",&scoremax4);
+    scanf("%d",&scoremax4);//prend l'entrée de clavier et la mets dans scoremax4
 
 
     puts("Voici vos meilleurs score");
-    printf("#1 %d\n", scoremax1);
-    printf("#2 %d\n", scoremax2);
-    printf("#3 %d\n", scoremax3);
-    printf("#4 %d\n", scoremax4);
+    printf("#1 %d\n", scoremax1);//affiche la var
+    printf("#2 %d\n", scoremax2);//affiche la var
+    printf("#3 %d\n", scoremax3);//affiche la var
+    printf("#4 %d\n", scoremax4);//affiche la var
 
 }
