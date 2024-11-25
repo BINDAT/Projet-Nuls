081cc35058fd7a8d3ca23fdd556e97e4ed1d2528
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-11-25 14:20:38 +0100

    Update ex1203.c

--- Pour les nul C/ex1203.c
+++ Pour les nul C/ex1203.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int score[4];
+int score[4];//Création de score avec sa limite
 
 int main(){
     printf("Écrivez vos scores: ");
-    for (int boucle = 0; boucle < 4; boucle++) {
-        scanf("%d", &score[boucle]);
+    for (int boucle = 0; boucle < 4; boucle++)/*Créer une boucle qui se répète 4 fois*/ {
+        scanf("%d", &score[boucle]);//prend l'entrée du clavier est la mets dans score avec comme curseur boucle
     }
 
     puts("Voici vos scores");
 
-    for (int boucle = 0; boucle < 4; boucle++) {
-        printf("#%d\n", score[boucle]);
+    for (int boucle = 0; boucle < 4; boucle++)/*Créer une boucle qui se répète 4 fois*/ {
+        printf("#%d\n", score[boucle]);//affiche les entrées du clavier est affiche leurs position avec le curseur.
     }
 
-    return 0;
+    return(0);
 }
