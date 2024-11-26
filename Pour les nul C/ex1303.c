53ec107b8bde91a158e8de345f9c09a18693bce6
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-11-26 22:12:54 +0100

    Update ex1303.c

--- Pour les nul C/ex1303.c
+++ Pour les nul C/ex1303.c
@@ -5,27 +5,27 @@ int main()
 {
     char phrase [] = "Art. 4. : La liberté consiste à pouvoir faire tout ce qui ne nuit pas à autrui : ainsi, l'exercice des droits naturels de chaque homme n'a de bornes que celles qui assurent aux autres Membres de la Société la jouissance de ces même droits. Ces bornes ne peuvent être déterminées que par la Loi.";
 
-    int index, alpha, blank, punct, caps, lcase,total;
+    int index, alpha, blank, punct, caps, lcase,total;//créer ici les vars int
 
-    alpha = blank = punct = caps, lcase = total = 0;
+    alpha = blank = punct = caps, lcase = total = 0; //alpha ici sert d’accumulateur des autres vars
 
     /* Collecte */
-        index = 0;
-        while(phrase[index])
+        index = 0; //sert au comptage des vals inscrites
+        while(phrase[index])//prend la var avec comme curseur index
         {
-            if(isalpha(phrase[index]))
-                alpha++;
-            if(isblank(phrase[index]))
-                blank++;
-            if(ispunct(phrase[index]))
-                punct++;
+            if(isalpha(phrase[index]))//prend avec l'opérateur isalpha la var phrase avec comme curseur index
+                alpha++; //fait ajoute comme une boucle d'incrémentation à les val à la var alpha 
+            if(isblank(phrase[index]))//prend avec l'opérateur isblank la var phrase avec comme curseur index
+                blank++; //fait ajoute comme une boucle d'incrémentation à les val à la var blank
+            if(ispunct(phrase[index]))//prend avec l'opérateur ispunct la var phrase avec comme curseur index
+                punct++; //fait ajoute comme une boucle d'incrémentation à les val à la var punct
                 
-            if(isupper(phrase[index]))
-                caps++;
-            if(islower(phrase[index]))
-                lcase++;
-            index++;
-        total = alpha+blank+punct+caps+lcase;
+            if(isupper(phrase[index]))//prend avec l'opérateur isupper la var phrase avec comme curseur index
+                caps++; //fait ajoute comme une boucle d'incrémentation à les val à la var caps
+            if(islower(phrase[index]))//prend avec l'opérateur islower la var phrase avec comme curseur index
+                lcase++; //fait ajoute comme une boucle d'incrémentation à les val à la var lcase
+            index++;//stop la boucle globale est ajoute comme une boucle des vals à index
+        total = alpha+blank+punct+caps+lcase;//fait le cumul des vals de toutes les vars
         }
     /* Affichage */
         printf("\"%s\"\n",phrase);
