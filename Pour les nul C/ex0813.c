c034849f8ff242872a10fa7cfb573380ab30bfcd
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-11-29 21:20:07 +0100

    Update ex0813.c

--- Pour les nul C/ex0813.c
+++ Pour les nul C/ex0813.c
@@ -2,15 +2,15 @@
 int main(){
     int premier,second;
     printf("Indiquez la valeur de premier : ");
-    scanf("%d",&premier);
+    scanf("%d",&premier);//prend la saisit du clavier et la mets en mémoire dans la var
     printf("Indiquez la valeur de second : ");
-    scanf("%d",&second);
+    scanf("%d",&second);//prend la saisit du clavier et la mets en mémoire dans la var
     puts("Evaluation en cours ...");
-    if(premier<second){
+    if(premier<second){//si le premier est plus que second
     printf("%d est plus petit que %d\n",premier,second);
-    }else if (premier>second){
+    }else if (premier>second){//si second est plus petit que premier
     printf("%d est plus grand que %d\n",premier,second);
-    }else{
+    }else{//si les deux sont égos
     printf("%d est egale a %d\n",premier,second);
     }
     return(0);
