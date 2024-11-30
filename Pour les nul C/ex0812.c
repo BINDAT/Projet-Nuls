1c70ab9d3c4caa7a6d579fc22ff6b3fd251ca314
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-11-30 21:26:26 +0100

    Update ex0812.c

--- Pour les nul C/ex0812.c
+++ Pour les nul C/ex0812.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#define SECRET 17
+#define SECRET 17 //création de la constances SECRET établie à 17
 int main()
 {
-    int devinessai;
+    int devinessai; //création de devinessai
     printf("Tentez de deviner le chiffre secret : ");
-    scanf("%d", &devinessai);
-    if(devinessai == SECRET)
+    scanf("%d", &devinessai);//prend la saisit de clavier et la stock dans la var devinessai
+    if(devinessai == SECRET)//si la val de la var devinessai correspond à la val de la const SECRET.
     {
         puts("Bravo !");
     }
-    else 
+    else //cas contraire
     {
         puts("Non ce n'est pas cela !");
     }
     return(0);
 }
 /*
-Observation : ce code fait appel a un bloc de fonction if-else au lieu de deux if pour comparer a la solution precedente pour le meme resultat que ex0806.c 
+Observation : ce code fait appel a un bloc de fonction if-else au lieu de deux if pour comparer a la solution précédente pour le même résultat que ex0806.c 
 */
