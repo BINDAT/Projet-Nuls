e1e7097ae4e0674a6b04f65894563cfd0eef404d
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-12-10 22:09:14 +0100

    Update ex1804.c

--- Pour les nul C/ex1804.c
+++ Pour les nul C/ex1804.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
-
+#include <string.h>
 int main()
 {
-    struct robot {
-        int alive;
-        char name[5];
-        int xpos;
-        int ypos;
-        int strength;
-    };
-
-    printf("Taille de la structure robot : %u\n", sizeof(struct robot));/*Ici l'exercice indique un %u mais 
-    un %lu est recommander pour éviter une erreur de format*/
+    char string[] = "Suis-je trop longue pour vous ?";
+    int tableau[5];
+    printf("La chaine \"%s\" mesure %lu.\n",string, sizeof(string));
+    printf("Le tableau mesure \"%lu\" ",sizeof(tableau));
     return(0);
 }
-
-/*
-Observation : le code affiche l'empreinte mémoire de la structure 
-robot (Empreinte mémoire = les octets utilisés)
-*/
