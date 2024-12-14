714e56c2d9d75d9917d071a52ff7f207094978a5
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-12-14 14:19:29 +0100

    Create ex1809.c

---
+++ Pour les nul C/ex1809.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char lead;
+    char*sidekick;
+
+    lead = 'A'; /* Initialise la variable*/
+    sidekick = &lead; /* Initialise le pointeur. IMPORTANT!*/
+
+    printf("Pour la variable 'lead' :\n");
+    printf("Taille\t\t%ld\n",sizeof(lead));
+    printf("Contenu\t\t%c\n", lead);
+    printf("Adresse\t\t%p\n", &lead);
+    printf("Pour la variable 'sidekick':\n");
+    printf("Contenu\t\t%p\n", sidekick);
+
+    return(0);
+}
