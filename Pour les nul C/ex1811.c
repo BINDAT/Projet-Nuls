a59135136dd317879812dea534544b1badb608fc
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-12-15 19:06:35 +0100

    Create ex1811.c

---
+++ Pour les nul C/ex1811.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char a,b,c;
+    char *p;
+
+    a = 'A'; b = 'B'; c = 'C';
+
+    printf("Apprenez votre ");
+    p = &a;     //Initialise
+    putchar(*p);    //Utilise
+    p = &b;     //Initialise
+    putchar(*p);    //Utilise
+    p = &c;     //Initialise
+    putchar(*p);    //Utilise
+    printf("s\n");
+
+    return(0);
+}
