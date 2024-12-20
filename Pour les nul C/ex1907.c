79d9d9edb2282dcc033a24e329e648d34db2b0d9
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-12-20 19:02:19 +0100

    Create ex1907.c

---
+++ Pour les nul C/ex1907.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char alphabet[27];
+    int a;
+    char *pa;
+    
+    pa = alphabet;
+    for (a=0; a<26; a++)
+    {
+        *pa=a+'A';
+        pa++;
+    };
+    pa = alphabet;
+    for (a=0; a<26; a++)
+    {  
+        putchar(*pa);
+        pa++;  
+    }
+    putchar('\n');
+
+    return(0);
+}
