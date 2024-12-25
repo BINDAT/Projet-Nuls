eb313d668b73689e75f7954b3ae7a681a5c2d323
BINDAT <136336341+BINDAT@users.noreply.github.com>
2024-12-25 21:43:03 +0100

    Create ex1912(o).c

---
+++ Pour les nul C/annexe code c/ex1912(o).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char sample[] = "D'ou me viendra le secours?\n";
+    int index = 0;
+    
+
+    while(sample[index] != '\0')
+    {
+        putchar(sample[index]);
+        index++;
+    }
+    return(0);
+}
