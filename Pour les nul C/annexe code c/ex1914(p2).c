5de76d61372bab6a3cdf82e9d79dca91a065b7c2
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-01 21:14:35 +0100

    Create ex1914(p2).c

---
+++ Pour les nul C/annexe code c/ex1914(p2).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char sample[] = "D'ou me viendra le secours?\n";
+    int index = 0;
+
+    for(index; sample[index];)
+    {
+        putchar(sample[index++]);
+    }
+    return(0);
+}
