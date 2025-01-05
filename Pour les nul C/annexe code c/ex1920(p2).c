5564897a39cd2b2a78d3e41391f839d4293b2cfe
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-05 16:19:04 +0100

    Create ex1920(p2).c

---
+++ Pour les nul C/annexe code c/ex1920(p2).c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    char *fruits[] ={
+        "melon",
+        "banane",
+        "poire",
+        "pomme",
+        "noix",
+        "raisin",
+        "myrtille"
+    };
+    int x,y;
+    for (x=0; x <7; x++)
+    {
+        y = 0;
+        while(fruits[x][y] && putchar(fruits[x][y++]))
+            {
+
+            }      
+            putchar('\n');
+    }
+    return(0);
+}
