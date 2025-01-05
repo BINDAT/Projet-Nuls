a25adb7c98bd373e6968c6fad5c04dfc4aa2372a
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-05 17:14:01 +0100

    Create ex1921.c

---
+++ Pour les nul C/ex1921.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    char *fruits[] ={
+        "melon",
+        "banane",
+        "poire",
+        "pomme",
+        "noix",
+        "raisin",
+        "myrtille"
+    };
+    char *temp;
+    int a,b,x;
+
+    for (a=0; a<6; a++)
+        for (b=a+1; b<7; b++)
+            if (*(fruits+a) > *(fruits+b))
+            {
+                temp = *(fruits+a);
+                *(fruits+a) = *(fruits+b);
+                *(fruits+b) = temp;
+            }
+        for (x=0; x<7; x++)
+            puts(fruits[x]);
+    
+    return(0);
+}
