219b2f22cac0c0a46e78c0cd309f9930367f0979
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-10 16:57:04 +0100

    Create ex1925.c

---
+++ Pour les nul C/ex1925.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAILLE 10
+
+void create(int tableau);
+
+int main()
+{
+    int curseur;
+    puts("Données stockées : ");
+    for (curseur =0; curseur < TAILLE; curseur++)
+    {
+        
+    }
+    
+    return(0);
+}
+
+void create(int tableau)
+{
+    int *tableau[10] = {0,1,2,3,4,5,6,7,8,9};
+    int curseur_create;
+    for(curseur_create; curseur_create<TAILLE; curseur_create++)
+    {
+        printf("",);
+    }
+}
