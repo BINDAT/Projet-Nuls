f302e0b14e4241e940105f69e69cb3475feb16ff
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-15 15:56:07 +0100

    Update ex1924.c

--- Pour les nul C/ex1924.c
+++ Pour les nul C/ex1924.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-void remiser(float *a);
+void remiser(float *a);/*fonction void remiser avec sa variable float pointée a en paramètre.*/
 
-int main()
+int main()/*Fonction principale*/
 {
-    float prixbase = 42.99;
-    float p;
-    p = prixbase;
+    float prixbase = 42.99;/*variable prixbase en float avec valeur 42.99*/
+    float p;/*variable float p*/
+    p = prixbase;/*prise de la valeur prixbase dans la var p*/
     printf("L'article coute $%.2f\n",prixbase);
-    remiser (&p);
+    remiser (&p);/*appel de la fonction remiser avec en paramètre p en mémoire*/
     printf("Apres remise, cela donne $%.2f\n",p);
     return(0);
 }
 
-void remiser(float *a)
+void remiser(float *a)/*Fonction remiser*/
 {
-    *a = *a * 0.90;
+    *a = *a * 0.90;/* a pointée est égale à a pointée fois 0,90*/
 }
