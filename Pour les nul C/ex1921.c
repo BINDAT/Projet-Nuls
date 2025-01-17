e0dc896c85d637ac072e7587b3c6fbea45ce3067
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-17 21:10:42 +0100

    Update ex1921.c

--- Pour les nul C/ex1921.c
+++ Pour les nul C/ex1921.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
-int main()
+int main()/*Fonctionne principale*/
 {
-    char *fruits[] ={
+    char *fruits[] ={/*Variable tableau avec pointeur fruits*/
         "melon",
         "banane",
         "poire",
         "pomme",
         "noix",
         "raisin",
-        "myrtille"
+        "myrtille"/*Données du tableau*/
     };
-    char *temp;
-    int a,b,x;
+    char *temp;/*Variable pointée temp*/
+    int a,b,x;/*variable a b x*/
 
-    for (a=0; a<6; a++)
-        for (b=a+1; b<7; b++)
-            if (*(fruits+a) > *(fruits+b))
+    for (a=0; a<6; a++)/*boucle qui compte de 0 à 6 et augmente à partir de a*/
+        for (b=a+1; b<7; b++)/*boucle qui compte ) partir de b avec a +1 en dessous de b7 et b s'auto incrémente*/
+            if (*(fruits+a) > *(fruits+b))/*sans strcmp reprise de fruits a et b pour tri*/
             {
-                temp = *(fruits+a);
-                *(fruits+a) = *(fruits+b);
-                *(fruits+b) = temp;
+                temp = *(fruits+a); /*temp prend les val de fruits a*/
+                *(fruits+a) = *(fruits+b);/*fruits a et b sont égalisés*/
+                *(fruits+b) = temp;/*temp fait prendre ses vals en stock à fruits b*/
             }
-        for (x=0; x<7; x++)
+        for (x=0; x<7; x++)/*boucle d'impression*/
             puts(fruits[x]);
     
     return(0);
