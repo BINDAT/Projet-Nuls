47a25b6b0ce5011233d89be2bdcacd75930cfc19
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-18 21:49:55 +0100

    Update ex1919.c

--- Pour les nul C/ex1919.c
+++ Pour les nul C/ex1919.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-int main()
+int main()/*Fonction principale*/
 {
-    char *fruits[] ={
+    char *fruits[] ={/*Variable tableau pointée char fruits*/
         "melon",
         "banane",
         "poire",
         "pomme",
         "noix",
         "raisin",
-        "myrtille"
+        "myrtille"/*Données du tableau*/
     };
-    int x;
+    int x;/*Variable int x*/
 
-    for (x=0; x<7; x++)
+    for (x=0; x<7; x++)/*Boucle d'affichage*/
         {
-            putchar(**(fruits+x));
-            putchar('\n');
+            putchar(**(fruits+x));/*putchar double pointeur pour fruits pour sont affiche avec
+            incrémentation par la variable x de la boucle for*/
+            putchar('\n');/*putchar de nouvelle ligne*/
         }
-    return(0);
+    return(0); /*renvoie de la fonction principale avec paramètre 0*/
 }
