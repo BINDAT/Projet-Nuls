acb34d068aa96e5b29c3bef950d43c202a5743c4
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-20 21:32:19 +0100

    Update ex1920.c

--- Pour les nul C/ex1920.c
+++ Pour les nul C/ex1920.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main()
+int main()/*Fonction principale*/
 {
     char *fruits[] ={
         "melon",
@@ -9,17 +9,21 @@ int main()
         "pomme",
         "noix",
         "raisin",
-        "myrtille"
+        "myrtille"/*Données du tableau*/
     };
-    int x,y;
+    int x,y;/*Variable int x et y*/
 
-    for(x=0; x<7; x++)
+    for(x=0; x<7; x++)/*Boucle d'incrémentation de x*/
         {
-            for (y=0; fruits[x][y]; y++)
+            for (y=0; fruits[x][y]; y++)/*Boucle d'affichage et d'affichage de y*/
             {
-                putchar((fruits[x][y]));
+                putchar((fruits[x][y]));/*putchar qui gère l'affichage tableau en deux parties x et y incrémenter par les boucles*/
             }
-            putchar('\n');
+            putchar('\n');/*putchar de nouvelle ligne*/
         }
     return(0);
 }
+/*
+Observation : affichage correct des données par la gestion du tableau 
+en deux parties et les deux boucles for
+*/
