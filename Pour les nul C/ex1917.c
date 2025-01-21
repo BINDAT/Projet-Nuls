24253928c373a326bb6821c8e29f27f07f4764cc
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-21 21:21:02 +0100

    Update ex1917.c

--- Pour les nul C/ex1917.c
+++ Pour les nul C/ex1917.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
-int main()
+int main()/*Fonction principale*/
 {
-    char *fruits[] ={
+    char *fruits[] ={/*Variable tableau pointée char fruits*/
         "melon",
         "banane",
         "poire",
         "pomme",
         "noix",
         "raisin",
-        "myrtille"
+        "myrtille"/*Données du tableau*/
     };
-    int x;
+    int x;/*Variable int x*/
 
-    for (x=0; x < 7; x++)
-        puts(fruits[x]);
+    for (x=0; x < 7; x++)/*putchar simple pointeur pour fruits pour sont affiche avec
+    incrémentation par la variable x de la boucle for*/
+        puts(fruits[x]);/*putchar de nouvelle ligne qui affiche une à une les valeurs de fruits x en tableau*/
     
     return(0);
 }
+/*
+Observation : Affichage des données correcte.
+*/
