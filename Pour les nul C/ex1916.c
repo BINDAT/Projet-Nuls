65660b34861f7be6b18927584f124936a7575955
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-22 17:37:25 +0100

    Update ex1916.c

--- Pour les nul C/ex1916.c
+++ Pour les nul C/ex1916.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-int main()
+int main()/*Fonction principale*/
 {
-    char *sample = "D'ou me viendra le secours?\n";
-    char *save;
+    char *sample = "D'ou me viendra le secours?\n";/*variable char pointée avec une chaîne de caractères*/
+    char *save;/*variable de caractères pointée*/
     
-    save = sample;
+    save = sample; /*la var save prend la val de la var sample*/
     
 
-    while (putchar(*sample++))
+    while (putchar(*sample++))/*la boucle affiche toute la chaîne de caractères de sample*/
     {
-        ;
+        ;/*instruction qui permet au code de fonctionner*/
     }
     
-    sample = save;
-    puts(sample);
+    sample = save;/*sample prend la val de save point de restauration*/
+    puts(sample);/*sample est affichée après le point de restauration*/
 
     return(0);
 }
