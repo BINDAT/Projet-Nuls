23f80e4bceb0e9173b74b8774c3aa50857b78732
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-23 17:44:53 +0100

    Update ex1914.c

--- Pour les nul C/ex1914.c
+++ Pour les nul C/ex1914.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-int main()
+int main()/*fonction principale*/
 {
-    char sample[] = "D'ou me viendra le secours?\n";
-    int index = 0;
+    char sample[] = "D'ou me viendra le secours?\n";/*Variable tableau sample*/
+    int index = 0;/*variable index et valeur 0*/
 
-    while(sample[index])
+    while(sample[index])/*la boucle prend sample et sample prend en paramètre index*/
     {
-        putchar(sample[index++]);
+        putchar(sample[index++]);/*putchar prend sample qui lui même prend index qui incrémente*/
     }
     return(0);
 }
+/*
+Observation : ce code permet l'affichage correct de la var sample.
+*/
