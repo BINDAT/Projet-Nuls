0418b59cc590e75ff95a67c90287373dcd930ddf
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-23 17:34:07 +0100

    Update ex1915.c

--- Pour les nul C/ex1915.c
+++ Pour les nul C/ex1915.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int main()
+int main()/*fonction principale*/
 {
-    char *sample = "D'ou me viendra le secours?\n";
+    char *sample = "D'ou me viendra le secours?\n";/*Variable char pointée  avec chaîne de caractère en valeur*/
 
-    while (putchar(*sample++))
+    while (putchar(*sample++))/*affiche tout de la var sample*/
     {
-        ;
+        ;/*instruction vide pour que la boucle while fonctionne*/
     }
     return(0);
 }
+/*
+Observation : ce code affiche un résultat correct pour peut d'écriture
+*/
