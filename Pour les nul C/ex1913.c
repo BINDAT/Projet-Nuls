e0df347590d5166e3305e59d28ab3c8280f685c9
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-24 21:13:31 +0100

    Update ex1913.c

--- Pour les nul C/ex1913.c
+++ Pour les nul C/ex1913.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
-int main()
+int main()/*fonction principale*/
 {
-    char sample[] = "D'ou me viendra le secours?\n";
-    int index = 0;
+    char sample[] = "D'ou me viendra le secours?\n";/*Variable de chaîne de caractère en tableau*/
+    int index = 0;/*variable mise à 0*/
 
-    while(sample[index])
+    while(sample[index])/*Boucle while avec paramètre sample est sample avec curseur de tableau index*/
     {
-        putchar(sample[index]);
-        index++;
+        putchar(sample[index]);/*putchar affiche sample à l'avancer de du curseur index en tableau*/
+        index++;/*Incrémentation de index pour faire avancer le tableau en boucle*/
     }
     return(0);
 }
+/*
+Observation : affiche la valeur correctement.
+*/
