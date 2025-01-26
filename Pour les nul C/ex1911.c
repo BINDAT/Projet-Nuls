e2e6ca666d6331148060f66b0af4d442867dc138
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-26 18:42:42 +0100

    Update ex1911.c

--- Pour les nul C/ex1911.c
+++ Pour les nul C/ex1911.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main()
+int main()/*Fonction principale*/
 {
-    float temps[5] = { 58.7, 62.8, 65.0, 63.3, 63.2 };
+    float temps[5] = { 58.7, 62.8, 65.0, 63.3, 63.2 };/*variable tableau à limite de 5 valeurs maximum qui contient des valeurs*/
 
-    printf("Mardi il fera %.1f\n", temps[1]);
-    printf("Vendredi il fera %.1f\n", temps[4]);
+    printf("Mardi il fera %.1f\n", temps[1]);/*affiche la première valeur*/
+    printf("Vendredi il fera %.1f\n", temps[4]);/*affiche la quatrième valeur*/
     return(0);
 }
+/*
+Observation : le code affiche la première valeur et la quatrième valeurs grâce aux curseurs dans les printf avec les variable temp appelées.
+*/
