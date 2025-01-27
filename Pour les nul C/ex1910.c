c718293bee43bde19b3cfbbf3f86abadadeed39c
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-27 20:28:00 +0100

    Update ex1910.c

--- Pour les nul C/ex1910.c
+++ Pour les nul C/ex1910.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-int main()
+int main()/*fonction principale*/
 {
-    char alphabet = 'A';
-    int a;
-    char *pa;
+    char alphabet = 'A';/*variable de caractère de valeur égale A*/
+    int a;/*variable a*/
+    char *pa;/*variable pointée pa*/
     
-    pa = &alphabet;
+    pa = &alphabet;/*pa prend en mémoire la valeur de la variable alphabet*/
     
-    for (a=0; a<26; a++)
+    for (a=0; a<26; a++)/*boucle qui vas jusqu'à 27 tours*/
       
-        putchar((*pa)++);  
+        putchar((*pa)++);/*putchar qui affiche le contenu de pa en incrémentent*/
     putchar('\n');
 
     return(0);
