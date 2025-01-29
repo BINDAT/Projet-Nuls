36946958e7bd124b649e0c38e844206f4214c905
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-01-29 16:04:39 +0100

    Update ex1908.c

--- Pour les nul C/ex1908.c
+++ Pour les nul C/ex1908.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-int main()
+int main()/*fonction principale*/
 {
-    char alphabet[27];
-    int a;
-    char *pa;
+    char alphabet[27];/*tableau limite de valeur 27*/
+    int a;/*var a*/
+    char *pa;/*var pointée pa*/
     
-    pa = alphabet;
-    for (a=0; a<26; a++)
+    pa = alphabet;/*pa prend la valeur alphabet*/
+    for (a=0; a<26; a++)/*boucle de 0 à 26 en auto incrémentation*/
     {
-        *pa++=a+'A';
+        *pa++=a+'A';/*pa incrémente la boucle a qui commence à A*/
     }
-    pa = alphabet;
-    for (a=0; a<26; a++)
+    pa = alphabet;/*point de restauration*/
+    for (a=0; a<26; a++)/*boucle de 0 à 26 en auto incrémentation*/
     {  
-        putchar(*pa);
-        pa++;  
+        putchar(*pa);/*affichage*/
+        pa++;/*incrémentation de pa*/
     }
     putchar('\n');
 
