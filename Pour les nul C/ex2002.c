72a5b85c3431e8bc7b546e73006c0c09ed46fe23
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-02-03 09:54:55 +0100

    Update ex2002.c

--- Pour les nul C/ex2002.c
+++ Pour les nul C/ex2002.c
@@ -8,13 +8,16 @@ int main ()/*fonction principale*/
   kelvin = celsius + 273.15;
   kelvin = (fahrenheit + 459.67)*(5.0/9.0);
   kelvin = (float*)malloc(sizeof(float)*1);/*A etudier*/
-  if(kelvin == NULL)
+  if(kelvin == 'C' || 'c')
   {
-    puts("Allocation memoire impossible");
+    puts("Température en celsius");
     exit(1);/*sortie de if*/
+  }elseif(kelvin == 'F' || 'f')
+  {
+   
   }
   printf("Quel est votre température ? ");
-  scanf("%d",kelvin);
-  printf("Vous avez %d .\n", *kelvin);
+  scanf("%f",kelvin);
+  printf("Vous avez %f .\n", *kelvin);
   return(0);
 }
