2da260cd106e1c4657bb87959be6600ffa337a90
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-02-07 16:08:07 +0100

    Update ex0404.c

--- Pour les nul C/ex0404.c
+++ Pour les nul C/ex0404.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdio.h>/*Bibliothèque*/
 
-int main ()
+int main ()/*Fonction principale*/
 {
     puts("Une souris verte,");
     puts("qui courait dans l'herbe.");
     puts("Je l'attrape par la queue,");
     puts("Je la montre a ces messieurs .");
-    puts("Ces messieurs me disent");
+    puts("Ces messieurs me disent");/*Affichage*/
     return(0);
 }
