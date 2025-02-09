2a4bc0ded298b4d3b784feade48f2de84fd746a5
bindat <maximerigaud66@gmail.com>
2025-02-09 18:00:39 +0100

    Mise à jour     modified:   Pour les nul C/ex2002.c

--- Pour les nul C/ex2002.c
+++ Pour les nul C/ex2002.c
@@ -3,13 +3,13 @@
 
 int main ()/*fonction principale*/
 {  
-  float celsius,fahrenheit;
-  celsius = 273.15;
-  fahrenheit = (459.67)*(5.0/9.0);
-  float *kelvin;
-  char type;
+  float celsius,fahrenheit; /*variable float*/
+  celsius = 273.15;/*attribution de valeur float à la variable celsius*/
+  fahrenheit = (459.67)*(5.0/9.0);/*attribution de valeur à la variable fahrenheit*/
+  float *kelvin;/*variable float pointée kelvin*/
+  char type;/*variable de type chaîne de caractère*/
   
-  kelvin = (float *)malloc(sizeof(float));/*A etudier*/
+  kelvin = (float *)malloc(sizeof(float));/*A étudier*/
   
   if(kelvin == NULL)
   {
@@ -24,7 +24,7 @@ int main ()/*fonction principale*/
       {
         *kelvin = *kelvin + celsius; 
         printf("Vous avez %2.f °c\n",*kelvin);
-          exit(1);/*sortie de if*/
+          exit(2);/*sortie de if*/
       }else if(type == 'F' || type == 'f')
       {
         *kelvin = *kelvin + fahrenheit;
@@ -37,3 +37,8 @@ int main ()/*fonction principale*/
 
   return(0);
 }
+/*
+Observation : Ce code propose d'afficher dans l'unité choisie grâce à la valeur entrée au clavier dans la variable type. Par la suite si c'est Celsius
+la variable kelvin et en addition avec Celsius si Fahrenheit et choisie cette dernier applique une addition mêlée à une multiplication dans une division.
+Dans les deux cas la valeur inscrite est reprise est retravaillée.
+*/
