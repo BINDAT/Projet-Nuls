26024cb93e5a9826e72d2d5f0c83ac51a804c3cd
bindat <maximerigaud66@gmail.com>
2025-02-12 22:11:51 +0100

    Nouveau code pas complet pour le moment

---
+++ Pour les nul C/ex2003.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int nombre1, nombre2, nombre3;
+    nombre1 =100;
+    nombre2 =200;
+    nombre3 =300;
+    int nombre[3]={}; 
+    
+    nombre[3]= (int *)malloc(sizeof(int)*3);
+}
