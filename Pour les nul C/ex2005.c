7545139c866f94471da340140f5ff1c4a623cf4c
bindat <maximerigaud66@gmail.com>
2025-02-18 20:52:44 +0100

    update 2005.c

--- Pour les nul C/ex2005.c
+++ Pour les nul C/ex2005.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
@@ -14,20 +15,20 @@ int main()
     puts("Saisissez quelque chose d'un peu long :");
 
     fgets(entrante,1023,stdin);
-    puts("Vous avez saisi :");
+    /*puts("Vous avez saisi :"); Test ici*/
     printf("\"%s\"\n",entrante);
     
     sortante = (char *)malloc(sizeof(char)*1024);
-    
     if (sortante==NULL)
     {
         puts("Allocation de impossible ! Banzai !");
-        sortante = entrante;
+        strcpy(entrante,sortante);
         free(entrante);
+        free(sortante);
         exit(2);
     }
     puts("Vous avez saisi :");
-    printf("\"%s",sortante); 
+    printf("\"%s\"\n",sortante); 
   
     return(0);
 }
