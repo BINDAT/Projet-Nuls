fa4755ba3bd504728bab2c1e5cf9962b473f6d21
bindat <maximerigaud66@gmail.com>
2025-03-03 18:58:56 +0100

    update ex2012.C en chantier

---
+++ Pour les nuls C/ex2012.c
@@ -0,0 +1,50 @@
+/* Programme interactif*/
+/* Dan Gooking, Beginning Programming with C For Dummies*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+struct stypik
+{
+    int maval;
+    struct stypik *asuiv;
+};
+struct stypik *aprems; /* Pointeur vers la première action */
+struct stypik *acour;  /* Pointeur temporaire pour le parcours */
+struct stypik *anouv;  /* Pointeur pour la création de nouvelles actions. Prévois des variable pointées pour la structure action */
+/*Protypage des fonctions*/
+int menu(void);
+void ajouter(void);
+void montrer(void);
+void supprimer(void);
+struct stypik *creer(void);
+
+/* La fonction main() ne se charge que de la saisie.
+   Le reste est dans les fonctions.
+*/
+
+int main()
+{
+    int choixmenu ='\0'; /* Initialise la boucle while */
+    aprems = NULL;
+
+    while (choixmenu != 'Q')
+    {
+        choixmenu = menu();
+        switch (choixmenu)
+        {
+        case 'M':
+            montrer();
+            break;
+        case 'A':
+            ajouter();
+            break;
+        case'Q':
+            supprimer();
+            break;
+        default:
+            break;
+        }
+    }
+    return(0);
+}
