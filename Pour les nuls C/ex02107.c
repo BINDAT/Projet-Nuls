79a1c610daafa06b5af9afa554c21717af693591
bindat <maximerigaud66@gmail.com>
2025-03-12 14:37:54 +0100

    update ex2107.c

---
+++ Pour les nuls C/ex02107.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    time_t tictoc;
+    struct tm *present;
+
+    time(&tictoc);
+    present = localtime(&tictoc);
+    printf("A present, nous sommes le %d/%d/%d\n",
+        present->tm_mday,
+        present->tm_mon +1,
+        present->tm_year +1900);
+    return(0);
+}
