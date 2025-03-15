b3315d16fbf068b1c1bad5e643ec788ebfd0501f
bindat <maximerigaud66@gmail.com>
2025-03-15 22:12:29 +0100

    update ex2203.c et ex2204.c

---
+++ Pour les nuls C/ex2203.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *fh;
+
+    fh=fopen("hello.txt","w"); // Mode ecriture
+    if (fh==NULL)
+    {
+        puts("Ouverture du fichier impossible");
+        exit(1);
+    }
+    fprintf(fh, "Je laisse une trace.\n");
+    fputs("C'est mon programme qui produit ce contenu.\n",fh);
+    fclose(fh);
+    return(0);
+}
