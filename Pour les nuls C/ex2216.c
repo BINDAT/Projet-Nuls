e240ee6f2b77175e195471ef36d5886e5c71b494
bindat <maximerigaud66@gmail.com>
2025-03-19 22:18:36 +0100

    correction

--- Pour les nuls C/ex2216.c
+++ Pour les nuls C/ex2216.c
@@ -11,20 +11,20 @@ int main()
     };
     struct filmo bond;
     FILE *a007;
-    int count = 0;
 
     a007 = fopen("bond.db","r");
+   
     if (!a007)
     {
         puts("SPECTRE gagne !");
         exit(1);
     }
     fseek(a007, sizeof(struct filmo)*1, SEEK_SET);
-    fread(&bond, sizeof(struct filmo), 1, a007);
-            printf("%s\t%d\t%s\n",
-                bond.acteur,
-                bond.annee,
-                bond.titre);
+    fread(&bond,sizeof(struct filmo), 1, a007);
+        printf("%s\t%d\t%s\n",
+            bond.acteur,
+            bond.annee,
+            bond.titre);
     fclose(a007);
     
     return(0);
