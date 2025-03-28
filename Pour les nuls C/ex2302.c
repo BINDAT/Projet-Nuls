5a121f603f1f5cfd51ef6bd978a950fc653c43a2
bindat <maximerigaud66@gmail.com>
2025-03-28 21:50:50 +0100

    update ex2302.c

---
+++ Pour les nuls C/ex2302.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+int main()
+{
+    DIR *nomdoss;
+    struct dirent *file;
+
+    nomdoss = opendir(".");
+    if (nomdoss == NULL)
+    {
+        puts("Lecture du dossier impossible");
+        exit(1);
+    }
+    while (fread(&nomdoss,sizeof(struct dirent),1,file))
+    {
+        file = readdir(nomdoss);
+        printf("Nom du fichier ou dossier '%s'\n", file->d_name);
+    
+    }
+    closedir(nomdoss);
+
+    return(0);
+}
