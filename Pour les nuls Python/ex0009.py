f1b0da5ac5569f06b7db643ed7591d3144b47f04
bindat <maximerigaud66@gmail.com>
2025-04-10 22:00:39 +0200

    update py 9 & 10

---
+++ Pour les nuls Python/ex0009.py
@@ -0,0 +1,12 @@
+Test = 0b100
+print("100 Binaire : ",Test)
+
+Test = 0o100
+print("100 Octal : ", Test)
+
+Test = 0x100
+print("100 Hexadécimal : ", Test)
+
+print(bin(Test))
+print(oct(Test))
+print(hex(Test))
---
+++ Pour les nuls Python/ex0010.py
@@ -0,0 +1,8 @@
+Test = 255.0
+print("Affection directe : ", Test)
+
+Test = 2.55e2
+print("Notation scientifique : ", Test)
+
+Test = 2.55e-2
+print("Exposant négatif : ", Test)
