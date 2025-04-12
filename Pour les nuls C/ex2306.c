46a116afd937b9acdb93495159afe690ccbceabd
bindat <maximerigaud66@gmail.com>
2025-04-12 21:06:23 +0200

    update commentaire pour ex2306 & 07

--- Pour les nuls C/ex2306.c
+++ Pour les nuls C/ex2306.c
@@ -7,17 +7,17 @@ int main()
 {
     char dosscour[255];      
         
-        if(getcwd(dosscour, sizeof(dosscour)) == NULL)            
+        if(getcwd(dosscour, sizeof(dosscour)) == NULL)/*Si pas possible de prendre le chemin*/          
             {
                 printf("erreur de récupération");
                 return(1);
             }
             
-        if(mkdir("ultra_tempo", 0755)==0)
+        if(mkdir("ultra_tempo", 0755)==0)/*S'il est possible de modifier le contenu et créer le dossier avec 0755 qui donne tout les droits*/
             {
                 puts("Nouveau dossier fait.");
                 printf("Dossier courant : %s\n", dosscour);
-                chdir("ultra_tempo");
+                chdir("ultra_tempo");/*modifie l'accès au dossier cible*/
                 getcwd(dosscour, 255);
                 printf("Dossier courant à l'endroit : %s\n", dosscour);
             }
--- Pour les nuls C/ex2307.c
+++ Pour les nuls C/ex2307.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main()/*Fonction principale*/
 {
-    FILE *test;
+    FILE *test; /*Var pointée test de typedef FILE*/
 
-    test = fopen("blorfus", "w");
+    test = fopen("blorfus", "w");/*Test créer un fichier blorfus en écriture*/
     if (!test)
     {
-        puts("Création du fichier impossible");
+        puts("Création du fichier impossible");/*En cas d'impossibilité*/
         exit(1);
     }
-    fclose(test);
+    fclose(test);/*Ferme la création du fichier avec test si réussit*/
     puts("Fichier cree");
-    if (rename("blorfus", "wambooli") == -1)
+    if (rename("blorfus", "wambooli") == -1)/*Modifie le nom du fichier*/
     {
-        puts("Impossible de renommer");
+        puts("Impossible de renommer");/*Si cela n'est pas possible*/
         exit(1);
     }
     puts("Nom du fichier modifie");
