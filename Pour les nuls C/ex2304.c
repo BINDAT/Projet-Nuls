ebd7d31efe38f24b4de842a9cc7e7dd40fdbf77e
bindat <maximerigaud66@gmail.com>
2025-04-13 21:30:23 +0200

    update commentaire de ex2304 et 05.c

--- Pour les nuls C/ex2304.c
+++ Pour les nuls C/ex2304.c
@@ -6,24 +6,24 @@
 
 int main()
 {
-    DIR *nomdoss;
-    struct dirent *file;
-    struct stat statisfic;
+    DIR *nomdoss; /*Var pointée de type DIR*/
+    struct dirent *file; /*Création d'une structure qui rejoint sur une var pointée avec fonction dirent pour accéder au fichier est dossier*/
+    struct stat statisfic; /*Création d'une structure sur une var simple et stat pour faire des statiques*/
     
-    nomdoss = opendir(".");
+    nomdoss = opendir("."); /*Ouvre le dossier ici et le retient dans la var*/
     
     if (nomdoss == NULL)
     {
         puts("Lecture du dossier impossible");
         exit(1);
     }
-    while(file = readdir(nomdoss))
+    while(file = readdir(nomdoss))/*Ici la boucle affiche les infos du dossier*/
     {
-        stat(file->d_name, &statisfic);
+        stat(file->d_name, &statisfic);/*Prend le nom du dossier pour le mettre en mémoire dans la var*/
         printf("%-20s %5s %10ld octect %s\n", file->d_name,S_ISDIR(statisfic.st_mode) ? "[DIR]" : "[FILE]",
         (long)statisfic.st_size,
-        ctime(&statisfic.st_mtime));
+        ctime(&statisfic.st_mtime));/*Affiche le nom la mémoire la date de modification et le type entre dossier et fichier*/
     }
-    closedir(nomdoss);
+    closedir(nomdoss);/*Ferme l'exploration du chemin d'accès*/
     return(0);
 }
--- Pour les nuls C/ex2305.c
+++ Pour les nuls C/ex2305.c
@@ -4,14 +4,14 @@
 
 int main()
 {
-    char dosscour[255];
+    char dosscour[255]; /*Var tableau avec limite définie*/
 
-    getcwd(dosscour, 255);
-    printf("Dossier courant : %s\n", dosscour);
-    mkdir("ultra_tempo", 755);  //L11 pour Unix
+    getcwd(dosscour, 255); /*Prend le chemin d'accès pour le mettre en mémoire*/
+    printf("Dossier courant : %s\n", dosscour);/*Ici la mémoire de la var et afficher sois le chemin d'accès*/
+    mkdir("ultra_tempo", 755);  //L11 pour Unix /*Modifie le contenu du dit chemin d'accès pour ajouter un dossier*/
     puts("Nouveau dossier fait.");
-    chdir("ultra_tempo");
-    getcwd(dosscour, 255);
-    printf("Dossier courant : %s\n", dosscour);
+    chdir("ultra_tempo");/*Ici change de chemin pour le dossier créer*/
+    getcwd(dosscour, 255);/*Prend en mémoire ici le nouveau chemin d'accès et le mets en mémoire*/
+    printf("Dossier courant : %s\n", dosscour);/*Pour afficher ici le dossier créer*/
     return(0);
 }
