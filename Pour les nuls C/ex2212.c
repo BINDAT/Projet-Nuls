94a852bfafaa135337c57e9d39d304780525649e
bindat <maximerigaud66@gmail.com>
2025-04-16 19:41:20 +0200

    update commentaire sur ex2212 et 13.c

--- Pour les nuls C/ex2212.c
+++ Pour les nuls C/ex2212.c
@@ -8,22 +8,22 @@ int main()
         char acteur[32];
         int annee;
         char titre[32];
-    };
-    struct filmo bond;
-    FILE *a007;
+    }; /*Base de donnée*/
+    struct filmo bond; /*Reprise de la structure de donnée*/
+    FILE *a007; /*Var pointée type FILE*/
 
-    strcpy(bond.acteur, "Sean Connery");
+    strcpy(bond.acteur, "Sean Connery"); /*Ajout d'information*/
     bond.annee = 1962;
-    strcpy(bond.titre,"Dr. No");
+    strcpy(bond.titre,"Dr. No"); /*Fin d'ajout d'information*/
 
-    a007 = fopen("bond.db","w");
-    if (!a007)
+    a007 = fopen("bond.db","w"); /*Ouverture de session d'écriture en mode écriture en écrasement*/
+    if (!a007) /*En cas de pépin*/
     {
         puts("SPECTRE gagne !");
         exit(1);
     }
-    fwrite(&bond, sizeof(struct filmo), 1, a007);
-    fclose(a007);
+    fwrite(&bond, sizeof(struct filmo), 1, a007); /*Ajout les information dans le fichier*/
+    fclose(a007); /*Fermeture de session*/
     puts("Enregistrement ecrit");
 
     return(0);
--- Pour les nuls C/ex2213.c
+++ Pour les nuls C/ex2213.c
@@ -8,29 +8,29 @@ int main()
         char acteur[32];
         int annee;
         char titre[32];
-    };
-    struct filmo bond;
-    FILE *a007;
+    }; /*Base de donnée*/
+    struct filmo bond; /*Appelle de la structure avec bond en affection*/
+    FILE *a007; /*var de pointée de type FILE*/
 
-    strcpy(bond.acteur, "Roger Moore");
-    bond.annee = 1973;
-    strcpy(bond.titre,"Live and Let Die");
+    strcpy(bond.acteur, "Roger Moore"); /*Ici ajout du nom d'acteur*/
+    bond.annee = 1973; /*Ici de l'année*/
+    strcpy(bond.titre,"Live and Let Die"); /*Est la du titre du film*/
     
-    a007 = fopen("bond.db","a");
+    a007 = fopen("bond.db","a"); /*Ouvre ici le fichier en mode ajout sans effacement de l'écriture précédente*/
        
-        if (!a007)
+        if (!a007) /*En cas de problème*/
         {
             puts("SPECTRE gagne !");
             exit(1);
         }
-        fwrite(&bond, sizeof(struct filmo), 1, a007);
+        fwrite(&bond, sizeof(struct filmo), 1, a007); /*Reprend le fichier pour une nouvelle écriture*/
     
-    strcpy(bond.acteur, "Pierce Brosnan");
+    strcpy(bond.acteur, "Pierce Brosnan"); /*Rajoute de nouvelle ici*/
     bond.annee = 1995;
     strcpy(bond.titre,"GoldenEye");
     
-    fwrite(&bond, sizeof(struct filmo), 1, a007);
-    fclose(a007);
+    fwrite(&bond, sizeof(struct filmo), 1, a007); /*A là*/
+    fclose(a007); /*Fermeture du fichier est fin d'écriture*/
     puts("Enregistrement ecrit");
 
     return(0);
