b1dfef66fc8e866bac89bd3c04aea72fbc07ab06
bindat <maximerigaud66@gmail.com>
2025-04-18 22:33:11 +0200

    update de 0030.py terminé.

--- Pour les nuls Python/ex0030.py
+++ Pour les nuls Python/ex0030.py
@@ -27,4 +27,22 @@ if (MainChoice == 1):
         print("Nous avons des oeufs, mais pas ce type de crêpe.")
 
 elif (MainChoice == 2) or (MainChoice == 3):
-    
+    print("1. Sirop")
+    print("2. Confiture de fraises")
+    print("1. Sucre en poudre")
+    Topping = int(input("Choisissez une garniture : "))
+
+    if (Topping == 1):
+        print("Vous avez choisi " + Meal + "au sirop.")
+    if (Topping == 2):
+        print("Vous avez choisi " + Meal + " à la confiture de fraises.")
+    if (Topping == 3):
+        print("Vous avez choisis " + Meal + " avec du sucre en poudre.")
+    else:
+        print("Nous avons des " + Meal + ", mais pas cette garniture.")
+
+elif (MainChoice == 4):
+    print("Vous avez choisi de céréales.")
+
+else:
+    print("Désolé, nous n'avons pas cela dans notre menu !")
