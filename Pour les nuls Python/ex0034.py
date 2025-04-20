805d19ffa5b19654460ffff51489e79dd658310b
bindat <maximerigaud66@gmail.com>
2025-04-20 21:29:51 +0200

    update ex0033 et 34.py

---
+++ Pour les nuls Python/ex0034.py
@@ -0,0 +1,7 @@
+LetterNum = 1
+for Letter in "Bonjour !":
+    if Letter == "j":
+        pass # Ajouter du code plus ici pour traiter j
+        print("La lettre j rencontrée, traitement interrompu")
+    print("La lettre ", LetterNum, " est ", Letter)
+    LetterNum+=1
