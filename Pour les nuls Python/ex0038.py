8ede3955a62dc1b076072f57ec849afc5793b7cb
bindat <maximerigaud66@gmail.com>
2025-04-22 21:35:04 +0200

    update de ex0037 et 38.py

---
+++ Pour les nuls Python/ex0038.py
@@ -0,0 +1,9 @@
+try:
+    Value = int(input("Tapez un nombre entre 1 et 10 :"))
+except ValueError:
+    print("Vous devez tapez un nombre entre 1 et 10 !")
+else:
+    if (Value > 0) and (Value <= 10):
+        print("Vous avez tapé : ", Value)
+    else:
+        print("La valeur que vous avez tapée est incorrecte !")
