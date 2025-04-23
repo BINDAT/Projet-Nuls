20c924edb7469617c49315c16e403e329f6b8d62
bindat <maximerigaud66@gmail.com>
2025-04-23 16:38:30 +0200

    update commentaire de ex2303 et 02.c

--- Pour les nuls C/ex2302.c
+++ Pour les nuls C/ex2302.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
+#include <dirent.h> /*Pour afficher des information du dossier*/
 
 int main()
 {
-    DIR *nomdoss;
-    struct dirent *file;
+    DIR *nomdoss; /*Sert de pointeur de dossier*/
+    struct dirent *file;/*sert de pointeur de fichier*/
 
-    nomdoss = opendir(".");
+    nomdoss = opendir("."); /*Ouvre la lecture du dossier*/
     if (nomdoss == NULL)
     {
         puts("Lecture du dossier impossible");
@@ -16,10 +16,10 @@ int main()
     while ((file = readdir(nomdoss))!= NULL)
     {
         file = readdir(nomdoss);
-        printf("Nom du fichier ou dossier '%s'\n", file->d_name);
+        printf("Nom du fichier ou dossier '%s'\n", file->d_name); /*Affiche des information du dossier et fichier*/
     
     }
-    closedir(nomdoss);
+    closedir(nomdoss); /*Fermer la lecture du dossier*/
 
     return(0);
 }
--- Pour les nuls C/ex2303.c
+++ Pour les nuls C/ex2303.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
-#include <time.h>
+#include <dirent.h> /*Pour lire les dossier*/
+#include <time.h> /*Pour afficher les information du temps*/
 #include <sys/stat.h>
 
 int main()
 {
     DIR *nomdoss;
     struct dirent *file;
-    struct stat statisfic;
+    struct stat statisfic; /*Ici la structure stat statisfic montreras des information quand elle seras appelée*/
     
-    nomdoss = opendir(".");
+    nomdoss = opendir("."); /*Ouvre la lecture du dossier*/
     if (nomdoss == NULL)
     {
         puts("Lecture du dossier impossible");
@@ -22,8 +22,8 @@ int main()
         printf("%-14s %5ld %s",
         file->d_name,
         (long)statisfic.st_size,
-        ctime(&statisfic.st_mtime));
+        ctime(&statisfic.st_mtime)); /*Ici statisfic est appelée est montre les information de taille et de date de modification*/
     }
-    closedir(nomdoss);
+    closedir(nomdoss);/*Ferme la lecture du dossier*/
     return(0);
 }
