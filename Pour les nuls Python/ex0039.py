02a391066b0b38532132b171f0bca5b1c809e2e0
bindat <maximerigaud66@gmail.com>
2025-04-23 15:57:08 +0200

    update ex0039 et 40.py

---
+++ Pour les nuls Python/ex0039.py
@@ -0,0 +1,11 @@
+try:
+    Value = int(input("Tapez un nombre entre 1 et 10 :"))
+except ValueError:
+    print("Vous devez tapez un nombre entre 1 et 10 !")
+except:
+    print("Erreur générique")
+else:
+    if (Value > 0) and (Value <= 10):
+        print("Vous avez tapé : ", Value)
+    else:
+        print("La valeur que vous avez tapée est incorrecte !")
---
+++ Pour les nuls Python/ex0040.py
@@ -0,0 +1,8 @@
+import sys
+try:
+    File = open("myfile.txt")
+except IOError as e:
+    print("Erreur lors de l'ouverture du fuchier ! \r\n" + "Numéro de l'erreur : {0}\r\n".format(e.errno)+"Texte de l'erreur : {0}".format(e.strerror))
+else:
+    print("Le fichier a bien été ouvert.")
+    File.close()
