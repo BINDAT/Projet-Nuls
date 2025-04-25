d32a19880140a75103d384b75563a3a03652c11e
bindat <maximerigaud66@gmail.com>
2025-04-25 21:32:39 +0200

    update commentaire de ex2207.c

--- Pour les nuls C/ex2207.c
+++ Pour les nuls C/ex2207.c
@@ -3,19 +3,19 @@
 
 int main()
 {
-    FILE *handle;
-    int scoremax;
+    FILE *handle; /*Cette var est la pour la création*/
+    int scoremax; /*Cette var pour la saisie de données*/
 
-    handle = fopen("scores.dat", "w");
-    if (!handle)
+    handle = fopen("scores.dat", "w"); /*handle avec fopen créer le fichier score.dat en écriture (w)*/
+    if (!handle) /*En cas d'erreur*/
     {
         puts("Erreur fichier!");
         exit(1);
     }
     printf("Indiquez votre meilleur score : ");
-    scanf("%d", &scoremax);
-    fwrite(&scoremax, sizeof(int), 1, handle);
-    fclose(handle);
+    scanf("%d", &scoremax); /*Prend la saisie*/
+    fwrite(&scoremax, sizeof(int), 1, handle); /*écrit la donnée dans scoremax*/
+    fclose(handle); /*Ferme l'écriture du fichier*/
     puts("Score sauf");
     return(0);
 }
--- Pour les nuls C/ex2208.c
+++ Pour les nuls C/ex2208.c
@@ -3,25 +3,25 @@
 
 int main()
 {
-    FILE *handle;
-    int scoremax;
+    FILE *handle; /*Cette var est la pour la création*/
+    int scoremax; /*Cette var pour la saisie de données*/
 
-    handle = fopen("scores.dat", "w");
-    if (!handle)
+    handle = fopen("scores.dat", "w"); /*handle avec fopen créer le fichier score.dat en écriture (w)*/
+    if (!handle) /*En cas d'erreur*/
     {
         puts("Erreur fichier!");
         exit(1);
     }
     printf("Indiquez cinq score : ");
-    int boucle = 0;
-    while(boucle < 5)
+    int boucle = 0; /*Initialise la boucle à 0*/
+    while(boucle < 5) /*La boucle permet 5 entrées*/
     {
-        printf("score #%d : ",boucle +1);
-        scanf("%d", &scoremax);
-        boucle++;
+        printf("score #%d : ",boucle +1); /*affiche les saisies dans la boucle avec +1 pour éviter 0*/
+        scanf("%d", &scoremax); /*Prend la saisie*/
+        boucle++; /*Incrémente la boucle*/
     }
-    fwrite(&scoremax, sizeof(int), 5, handle);
-    fclose(handle);
+    fwrite(&scoremax, sizeof(int), 5, handle); /*écrit les 5 entrées de scoremax*/
+    fclose(handle); /*Ferme l'écriture du fichier*/
     puts("Score sauf");
     return(0);
 }
