64c1147e700f9be99e5cd4d571ece74d74267911
bindat <maximerigaud66@gmail.com>
2025-04-26 22:01:18 +0200

    update comment ex2206.c et ex0041.py push

--- Pour les nuls C/ex2206.c
+++ Pour les nuls C/ex2206.c
@@ -3,19 +3,19 @@
 
 int main()
 {
-    FILE *handle;
-    int scoremax;
+    FILE *handle; /*Cette var est la pour la création*/
+    int scoremax; /*Cette var pour la saisie de données*/
 
-    handle = fopen("scores.dat", "w");
+    handle = fopen("scores.dat", "w"); 
     if (!handle)
     {
         puts("Erreur fichier!");
         exit(1);
     }
     printf("Indiquez votre meilleur score : ");
-    scanf("%d", &scoremax);
-    fprintf(handle, "%d", scoremax);
-    fclose(handle);
+    scanf("%d", &scoremax); /*Prend la saisie*/
+    fprintf(handle, "%d", scoremax); /*écrit la donnée dans scoremax*/
+    fclose(handle); /*Ferme l'écriture du fichier*/
     puts("Score sauf");
     return(0);
 }
