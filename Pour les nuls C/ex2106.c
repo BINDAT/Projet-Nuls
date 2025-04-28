ade07fec5c7cf340c8e70df2a5c36f953f021490
bindat <maximerigaud66@gmail.com>
2025-04-28 21:50:20 +0200

    update commentaire de ex2016 et 17.c + MaPremiereClasse.java

--- Pour les nuls C/ex2106.c
+++ Pour les nuls C/ex2106.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include <time.h>
+#include <time.h> /*Permet de gérer les fonction sur le temps*/
 
 int main()
 {
-    time_t tictoc;
-    struct tm *present;
+    time_t tictoc; /*Ici ont prend la date entièrement*/
+    struct tm *present; /*La structure est un pointeur qui permet de choisir des données sur une date*/
 
-    time(&tictoc);
-    present = localtime(&tictoc);
+    time(&tictoc); /*time est en valeur dans mémoire de tictoc*/
+    present = localtime(&tictoc); /*present reprend la valeur de tictoc*/
     printf("A present, nous sommes le %d/%d/%d\n",
         present->tm_mon,
         present->tm_mday,
-        present->tm_year);
+        present->tm_year); /*puis ici ont décortique pour afficher la date.*/
     return(0);
 }
