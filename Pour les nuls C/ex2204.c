f2bddbdbeb1a638016f2cd2d8f78022739ed980a
bindat <maximerigaud66@gmail.com>
2025-04-29 21:01:29 +0200

    update ex2204 et 05.c

--- Pour les nuls C/ex2204.c
+++ Pour les nuls C/ex2204.c
@@ -3,17 +3,17 @@
 
 int main()
 {
-    FILE *fh;
-    char buffer[64];
+    FILE *fh; /*Var type FILE pointée pour trouver les fichier*/
+    char buffer[64]; /*Var char tableau limiter*/
 
     fh=fopen("hello.txt","r"); // Mode lecture
-    if (fh==NULL)
+    if (fh==NULL) /*En cas de pépin*/
     {
         puts("Ouverture du fichier impossible!");
         exit(1);
     }
-    while (fgets(buffer, 64, fh))
+    while (fgets(buffer, 64, fh)) /*Reprend les données de buffer et de fh*/
         printf("%s",buffer);
-    fclose(fh);
+    fclose(fh); /*Ferme le fichier cible de fh*/
     return(0);
 }
--- Pour les nuls C/ex2205.c
+++ Pour les nuls C/ex2205.c
@@ -3,15 +3,15 @@
 
 int main()
 {
-    FILE *fh;
+    FILE *fh; /*Ici une var pointée type FILE pour trouver les fichiers*/
 
     fh=fopen("hello.txt","a"); // Mode ajout
-    if (fh==NULL)
+    if (fh==NULL) /*En cas de pépin*/
     {
         puts("Ouverture du fichier impossible!");
         exit(1);
     }
-    fprintf(fh, "Texte apparu plus tard\n");
-    fclose(fh);
+    fprintf(fh, "Texte apparu plus tard\n"); /*Ajoute ceci dans le fichier*/
+    fclose(fh); /*Ferme le fichier.*/
     return(0);
 }
