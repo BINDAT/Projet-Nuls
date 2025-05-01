4e555485f6f77e997307146ad3b9db6a4521bae7
bindat <maximerigaud66@gmail.com>
2025-05-01 22:44:48 +0200

    update Choses2LaVie .java

---
+++ Pour les nuls Java/annexe code java/emprunt/Choses2LaVie.java
@@ -0,0 +1,12 @@
+/*
+ * Un programme pou lister les bonnes choses dde la vie 
+ * Auteur: Barry Burd, BeginProg@allmycode.com
+ * révisé le 13 octobre de 2016. VF OEN 2021
+ */
+
+public class Choses2LaVie {
+    
+    public static void main(String args[]){
+        System.out.println(("chocolat, argent, sommeil"));
+    }
+}
