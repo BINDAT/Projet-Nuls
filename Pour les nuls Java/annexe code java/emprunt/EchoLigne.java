847cdaef2d5365fe3293e7241734e9c3bf1ab1df
bindat <maximerigaud66@gmail.com>
2025-05-02 16:08:20 +0200

    update de EchoLine.java

---
+++ Pour les nuls Java/annexe code java/emprunt/EchoLigne.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class EchoLigne{
+    public static void main(String args[]){
+        Scanner clavier = new Scanner(System.in);
+
+        System.out.println(clavier.nextLine());
+
+        clavier.close();
+    }
+}
---
+++ Pour les nuls Java/annexe code java/emprunt/ReparerAlternateur.java
@@ -0,0 +1,7 @@
+void ReparerAlternateur() {
+    Conduire(voiture, atelier);
+    Lever(capot);
+    Prendre(cle);
+    Desserrer(courroieAlernateur);
+    // etc.
+}
