45d40a9a5cc331c16212cbf97cc90004c8d70b2f
bindat <maximerigaud66@gmail.com>
2025-05-05 22:49:28 +0200

    update ex0048 à 50.py

---
+++ Pour les nuls Python/ex0049.py
@@ -0,0 +1,12 @@
+import Chapitre_11_Packages
+dir(Chapitre_11_Packages)
+
+['SayGoodbye',
+ 'SayHello',
+ '__builtins__',
+ '__cached__',
+ '__doc__',
+ '__loader__',
+ '__name__',
+ '__package__',
+ '__spec__']
---
+++ Pour les nuls Python/ex0050.py
@@ -0,0 +1,4 @@
+import sys
+del sys.modules["Chapitre_11_Packages"]
+del Chapitre_11_Packages
+dir(Chapitre_11_Packages)
