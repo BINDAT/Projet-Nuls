16e6dc9bae05d57f91a0b9f699589ed8311029d4
bindat <maximerigaud66@gmail.com>
2025-05-07 21:32:36 +0200

    update ex0054.py

---
+++ Pour les nuls Python/ex0054.py
@@ -0,0 +1,5 @@
+print('Bonjour la Terre(Apostrophes simple)!')
+print("Bonjour la Terre(Guillemets)!")
+print("""Ceci est une ligne multiple
+      utilisant trois guillemets,
+      mais vous pourriez également utiliser trois apostrophes simples.""")
