7d2f2ea203134f85984786ce698be554db98bf02
bindat <maximerigaud66@gmail.com>
2025-05-12 20:29:12 +0200

    update SnitSoft

---
+++ Pour les nuls Java/SnitSoft.java
@@ -0,0 +1,12 @@
+class SnitSoft {
+    public static void main(String[] args) {
+        double montant;
+
+        montant = 5.95;
+        montant = montant +25.00;
+
+        System.out.print("Nous allons prélever ");
+        System.out.print(montant);
+        System.out.println(" € sur votre carte bancaire.");
+    }    
+}
