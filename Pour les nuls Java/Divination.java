f982b1d0bdafab664f828d2a0892f3161d2ef675
bindat <maximerigaud66@gmail.com>
2025-05-29 22:19:01 +0200

    update commentaire Divination.java

--- Pour les nuls Java/Divination.java
+++ Pour les nuls Java/Divination.java
@@ -1,13 +1,13 @@
-import java.util.Random;
+import java.util.Random; /*Import de la bibliothèque pour utiliser des fonction liées à l'aléatoire*/
 
-class Divination {
-    public static void main(String[] args) {
-        Random monGenRandom = new Random();
-        int nAleatoire = monGenRandom.nextInt(10) +1;
+class Divination /*Créer une fonction de classe du nom de Divination avec ses instructions*/{
+    public static void main(String[] args) /*créer une fonction static public du nom de main qui prends tout en void*/{
+        Random monGenRandom = new Random(); /*Ici création de la variable monGenRandom qui utilise Random*/
+        int nAleatoire = monGenRandom.nextInt(10) +1; /*Ici création est attribution de nAléatoire avec en valeur monGenRandom*/
 
-        if (nAleatoire == 7) {
-            System.out.println("Vous êtes l'heureux  gagnant !");
+        if (nAleatoire == 7) /*Condition*/{
+            System.out.println("Vous êtes l'heureux  gagnant !"); /*affichage*/
         }
-        System.out.println(nAleatoire);
+        System.out.println(nAleatoire); /*affichage*/
     }    
 }
