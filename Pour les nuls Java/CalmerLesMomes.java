b1ad41ca527e08143fdf64452d7ab1aaf0bb0e90
bindat <maximerigaud66@gmail.com>
2025-05-30 21:05:49 +0200

    update de commentaire pour CalmerLesMomes.java

--- Pour les nuls Java/CalmerLesMomes.java
+++ Pour les nuls Java/CalmerLesMomes.java
@@ -1,17 +1,17 @@
-class CalmerLesMomes {
+class CalmerLesMomes /*class du nom CalmerLesMomes avec ses instructions*/{
     
-    public static void main(String[] args) {
+    public static void main(String[] args) /*appel une fonction public static void du nom de main*/ {
         int nBouleGom;
         int nMome;
-        int nBoulesMome;
+        int nBoulesMome; /*Créer des var int sans attributions de val*/
 
         nBouleGom = 30;
-        nMome = 4;
-        nBoulesMome = nBouleGom / nMome;
+        nMome = 4; /*Attribution de val à la var */
+        nBoulesMome = nBouleGom / nMome; /*Reprend ici 2 var en division pour faire la val de la var*/
 
         System.out.print("Chaque enfant reçois ");
-        System.out.print(nBoulesMome);
-        System.out.println(" boules de gomme");
+        System.out.print(nBoulesMome); /*Affichage de la val de la var*/
+        System.out.println(" boules de gomme"); /*Affichage*/
 
     }
 }
