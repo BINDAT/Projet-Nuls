a33215a9f0d8d24ead5eaff687dcc82df66dc048
bindat <maximerigaud66@gmail.com>
2025-06-03 21:40:09 +0200

    update commentaire de ex0010 et 11.py

--- Pour les nuls Python/ex0010.py
+++ Pour les nuls Python/ex0010.py
@@ -1,8 +1,8 @@
-Test = 255.0
+Test = 255.0 #Réglée en valeur basique
 print("Affection directe : ", Test)
 
-Test = 2.55e2
+Test = 2.55e2 #Réglée en valeur scientifique
 print("Notation scientifique : ", Test)
 
-Test = 2.55e-2
-print("Exposant négatif : ", Test)
+Test = 2.55e-2 #Réglée en valeur scientifique négative
+print("Exposant négatif : ", Test) #Affiche la valeur de Test.
--- Pour les nuls Python/ex0011.py
+++ Pour les nuls Python/ex0011.py
@@ -1,6 +1,6 @@
-print(ord("A"))
+print(ord("A")) #ici affiche 65 car A en unicode est égal à 65
 
-monInt = int("123")
-print(monInt)
+monInt = int("123") #ici la var monInt en égal int avec en val 123
+print(monInt) #affiche la val de la var monInt
 
-print(type(monInt))
+print(type(monInt)) #ici affiche le type de la var monInt
