08075229fd91b08acea973a4af64f5551a9eef77
bindat <maximerigaud66@gmail.com>
2025-06-07 21:46:45 +0200

    update commentaire de ex0018 et 19.py

--- Pour les nuls Python/ex0018.py
+++ Pour les nuls Python/ex0018.py
@@ -1,2 +1,4 @@
-def AddIt(Valeur1, Valeur2):
-    print(Valeur1, " + ", Valeur2, " = ", (Valeur1 + Valeur2))
+def AddIt(Valeur1, Valeur2): #Créer une fonction AddIt avec de variable en paramètre mais sans valeur
+    print(Valeur1, " + ", Valeur2, " = ", (Valeur1 + Valeur2)) #affiche les valeurs des variables
+
+#Code executable en fenêtre interactive par AddIt(Valeur1 = any, Valeur2 = any) ou AddIt(any, any)
--- Pour les nuls Python/ex0019.py
+++ Pour les nuls Python/ex0019.py
@@ -1,4 +1,4 @@
-def Hello4(ArgCount, *VarArgs):
-    print("Vous avez passé ", ArgCount, " argument(s).")
-    for Arg in VarArgs:
-        print(Arg)
+def Hello4(ArgCount, *VarArgs): #Ici marquer le nombre d'argument puis l'argument ainsi Hello4(1,"Coucou")
+    print("Vous avez passé ", ArgCount, " argument(s).") #Affiche le nombre d'argument que vous avez renseignez
+    for Arg in VarArgs: #Boucle autant de fois que renseigner
+        print(Arg) #affiche ce que vous avez écrit
