eea98e2c3d1143167aa1038b56465fa49058abb4
bindat <maximerigaud66@gmail.com>
2025-06-08 21:42:51 +0200

    update commentaire de 0020 et 21.py

--- Pour les nuls Python/ex0020.py
+++ Pour les nuls Python/ex0020.py
@@ -1,3 +1,3 @@
-def DoAdd(Valeur1, Valeur2):
-    return Valeur1 + Valeur2
-print("3 + 4 égale 2 + 5 est", (DoAdd(3, 4) == DoAdd(2, 5)))
+def DoAdd(Valeur1, Valeur2): #créer une fonction avec des variable sans valeur en paramètre de la fonction
+    return Valeur1 + Valeur2 #retourne la Valeur1 et Valeur2
+print("3 + 4 égale 2 + 5 est", (DoAdd(3, 4) == DoAdd(2, 5))) #affiche et ajoute des valeurs aux variable deux fois et fait un comparatif des deux cumul des valeurs
--- Pour les nuls Python/ex0021.py
+++ Pour les nuls Python/ex0021.py
@@ -1,2 +1,2 @@
-Nom = input("Quel est votre nom : ")
-print("Bonjour ", Nom)
+Nom = input("Quel est votre nom : ") #permet une saisit est une affichage du message
+print("Bonjour ", Nom) #affiche la valeur de la variable Nom dans l'affichage
