89325f4ba5af874b29f12485834104d01bf739ca
bindat <maximerigaud66@gmail.com>
2025-06-10 21:26:24 +0200

    update de ex0024 et 25.py

--- Pour les nuls Python/ex0024.py
+++ Pour les nuls Python/ex0024.py
@@ -1,4 +1,4 @@
-TestMe = 6
-if TestMe == 6:
+TestMe = 6 #variable avec valeur de 6
+if TestMe == 6: #condition qui test la valeur de variable si 6 le print est affiché. (Condition True/False)
     print("TestMe est égal à 6 !")
     print("Terminé !")
--- Pour les nuls Python/ex0025.py
+++ Pour les nuls Python/ex0025.py
@@ -1,4 +1,4 @@
-Value = int(input("Tapez un nombre entre 1 et 10:"))
+Value = int(input("Tapez un nombre entre 1 et 10:")) #Variable int avec message qui demande une saisit
 
-if (Value > 0) and (Value <= 10):
+if (Value > 0) and (Value <= 10): #condition qui test la valeur de variable si Value est entre 1 est sous ou égal à 10 le print est affiché. (Condition True/False)
     print("Vous avez tapé : ", Value)
