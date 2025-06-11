e7d25e6c5f20ae398ea2be0dc59f49e79dc1dc80
bindat <maximerigaud66@gmail.com>
2025-06-11 21:31:37 +0200

    upload commentaire de ex0026 et 27.py et fix de 26.py

--- Pour les nuls Python/ex0026.py
+++ Pour les nuls Python/ex0026.py
@@ -1,2 +1,3 @@
-if (nn := len(objet)) > 10:
-    print(f"La liste est trop longue:({n} éléments, <= 10 attendu)")
+if (nn := len(objet)) > 10: #Programmer une variable objet si elle fait plus de dix éléments la condition s'enclenche et affiche le message
+    print(f"La liste est trop longue:({nn} éléments, <= 10 attendu)")
+#Code pour fenêtre interactive
--- Pour les nuls Python/ex0027.py
+++ Pour les nuls Python/ex0027.py
@@ -1,6 +1,6 @@
-Value = int(input("Tapez un nombre entre 1 et 10 : "))
+Value = int(input("Tapez un nombre entre 1 et 10 : ")) #Ici une variable avec un message et une entrée de saisit
 
-if (Value > 0) and (Value <= 10):
+if (Value > 0) and (Value <= 10): #La condition ici prendra pour correcte toute valeur en 1 et 10
     print("Vous avez tapé : ", Value)
 else:
     print("Vous avez tapé une valeur incorrecte !")
