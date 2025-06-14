a6d3f1f50a9e88311aca3ce8fab6e227cd4a23d3
bindat <maximerigaud66@gmail.com>
2025-06-14 21:42:44 +0200

    update de commentaire de ex0032 et 33.py

--- Pour les nuls Python/ex0032.py
+++ Pour les nuls Python/ex0032.py
@@ -1,9 +1,9 @@
-Value = input("Tapez un mot d'au plus 8 caractères : ")
-LetterNum = 1
+Value = input("Tapez un mot d'au plus 8 caractères : ") #Variable avec entrée de saisit et un affichage
+LetterNum = 1 #Créer est initialise la variable avec sa valeur à 1
 
-for Letter in Value:
-    print("La lettre ", LetterNum, " est ", Letter)
-    LetterNum+=1
-    if LetterNum > 8:
+for Letter in Value: #créer une boucle for avec Letter est prend dans Value
+    print("La lettre ", LetterNum, " est ", Letter) #affiche le nombre de lettre avec la lettre
+    LetterNum+=1 #Auto-incrémentation de la variable LetterNum
+    if LetterNum > 8: #Condition si supérieur à 8.
         print("La chaîne est longue !")
         break
--- Pour les nuls Python/ex0033.py
+++ Pour les nuls Python/ex0033.py
@@ -1,7 +1,7 @@
-LetterNum = 1
-for Letter in "Bonjour !":
-    if Letter == "j":
-        continue
-        print("La lettre j rencontrée, traitement interrompu")
-    print("La lettre ", LetterNum, " est ", Letter)
-    LetterNum+=1
+LetterNum = 1 #créer la variable avec une valeur
+for Letter in "Bonjour !": #ici une boucle for avec Letter et en valeur une chaîne de caractère
+    if Letter == "j": #ici Letter prend la lettre j directement
+        continue #saute le prochain
+        print("La lettre j rencontrée, traitement interrompu") #ici sauté
+    print("La lettre ", LetterNum, " est ", Letter) #tout est affiché sauf la lettre j à cause de la condition
+    LetterNum+=1 #auto-incrémentation
