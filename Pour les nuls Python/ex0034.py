f229326007c2fbb47774e29957796c3bd7f4d6a0
bindat <maximerigaud66@gmail.com>
2025-06-15 13:39:40 +0200

    update ex0034 et 35.py de commentaire

--- Pour les nuls Python/ex0034.py
+++ Pour les nuls Python/ex0034.py
@@ -1,7 +1,7 @@
-LetterNum = 1
-for Letter in "Bonjour !":
-    if Letter == "j":
+LetterNum = 1 #créer la variable avec une valeur
+for Letter in "Bonjour !": #ici une boucle for avec Letter et en valeur une chaîne de caractère
+    if Letter == "j": #ici Letter prend la lettre j directement
         pass # Ajouter du code plus ici pour traiter j
         print("La lettre j rencontrée, traitement interrompu")
-    print("La lettre ", LetterNum, " est ", Letter)
-    LetterNum+=1
+    print("La lettre ", LetterNum, " est ", Letter) #tout est affiché y compris le message du à pass
+    LetterNum+=1 #auto-incrémentation
--- Pour les nuls Python/ex0035.py
+++ Pour les nuls Python/ex0035.py
@@ -1,10 +1,10 @@
-Value = input("Tapez moins de 8 caractères : ")
-LetterNum = 1
+Value = input("Tapez moins de 8 caractères : ") #Ici ont créer une variable une variable avec entrée de saisit et message
+LetterNum = 1 #créer une variable avec sa valeur
 
-for Letter in Value:
+for Letter in Value: #ici une boucle for avec Letter et Value
     print ("La lettre ", LetterNum, " est ", Letter)
-    LetterNum+=1
-    if (Letter == "" or Letter == Value[-1]):
+    LetterNum+=1 #auto-incrémentation
+    if (Letter == "" or Letter == Value[-1]): #ici affichage avec un -1 probablement pour évité la saisit de l'entrée
         break
 else:
     print("La chaîne est vide.")
