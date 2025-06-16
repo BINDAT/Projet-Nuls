1e6bf10ea178c4e8c747e1abde9dfa892bc61cf7
bindat <maximerigaud66@gmail.com>
2025-06-16 16:22:44 +0200

    update commentaire de ex0036 et 37.py

--- Pour les nuls Python/ex0036.py
+++ Pour les nuls Python/ex0036.py
@@ -1,4 +1,4 @@
-Sum = 0
-while Sum < 5:
-    print(Sum)
-    Sum+=1
+Sum = 0 #créer une var et sa val
+while Sum < 5: #je reprend ici la var et la conditionne sous 5 dans la boucle while
+    print(Sum) #ici ont affiche la var et sa val actuelle
+    Sum+=1 #ici j'auto-incrémente
--- Pour les nuls Python/ex0037.py
+++ Pour les nuls Python/ex0037.py
@@ -1,13 +1,13 @@
 X = 1
-Y = 1
-print ('{:>4}'.format(''), end='')
-for X in range(1, 11):
-    print('{:>4}'.format(X), end='')
-print()
-for X in range(1,11):
-    print('{:>4}'.format(X), end='')
-    while Y <= 10:
-        print('{:>4}'.format(X * Y), end='')
-        Y +=1
-    print()
-    Y = 1
+Y = 1 #ici je créer les vars et leurs vals
+print ('{:>4}'.format(''), end='') #j'affiche en boucle
+for X in range(1, 11): #ici ont paramètre de 1 à 10 la boucle sur X
+    print('{:>4}'.format(X), end='') #j'affiche le résultat
+print()#j'affiche ici
+for X in range(1,11): #ici je refait une boucle du premier nombre au dernier par X.
+    print('{:>4}'.format(X), end='') #ici j'affiche le résultat
+    while Y <= 10: #ici une boucle en condition sous et ou égale à 10
+        print('{:>4}'.format(X * Y), end='') #ici ont affiche le résultat de X multiplié de Y
+        Y +=1 #j'auto-incrémente la val de la var Y
+    print() #j'affiche ici
+    Y = 1 #je remet la val de la var Y à sa valeur initiale
