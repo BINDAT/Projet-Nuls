35d3739d3a5f300bc31fbaeefa492e2afb7ee92a
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-06-17 20:52:38 +0200

    Update ex0038.py
    commentaire.

--- Pour les nuls Python/ex0038.py
+++ Pour les nuls Python/ex0038.py
@@ -1,9 +1,9 @@
-try:
+try: #ici ont lance fonction try
     Value = int(input("Tapez un nombre entre 1 et 10 :"))
-except ValueError:
+except ValueError: #si faux la condition ValueError ce lance
     print("Vous devez tapez un nombre entre 1 et 10 !")
-else:
-    if (Value > 0) and (Value <= 10):
+else: #ici else est coupée en deux
+    if (Value > 0) and (Value <= 10): #si bon la val sera afficher sans problème.
         print("Vous avez tapé : ", Value)
-    else:
+    else: #sinon si faux cela seras afficher ici.
         print("La valeur que vous avez tapée est incorrecte !")
