57538ea545dd0c3ad5e9cbdb210ad9b6bce76c1b
bindat <maximerigaud66@gmail.com>
2025-06-18 15:09:00 +0200

    update commentaire de ex0040.py

--- Pour les nuls Python/ex0040.py
+++ Pour les nuls Python/ex0040.py
@@ -1,8 +1,8 @@
-import sys
-try:
-    File = open("myfile.txt")
-except IOError as e:
-    print("Erreur lors de l'ouverture du fuchier ! \r\n" + "Numéro de l'erreur : {0}\r\n".format(e.errno)+"Texte de l'erreur : {0}".format(e.strerror))
-else:
+import sys #importe une bibliothèque pour les fonction système
+try: #ici ont lance une fonction try
+    File = open("myfile.txt") #pour le fichier texte
+except IOError as e: #si erreur cela seras afficher ici
+    print("Erreur lors de l'ouverture du fichier ! \r\n" + "Numéro de l'erreur : {0}\r\n".format(e.errno)+"Texte de l'erreur : {0}".format(e.strerror))
+else: #sinon les instructions seront suivis ici
     print("Le fichier a bien été ouvert.")
-    File.close()
+    File.close() #est le fichier seras fermer la.
