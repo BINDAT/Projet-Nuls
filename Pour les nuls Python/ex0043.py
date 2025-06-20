2ea10a297257d367b4abb694d4e6b0ea9c727ac1
bindat <maximerigaud66@gmail.com>
2025-06-20 21:09:01 +0200

    update commentaire de ex0043.py

--- Pour les nuls Python/ex0043.py
+++ Pour les nuls Python/ex0043.py
@@ -1,11 +1,11 @@
-try:
-    Value = int(input("Tapez un nombre entre 1 et 10 : "))
-except ValueError:
+try: #ici je me sert de try
+    Value = int(input("Tapez un nombre entre 1 et 10 : ")) #variable int avec entrée de saisie et message
+except ValueError: #erreur de valeur
     print("Vous devez taper un nombre entre 1 et 10 !")
-except KeyboardInterrupt:
+except KeyboardInterrupt: #erreur par contrôle + c
     print("Vous avez appuyé sur CRTL + C!")
-else:
-    if (Value > 0) and (Value <=10):
+else: #si la valeur est bien un nombre
+    if (Value > 0) and (Value <=10): #si bien entre 1 et 10
         print("Vous avez tapé : ", Value)
-    else:
+    else: #si est en dehors de 1 et 10
         print("La valeur que vous avez tapée est incorrecte !")
