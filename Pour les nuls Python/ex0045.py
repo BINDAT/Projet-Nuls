e6b0ab9850cab7b92ed14b5e10dfc0ea1c7980ed
bindat <maximerigaud66@gmail.com>
2025-06-22 15:44:13 +0200

    update debug et commentaire de ex0045.py et update de annexe6

--- Pour les nuls Python/ex0045.py
+++ Pour les nuls Python/ex0045.py
@@ -1,21 +1,21 @@
-TryAgain = True
-while TryAgain:
-    try:
-        Value = int(input("Tapez un nombre entier : "))
-    except ValueError:
+TryAgain = True #Var booléen contient True
+while TryAgain: #execute TryAgain en boucle while
+    try: #fonction try
+        Value = int(input("Tapez un nombre entier : ")) #affiche un message et capture la saisit clavier
+    except ValueError: #en cas d'erreur
         print("Vous devez taper un nombre entier !")
-        try:
-            DoOver = input("<essayez à nouveau (o/n) ?")
-        except:
+        try: #try pour le choix
+            DoOver = input("<essayez à nouveau (o/n) ?") #var avec capture de saisit
+        except: #si ne veut pas continuer
             print("Ok à la prochaine fois !")
-            TryAgain = False
-        else:
-            if (str.upper(DoOver) == "N"):
-                TryAgain = False
-    except KeyboardInterrupt:
-        print("Vous avez appuyez sur Ctrl+C !")
+            TryAgain = False #stop ici
+        else: 
+            if (str.upper(DoOver) == "N"): #prend la val ici et la met en upper
+                TryAgain = False #stop ici
+    except KeyboardInterrupt: #en cas d'interruption par clavier
+        print("Vous avez appuyez sur Ctrl+C ! ")
         print("A la prochaine fois !")
-    TryAgain = False
-else:
-    print(Value)
-    TryAgain = False
+        TryAgain = False #stop ici
+    else:
+        print(Value)
+        TryAgain = False #stop ici
