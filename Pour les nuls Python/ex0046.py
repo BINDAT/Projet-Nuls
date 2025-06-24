03027f5ae58769ca25d1ece5c9756396d498420e
bindat <maximerigaud66@gmail.com>
2025-06-24 21:14:58 +0200

    update commentaire de ex0046 et 47.py

--- Pour les nuls Python/ex0046.py
+++ Pour les nuls Python/ex0046.py
@@ -1,11 +1,11 @@
-import sys
-try:
-    raise ValueError
-    print("Exception levée")
-except ValueError:
+import sys #importe la bibliothèque sys pour les fonctionnalités système
+try: #ici je lance un try
+    raise ValueError #ici raise fait sauter le message
+    print("Exception levée") #message sauté
+except ValueError: #ici except et actif
     print("Exception ValueError !")
     sys.exit()
-finally:
+finally: #Et finally est actif aussi
     print("Prenez soin des détails de dernière minute.")
 
-print("Ce code ne sera jamais exécuté")
+print("Ce code ne sera jamais exécuté") #ici le message n'est pas affiché
--- Pour les nuls Python/ex0047.py
+++ Pour les nuls Python/ex0047.py
@@ -1,11 +1,11 @@
-import sys
-try:
+import sys #importe la bibliothèque sys pour les fonctionnalités système
+try: #ici je lance un try
     ##raise ValueError
-    print("Exception levée")
-except ValueError:
+    print("Exception levée") #ici le message est affiché
+except ValueError: #ici except ne sert pas.
     print("Exception ValueError !")
     sys.exit()
-finally:
+finally: #ici finally est actif
     print("Prenez soin des détails de dernière minute.")
 
-print("Ce code ne sera jamais exécuté")
+print("Ce code ne sera jamais exécuté")#est ce message est affiché.
