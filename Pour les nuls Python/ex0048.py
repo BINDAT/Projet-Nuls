164316d91155bdf54275797ae909a3c24493bd9f
bindat <maximerigaud66@gmail.com>
2025-06-25 21:19:59 +0200

    update commentaire de ex0048 et 49.py

--- Pour les nuls Python/ex0048.py
+++ Pour les nuls Python/ex0048.py
@@ -1,7 +1,8 @@
-def SayHello(Name):
-    print("Bonjour ", Name)
+def SayHello(Name): #créer une fonction SayHello avec en paramètre une var
+    print("Bonjour ", Name) #affiche un message avec la valeur de la var
     return
 
 def SayGoodbye(Name):
     print("Au revoir ", Name)
     return
+#code en fenêtre interactive
--- Pour les nuls Python/ex0049.py
+++ Pour les nuls Python/ex0049.py
@@ -1,5 +1,5 @@
-import Chapitre_11_Packages
-dir(Chapitre_11_Packages)
+import Chapitre_11_Packages #importe en paramètre directement un dossier
+dir(Chapitre_11_Packages) #utilise le dossier
 
 ['SayGoodbye',
  'SayHello',
@@ -9,4 +9,4 @@ dir(Chapitre_11_Packages)
  '__loader__',
  '__name__',
  '__package__',
- '__spec__']
+ '__spec__']#attribut spéciaux pour faire diverse chose comme afficher les fonctions accélérer l'import une doc le nom du module un pack et informations 
