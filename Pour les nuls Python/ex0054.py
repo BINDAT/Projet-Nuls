dc8d1978e57be57b5fc46ef648fbd564d8fa84f2
bindat <maximerigaud66@gmail.com>
2025-06-28 21:20:16 +0200

    update commentaire de ex0054 et 55.py

--- Pour les nuls Python/ex0054.py
+++ Pour les nuls Python/ex0054.py
@@ -1,5 +1,5 @@
-print('Bonjour la Terre(Apostrophes simple)!')
+print('Bonjour la Terre(Apostrophes simple)!')#affichage simple
 print("Bonjour la Terre(Guillemets)!")
 print("""Ceci est une ligne multiple
       utilisant trois guillemets,
-      mais vous pourriez également utiliser trois apostrophes simples.""")
+      mais vous pourriez également utiliser trois apostrophes simples.""")#affichage sur plusieurs lignes
--- Pour les nuls Python/ex0055.py
+++ Pour les nuls Python/ex0055.py
@@ -1,6 +1,6 @@
 print("Une partie de ce texte\r\nse trouve sur la ligne " \
-"suivante.")
-print("Ceci est un A avec un accent grave : \xC0.")
-print("Ceci est un caractère dessiné : \u2562.")
-print("Et voici un retour de chariot : \266.")
-print("Ceci et le signe de la division : \xF7.")
+"suivante.")#affichage sur plusieurs lignes
+print("Ceci est un A avec un accent grave : \xC0.") #affiche un accent grave
+print("Ceci est un caractère dessiné : \u2562.") #affiche un caractère dessiné
+print("Et voici un retour de chariot : \266.") #affiche un retour chariot
+print("Ceci et le signe de la division : \xF7.") #et la affiche le signe division
