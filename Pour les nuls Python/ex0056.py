072044d59d3d0933729b997fb1f6edff99ec8e69
bindat <maximerigaud66@gmail.com>
2025-06-29 16:26:39 +0200

    update commentaire ex56 et 57.py

--- Pour les nuls Python/ex0056.py
+++ Pour les nuls Python/ex0056.py
@@ -1,2 +1,2 @@
-MyString = "Bonjour le monde"
-print(MyString[0])
+MyString = "Bonjour le monde" #variable avec valeur chaîne de caractère
+print(MyString[0]) #affichage de la val de var comme un tableau sur l'indice 0 du coup affiche la première lettre
--- Pour les nuls Python/ex0057.py
+++ Pour les nuls Python/ex0057.py
@@ -1,9 +1,9 @@
 String1 = "Bonjour la Terre"
-String2 = "Ici Python"
-print(String1[0])
-print(String1[0:5])
-print(String1[:5])
-print(String1[6:])
-String3 = String1[:6] + String2[:6]
-print(String3)
-print(String2[:7]*5)
+String2 = "Ici Python" #Variables est valeurs en chaîne de caractères.
+print(String1[0])#affiche la première lettre
+print(String1[0:5]) #affiche de la première à la cinquième lettres
+print(String1[:5]) #affiche de la première à la cinquième lettres
+print(String1[6:]) #affiche à partir de la sixième lettre
+String3 = String1[:6] + String2[:6] #addition les valeurs
+print(String3) #affiche les 6 lettres de String1 et String2
+print(String2[:7]*5) #affiche les 7 lettres de String2 répétées 5 fois
