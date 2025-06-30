c4498a9bc331c73bc4b87933ac96916e100e0977
bindat <maximerigaud66@gmail.com>
2025-06-30 21:18:54 +0200

    update de commentaire de ex0058.py

--- Pour les nuls Python/ex0058.py
+++ Pour les nuls Python/ex0058.py
@@ -1,10 +1,10 @@
 MyString = " Bonjour la Terre "
-print(MyString.upper())
-print(MyString.strip())
-print(MyString.center(21,"*"))
-print(MyString.strip().center(21,"*"))
-print(MyString.isdigit())
-print(MyString.istitle())
-print(max(MyString))
-print(MyString.split())
-print(MyString.split()[0])
+print(MyString.upper()) #affiche tout en majuscule
+print(MyString.strip()) #retire les espaces superflus
+print(MyString.center(21,"*"))#affiche en centre avec des * et un nombre limite
+print(MyString.strip().center(21,"*")) #retire les espace superflus centre et ajoute des * dans un nombre limite
+print(MyString.isdigit()) #questionne le fait si c'est une suite numérique
+print(MyString.istitle()) #questionne le fait si c'est un titre
+print(max(MyString)) #affiche la lettre avec la plus forte valeur 
+print(MyString.split()) #affiche comme un tableau avec des séparation
+print(MyString.split()[0]) #affiche comme un tableau mais une seule val ici un mot bonjour
