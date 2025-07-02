4c93c2841bbc59799afd9197ee72df48aedaceed
bindat <maximerigaud66@gmail.com>
2025-07-02 21:10:54 +0200

    update commentaire ex0060.py

--- Pour les nuls Python/ex0060.py
+++ Pour les nuls Python/ex0060.py
@@ -1,3 +1,3 @@
-print("%c est la minuscule de %c" %('a', 65))
-print("%s sont %s" %("Les violets", "des bleus"))
-print("%d est égal à %#x en hexadécimal" %(60, 60))
+print("%c est la minuscule de %c" %('a', 65)) #affiche un A avec une manipulation avec valeur
+print("%s sont %s" %("Les violets", "des bleus")) #affiche les bout de textes dans les endroits cibles
+print("%d est égal à %#x en hexadécimal" %(60, 60)) #affiche la val de 60 et sont hexadécimal
