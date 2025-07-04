6c364a8c158020bf12ca0b3592fd69ae19462c68
bindat <maximerigaud66@gmail.com>
2025-07-04 21:09:41 +0200

    update ex0062.py dde commentaire

--- Pour les nuls Python/ex0062.py
+++ Pour les nuls Python/ex0062.py
@@ -7,10 +7,10 @@ Employes = {
     'Samuel': {
         'Fonction': "Programmeur",
         'Nom': "Samuel",
-        'Departement': "DSI"
-    }
-}
+        'Departement': "DSI" 
+    }#ici ont retrouve les données concernent Samuel la variable en '' et sa valeur en "" après : instructions terminée par ,  
+}#fonction servant de tableau de données
 
-for Item in Employes:
+for Item in Employes: #boucle servant à afficher le tableau
     print("%(Nom)s occupe la fonction de %(Fonction)s dans le service " \
         "%(Departement)s." %Employes[Item])
