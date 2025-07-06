c6dadbd2c75c919996526641709022c494f822e0
bindat <maximerigaud66@gmail.com>
2025-07-06 16:40:44 +0200

    update de ex0064.py commentaire

--- Pour les nuls Python/ex0064.py
+++ Pour les nuls Python/ex0064.py
@@ -1,6 +1,6 @@
 Valeur1 = 22
-Valeur2 = 33
-print(f"Valeur1 a une valeur de {Valeur1}.")
-print(f"{Valeur1} + {Valeur2} = {Valeur1 + Valeur2}.")
-print(f"Cette valeur : {str(Valeur1).center(8)} est centrée.")
-print(f"Valeur1 a une valeur hexadécimale de {Valeur1:x}.")
+Valeur2 = 33 #variable avec valeurs numériques
+print(f"Valeur1 a une valeur de {Valeur1}.") #affichage de la valeur de la variable Valeur1 en message formaté print(f:"{Valeur1}")
+print(f"{Valeur1} + {Valeur2} = {Valeur1 + Valeur2}.") #affichage des vals additionner des vars.
+print(f"Cette valeur : {str(Valeur1).center(8)} est centrée.") #affichage centré sur 8 caractère de la val de la var
+print(f"Valeur1 a une valeur hexadécimale de {Valeur1:x}.") #ici affichage de la val de ma var en hexadécimal
