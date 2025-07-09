f43fe7510131b6d1396bc7600b9bf2ce92a39f32
bindat <maximerigaud66@gmail.com>
2025-07-09 20:54:09 +0200

    update commentaire de 66 & 67.py

--- Pour les nuls Python/ex0066.py
+++ Pour les nuls Python/ex0066.py
@@ -1,2 +1,2 @@
-List1 = ["Un", 1, "Deux", True]
-print(List1)
+List1 = ["Un", 1, "Deux", True] #Créer un tableau avec plusieurs éléments
+print(List1) #affiche les valeurs de la variable
--- Pour les nuls Python/ex0067.py
+++ Pour les nuls Python/ex0067.py
@@ -1,3 +1,3 @@
-list1 = [0, 1, 2, 3, 4, 5]
-for Item in list1:
-    print(Item)
+list1 = [0, 1, 2, 3, 4, 5] #créer un tableau avec des valeurs numériques
+for Item in list1: #fait une boucle de nom Item qui prends les valeurs de la var list1
+    print(Item) #affiche les résultats de la boucle
