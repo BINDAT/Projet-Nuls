dfda9160769240bb8ace69d27dd9e3d2344ce565
bindat <maximerigaud66@gmail.com>
2025-07-10 21:25:18 +0200

    update ex0068.py

---
+++ Pour les nuls Python/ex0068.py
@@ -0,0 +1,18 @@
+List2 =[]
+len(List2)
+
+List2.append(1)
+len(List2)
+
+List2.insert(0, 2)
+List2
+
+List3 = List2.copy()
+List2.extend(List2)
+List2
+
+List2.pop()
+
+List2.remove(1)
+List2.clear()
+len(List2)
