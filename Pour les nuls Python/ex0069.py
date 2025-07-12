78a10cff4435c17ff038a91e14453c149efd4907
bindat <maximerigaud66@gmail.com>
2025-07-12 21:43:47 +0200

    update commentaire de ex0069.py

--- Pour les nuls Python/ex0069.py
+++ Pour les nuls Python/ex0069.py
@@ -1,8 +1,8 @@
-Colors = ["Rouge", "Orange", "Jaune", "Vert", "Bleu"]
-ColorsSelect = ""
-while str.upper(ColorsSelect) != "QUITTER":
-    ColorsSelect = input("Sélectionnez  le nom d'une couleur : ")
-    if (Colors.count(ColorsSelect) >= 1):
+Colors = ["Rouge", "Orange", "Jaune", "Vert", "Bleu"] #Var tableau contenant des chaînes de caractères
+ColorsSelect = "" #Var vide pour sélectionner contenant juste une val chaîne vide
+while str.upper(ColorsSelect) != "QUITTER": #boucle while qui cherche les vals dans ColorsSelect si sauf si quitte
+    ColorsSelect = input("Sélectionnez  le nom d'une couleur : ") #input dans ColorsSelect qui contient en mémoire la saisie
+    if (Colors.count(ColorsSelect) >= 1): #cherche dans le tableau Colors avec la mémoire si un éléments est contenu
         print("La couleur existe dans la liste !")
-    elif (str.upper(ColorsSelect) != "QUITTER"):
+    elif (str.upper(ColorsSelect) != "QUITTER"): #si ne correspond pas à un élément est dans un tableau
         print("La liste ne contient pas cette couleur.")
