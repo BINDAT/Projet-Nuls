cf0adb0a3cecc9e9e0746fe5c5441249f4d70904
bindat <maximerigaud66@gmail.com>
2025-07-15 22:16:19 +0200

    update ex0071.py de commentaire

--- Pour les nuls Python/ex0071.py
+++ Pour les nuls Python/ex0071.py
@@ -1,8 +1,8 @@
-Colors = ["Rouge", "Orange", "Jaune", "Vert", "Bleu"]
-print(*Colors, sep='\n')
+Colors = ["Rouge", "Orange", "Jaune", "Vert", "Bleu"] #var tableau de val
+print(*Colors, sep='\n')#affichage en nouvelle ligne de chaque val de la var
 
-for Item in Colors: print(Item.rjust(8),sep='\n')
+for Item in Colors: print(Item.rjust(8),sep='\n') #boucle for de nom Item en paramètre avec la var Colors et un affichage
 
-print('\n'.join(Colors))
+print('\n'.join(Colors)) #un affichage avec un saut de ligne sur chaque val de la var Colors en paramètre de join
 
-print('Premier : {0}\nDeuxième : {1}'.format(*Colors))
+print('Premier : {0}\nDeuxième : {1}'.format(*Colors)) #affichage de vals ciblées de la var Color en pointée avec un saut de ligne
