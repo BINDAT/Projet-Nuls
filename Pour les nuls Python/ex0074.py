2f979fc868c8c2daaa399693bf63f92c2ee5cccb
bindat <maximerigaud66@gmail.com>
2025-07-21 21:02:55 +0200

    ajout de code de ex0074.py

--- Pour les nuls Python/ex0074.py
+++ Pour les nuls Python/ex0074.py
@@ -4,3 +4,9 @@ MyTuple #appel la var
 MyTuple = MyTuple.__add__(("Jaune",
                            ("Orange", "Noir"))) #ajoute un élément et sous élément à une liste
 MyTuple[4] #Montre la var sur sont cinquième élément
+
+MyTuple[5]
+
+type(MyTuple[5]) == tuple
+
+MyTuple[5][0]
