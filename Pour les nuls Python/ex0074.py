8e90e17be91e3ad3ef7b51104dc4c19381a199a4
bindat <maximerigaud66@gmail.com>
2025-07-22 21:28:18 +0200

    update commentaire de ex0074.py

--- Pour les nuls Python/ex0074.py
+++ Pour les nuls Python/ex0074.py
@@ -3,10 +3,10 @@ MyTuple #appel la var
 
 MyTuple = MyTuple.__add__(("Jaune",
                            ("Orange", "Noir"))) #ajoute un élément et sous élément à une liste
-MyTuple[4] #Montre la var sur sont cinquième élément
+MyTuple[4] #Montre la val sur sont cinquième élément
 
-MyTuple[5]
+MyTuple[5] #montre la val sur le sixième élément (ici les sous éléments)
 
-type(MyTuple[5]) == tuple
+type(MyTuple[5]) == tuple #ici ont regarde si un sixième élément est stocké
 
-MyTuple[5][0]
+MyTuple[5][0] #montre spécifique le sixième élément sous sont premier sous élément 
