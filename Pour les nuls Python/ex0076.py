11c1b1b4445fb7bbbce7c9aa439d770a0ba29a0d
bindat <maximerigaud66@gmail.com>
2025-07-26 21:24:17 +0200

    update commentaire ex0076.py

--- Pour les nuls Python/ex0076.py
+++ Pour les nuls Python/ex0076.py
@@ -1,18 +1,18 @@
-for Item in Colors.keys():
+for Item in Colors.keys(): #boucle Item avec en lien Colors keys pour rechercher dans un tableau
     print("{0} aime la couleur {1}."
-          .format(Item, Colors[Item]))
+          .format(Item, Colors[Item])) #affichage ciblé sur des éléments choisi et répété en boucle jusqu'à atteindre le dernier élément
 
-Colors["Sarah"] = "Violet"
-Colors.update({"Harry": "Orange"})
+Colors["Sarah"] = "Violet" #fait une update pour changer une donnée ici la couleur
+Colors.update({"Harry": "Orange"}) #fait une update dans le tableau pour ajouter des données
 
 for name, color in Colors.items():
     print("{0} aime la couleur {1}."
           .format(name, color))
 
-del Colors["Sam"]
+del Colors["Sam"] #ici ont supprime des données du tableau
 for name, color in Colors.items():
     print("{0} aime la couleur {1}."
-          .format(name, color))
+          .format(name, color)) #ont affiche le tout
 
-Colors.clear()
-len(Colors)
+Colors.clear() #ici ont efface
+len(Colors) #est ont compte 0 élément ici
