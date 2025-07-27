2d8b4cb796c39826e5cabe6e01d77d8fdbb06bb8
bindat <maximerigaud66@gmail.com>
2025-07-27 21:11:30 +0200

    update commentaire annexe8.py

--- Pour les nuls Python/annexe code python/annexe8.py
+++ Pour les nuls Python/annexe code python/annexe8.py
@@ -4,24 +4,24 @@ Colors #appel de la fonction
 
 for Item in Colors.keys():
     print("{0} aime la couleur {1}."
-          .format(Item, Colors[Item]))
-print(len(Colors))
+          .format(Item, Colors[Item])) #affiche les éléments du tableau en boucle
+print(len(Colors)) #ici ont vois le nombres d'item dans le tableau
 
-Colors["Sarah"] = "Violet"
-Colors.update({"Harry": "Orange"})
+Colors["Sarah"] = "Violet" #ici ont modifie une val
+Colors.update({"Harry": "Orange"}) #la ont rajoute une val
 print(Colors.update == Colors) #A voir
-print(len(Colors))
+print(len(Colors)) #ont recompte les éléments
 
 for name, color in Colors.items():
     print("{0} aime la couleur {1}."
-          .format(name, color))
-print(len(Colors))
+          .format(name, color)) #affiche les éléments du tableau à nouveau
+print(len(Colors)) #ont compte les éléments à nouveau
 
-del Colors["Sam"]
+del Colors["Sam"] #ont supprime ici une val
 for name, color in Colors.items():
     print("{0} aime la couleur {1}."
-          .format(name, color))
-print(len(Colors))
+          .format(name, color)) #affiche les élément encore
+print(len(Colors)) #recompte encore les éléments du tableau
 
-Colors.clear()
-len(Colors)
+Colors.clear() #ont remet le compte à 0
+len(Colors) #ici ont affiche le compte ré-initialiser
