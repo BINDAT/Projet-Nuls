72a82a4a6c6c9a66addc761b0b54283d6fece228
bindat <maximerigaud66@gmail.com>
2025-07-28 21:18:18 +0200

    update code ex0077.py

---
+++ Pour les nuls Python/ex0077.py
@@ -0,0 +1,9 @@
+MonEntreprise = {"Propriétaire": "Moi",
+                 "Comptabilité": {151: "Martha",
+                                  152: "Jean"},
+                "Informatique": {251: "Annie",
+                                 252: "Albert"},
+                "Logistique": {351: "Gilles",
+                               352: "Sylvie"}
+                }
+MonEntreprise
