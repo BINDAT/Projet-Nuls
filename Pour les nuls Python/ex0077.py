ef2c6ba1bf49168dbf0670489d327f391788ac0b
bindat <maximerigaud66@gmail.com>
2025-07-30 22:12:35 +0200

    update commentaire ex0077.py

--- Pour les nuls Python/ex0077.py
+++ Pour les nuls Python/ex0077.py
@@ -5,15 +5,15 @@ MonEntreprise = {"Propriétaire": "Moi",
                                  252: "Albert"},
                 "Logistique": {351: "Gilles",
                                352: "Sylvie"}
-                }
+                } #fonction tableau de val de chaîne de caractère et numérique
 MonEntreprise
 
-def DisplayDict(Dict):
-    for key, value in Dict.items():
-        if type(value) == dict:
-            print(f"Le service {key} est composé de :")
-            DisplayDict(value)
+def DisplayDict(Dict): #constance avec en paramètre la var Dict
+    for key, value in Dict.items(): #boucle for avec en valeur key et value qui reprend la var Dict en items
+        if type(value) == dict: #ici ont reprend les valeurs de value avec un dictionnaire de créer.
+            print(f"Le service {key} est composé de :") #ici ont reprend {key} pour afficher le service
+            DisplayDict(value) #ici ont reprend les valeurs de value
         else:
-            print(f"{value} est {key}")
+            print(f"{value} est {key}") #et ici ont affiche le nom et le numéro de service
 
-DisplayDict(MonEntreprise)
+DisplayDict(MonEntreprise) #la ont appelle la fonction complète
