6c6ea88f7b2f76a13de93c2cbe3f1c7c26ae7748
bindat <maximerigaud66@gmail.com>
2025-08-02 21:11:03 +0200

    update commentaire de ex0078.py

--- Pour les nuls Python/ex0078.py
+++ Pour les nuls Python/ex0078.py
@@ -5,7 +5,7 @@ def PrintRouge():
 def PrintOrange():   
     print("Vous avez choisi orange ! \r\n")
 def PrintJaune():
-    print("Vous avez choisi jaune ! \r\n")
+    print("Vous avez choisi jaune ! \r\n") #Ici ont a défini toute les fonctions et leurs instructions
 
 ColorSelect = {
     0: PrintBleu,
@@ -13,15 +13,16 @@ ColorSelect = {
     2: PrintOrange,
     3: PrintJaune
     
-}
+} #La ont fait une sorte de tableau d'appel des fonctions auquel ont attribue une valeur numérique
 
-Selection = 0
-while (Selection != 4):
+Selection = 0 #Ici ont prépare la variable avec une valeur de base
+while (Selection != 4): #La ont fait une boucle while dans que 4 n'est pas saisit elle continue à l'infinie
     print("0. Bleu")
     print("1. Rouge")
     print("2. Orange")
     print("3. Jaune")
-    print("4. Quitter")
-    Selection = int(input("Sélectionnez une option de couleur : "))
-    if (Selection >= 0) and (Selection <4):
-        ColorSelect[Selection]()
+    print("4. Quitter") #Ici les instructions d'affichages
+    Selection = int(input("Sélectionnez une option de couleur : ")) #la variable reprend la saisit clavier pour l'enregistrer
+    if (Selection >= 0) and (Selection <4): #Ici dans que la variable est égale ou supérieur à 0 et inférieur à 4
+        ColorSelect[Selection]() #La valeur de Selection et reprise dans ColorSelect qui lui vas 
+        #se charger de reprend la valeur numérique attribuée à la bonne fonction
