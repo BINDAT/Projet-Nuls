9ec46a77f145d1ef66317520161962170e0d0a97
bindat <maximerigaud66@gmail.com>
2025-08-04 21:49:38 +0200

    update commentaire ex0079.py

--- Pour les nuls Python/ex0079.py
+++ Pour les nuls Python/ex0079.py
@@ -1,13 +1,13 @@
-MyStack = []
-StackSize = 3
-def DisplayStack():
+MyStack = [] #var tableau vide
+StackSize = 3 #var avec val elle servira de limite
+def DisplayStack(): #ici une fonction d'affichage
     print("État courant de la pile :")
-    for Item in MyStack:
-        print(Item)
-def Push(Value):
-    if len(MyStack) < StackSize:
-        MyStack.append(Value)
-        print(f"{Value} ajouté")
+    for Item in MyStack: #boucle Items qui prend les vals du tableau
+        print(Item) #affiche les vals
+def Push(Value): #function Push qui prendras Value en paramètre
+    if len(MyStack) < StackSize: #ici la boucle se termine ou bout de 3 tours len compte
+        MyStack.append(Value) #reprend le dernier objet
+        print(f"{Value} ajouté") #affiche toutes les vals
     else:
         print("La pile est pleine !")
 def Pop():
