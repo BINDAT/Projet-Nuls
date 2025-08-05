f8dca34bf96db0db84a268d690f5abe4f0db523a
bindat <maximerigaud66@gmail.com>
2025-08-05 20:54:25 +0200

    update commentaire ex0079.py

--- Pour les nuls Python/ex0079.py
+++ Pour les nuls Python/ex0079.py
@@ -10,21 +10,21 @@ def Push(Value): #function Push qui prendras Value en paramètre
         print(f"{Value} ajouté") #affiche toutes les vals
     else:
         print("La pile est pleine !")
-def Pop():
-    if len(MyStack) > 0:
-        Removed = MyStack.pop()
+def Pop(): #fonction Pop qui servira à la suppression
+    if len(MyStack) > 0: #compte les éléments de MyStack pour savoir si la pile est au dessus de 0 
+        Removed = MyStack.pop() #supprime ici le dernier élément
         print(f"{Removed} supprimé de la pile")
     else:
         print("La pile est vide.")
-Push(1)
-Push(2)
-Push(3)
+Push(1) #ici ont créer la valeur 1
+Push(2) #la 2
+Push(3) #et la 3 pour la def Push
+DisplayStack() #affichage
+input("Appuyez sur une touche quand vous êtes prêt...")
+Push(4) #et la 4 pour la def Push
 DisplayStack()
 input("Appuyez sur une touche quand vous êtes prêt...")
-Push(4)
-DisplayStack()
-input("Appuyez sur une touche quand vous êtes prêt...")
-Pop()
+Pop() #pour effacer la dernière val
 DisplayStack()
 input("Appuyez sur une touche quand vous êtes prêt...")
 Pop()
