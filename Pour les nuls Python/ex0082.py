bc0986246ec23d2e4ff362db6b445efe35659d7c
bindat <maximerigaud66@gmail.com>
2025-08-10 21:06:59 +0200

    update commentaire ex0082.py

--- Pour les nuls Python/ex0082.py
+++ Pour les nuls Python/ex0082.py
@@ -1,6 +1,6 @@
-class MyClass:
-    MyVar = 0
+class MyClass: #Création d'une classe MyClass
+    MyVar = 0 #Variable MyVar initialisée à 0
 
-MyInstance = MyClass()
+MyInstance = MyClass() #Création d'une instance de MyClass
 
-MyInstance.MyVar
+MyInstance.MyVar #Accès à la variable MyVar de l'instance MyInstance
