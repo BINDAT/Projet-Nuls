7a09e7dee3cf1171286a4ca4e9c3376fd42de65e
bindat <maximerigaud66@gmail.com>
2025-08-13 21:27:21 +0200

    upload ex0084.py

---
+++ Pour les nuls Python/ex0084.py
@@ -0,0 +1,5 @@
+class MyClass: # Définition de la classe
+    def SayHello(): # Méthode de la classe
+        print("Bonjour le monde !")
+
+MyClass.SayHello() # Appel de la méthode SayHello de la classe MyClass
