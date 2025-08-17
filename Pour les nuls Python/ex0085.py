af10f5d0e772c2014f39a3a13b98c61abe889a16
bindat <maximerigaud66@gmail.com>
2025-08-17 22:17:14 +0200

    upload code ex0085.py

---
+++ Pour les nuls Python/ex0085.py
@@ -0,0 +1,6 @@
+class MyClass: # Définition de la classe
+    def SayHello(self): # Méthode de la classe
+        print("Bonjour le monde !")
+
+MyInstance = MyClass() # Création d'une instance de MyClass
+MyInstance.SayHello() # Appel de la méthode SayHello
