313251f3b4d0bc8e81b13dbfc4a5a68a5d589992
bindat <maximerigaud66@gmail.com>
2025-08-21 15:56:38 +0200

    update commentaire ex0088&89.py

--- Pour les nuls Python/ex0088.py
+++ Pour les nuls Python/ex0088.py
@@ -1,12 +1,12 @@
-class MyClass:
-    def PrintList1(*args):
-        for Count, Item in enumerate(args):
-            print("{0}. {1}".format(Count, Item))
-    
-    def PrintList2(**kwargs):
-        for Name, Value in kwargs.items():
-            print("{0} aime {1}".format(Name, Value))
-        
-MyClass.PrintList1("Rouge", "Bleu", "Vert")
+class MyClass: #Cette classe contient des méthodes pour afficher des listes
+    def PrintList1(*args): #Cette méthode affiche une liste d'éléments
+        for Count, Item in enumerate(args): #Cette opération parcourt la liste des éléments
+            print("{0}. {1}".format(Count, Item)) #Cette opération affiche l'élément courant avec son index
+
+    def PrintList2(**kwargs): #Cette méthode affiche une liste d'éléments avec des noms
+        for Name, Value in kwargs.items(): #Cette opération parcourt la liste des éléments
+            print("{0} aime {1}".format(Name, Value)) #Cette opération affiche l'élément courant avec son nom
+
+MyClass.PrintList1("Rouge", "Bleu", "Vert") #Cette opération appelle la méthode PrintList1 avec trois arguments
 MyClass.PrintList2(Georges="Rouge", Annie="Bleu",
-                   Sarah="Vert")
+                   Sarah="Vert") #Cette opération appelle la méthode PrintList2 avec trois arguments
--- Pour les nuls Python/ex0089.py
+++ Pour les nuls Python/ex0089.py
@@ -1,22 +1,22 @@
-class MyClass:
-    def __init__(self, *args):
-        self.Input = args
-    
-    def __add__(self, Other):
-        Output = MyClass()
-        Output.Input = self.Input + Other.Input
-        return Output
-    
-    def __str__(self):
-        Output = ""
-        for Item in self.Input:
-            Output += Item
-            Output += " "
-        return Output
+class MyClass: #Cette classe gère une liste d'éléments
+    def __init__(self, *args): #Cette méthode initialise la classe avec une liste d'éléments
+        self.Input = args #Cette variable stocke la liste des éléments
 
-Value1 = MyClass("Rouge", "Vert", "Bleu")
-Value2 = MyClass("Jaune","Pourpre","Cyan")
-Value3 = Value1 + Value2
-    
+    def __add__(self, Other): #Cette méthode permet d'ajouter deux objets de la classe MyClass
+        Output = MyClass() #Cette variable stocke le résultat de l'addition
+        Output.Input = self.Input + Other.Input #Cette opération concatène les listes d'éléments
+        return Output #Cette opération renvoie le résultat de l'addition
+
+    def __str__(self): #Cette méthode renvoie une représentation sous forme de chaîne de caractères de l'objet
+        Output = "" #Cette variable stocke la représentation sous forme de chaîne de caractères de l'objet
+        for Item in self.Input: #Cette opération parcourt la liste des éléments
+            Output += Item #Cette opération ajoute l'élément courant à la représentation sous forme de chaîne de caractères
+            Output += " " #Cette opération ajoute un espace après chaque élément
+        return Output.strip() #Cette opération renvoie la représentation sous forme de chaîne de caractères de l'objet
+
+Value1 = MyClass("Rouge", "Vert", "Bleu") #Cette variable stocke la première liste d'éléments
+Value2 = MyClass("Jaune","Pourpre","Cyan") #Cette variable stocke la deuxième liste d'éléments
+Value3 = Value1 + Value2 #Cette variable stocke le résultat de l'addition des deux listes d'éléments
+
 print("{0} + {1} = {2}"
-      .format(Value1, Value2, Value3))
+      .format(Value1, Value2, Value3)) #Cette opération affiche le résultat de l'addition des deux listes d'éléments
