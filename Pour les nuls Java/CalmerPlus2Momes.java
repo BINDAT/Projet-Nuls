5ce0666ef4cf265103d0d7b665c04c71e11bed23
bindat <maximerigaud66@gmail.com>
2025-09-17 21:21:33 +0200

    update commentaire

--- Pour les nuls Java/CalmerPlus2Momes.java
+++ Pour les nuls Java/CalmerPlus2Momes.java
@@ -1,25 +1,25 @@
-import java.util.Scanner;
+import java.util.Scanner; // importation de la classe Scanner pour lire les entrées utilisateur
 
 class CalmerPlus2Momes {
 
-    public static void main(String[] args) {
-        Scanner clavier = new Scanner(System.in);
-        int nBouleGom;
-        int nMome;
-        int nBouleMome;
+    public static void main(String[] args) { // déclaration de la méthode principale
+        Scanner clavier = new Scanner(System.in); // création d'un objet Scanner pour lire les entrées utilisateur
+        int nBouleGom; // déclaration de la variable pour le nombre de boules de gomme
+        int nMome; // déclaration de la variable pour le nombre d'enfants
+        int nBouleMome; // déclaration de la variable pour le nombre de boules par enfant
         
         System.out.print
             ("Combien de boules de gomme ? Combien d'enfants ? ");
 
-        nBouleGom = clavier.nextInt();
-        nMome = clavier.nextInt();
+        nBouleGom = clavier.nextInt(); // lecture du nombre de boules de gomme
+        nMome = clavier.nextInt(); // lecture du nombre d'enfants
 
-        nBouleMome = nBouleGom / nMome;
+        nBouleMome = nBouleGom / nMome; // calcul du nombre de boules par enfant
 
         System.out.print("Chaque enfant reçois ");
-        System.out.print(nBouleMome);
+        System.out.print(nBouleMome); // affichage du nombre de boules par enfant
         System.out.println(" boule de gomme.");
 
-        clavier.close();
+        clavier.close(); // fermeture du scanner
     }
 }
--- Pour les nuls Java/annexe code java/emprunt/EchoLigne3.java
+++ Pour les nuls Java/annexe code java/emprunt/EchoLigne3.java
@@ -1,14 +1,14 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.BufferedReader; // importation de la classe BufferedReader pour lire des lignes
+import java.io.InputStreamReader; // importation de la classe InputStreamReader pour lire des flux d'entrée
+import java.io.IOException; // importation de la classe IOException pour gérer les exceptions d'entrée/sortie
 
-public class EchoLigne {
-    public static void main(String[] args)
+public class EchoLigne3 {
+    public static void main(String[] args) // déclaration de la méthode principale
         throws IOException {
     InputStreamReader isr =
-            new InputStreamReader(System.in);
-    BufferedReader br = new BufferedReader(isr);
-    String input = br.readLine();
-    System.out.println(input);
+            new InputStreamReader(System.in); // création d'un InputStreamReader pour lire depuis l'entrée standard
+    BufferedReader br = new BufferedReader(isr); // création d'un BufferedReader pour lire des lignes de texte
+    String input = br.readLine(); // lecture d'une ligne de texte
+    System.out.println(input); // affichage de la ligne lue
     }
 }
