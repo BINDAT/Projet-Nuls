5ba0221699f488b358a5a346d624713b24160a55
bindat <maximerigaud66@gmail.com>
2025-09-21 22:06:01 +0200

    update commentaire DeuxEquipe.java

--- Pour les nuls Java/DeuxEquipes.java
+++ Pour les nuls Java/DeuxEquipes.java
@@ -1,31 +1,31 @@
-import java.util.Scanner;
-import static java.lang.System.in;
-import static java.lang.System.out;
+import java.util.Scanner; // pour Scanner clavier
+import static java.lang.System.in; // pour System.in et System.out
+import static java.lang.System.out; // pour System.in et System.out
 
 
 class DeuxEquipes {
     
     public static void main(String arg[]) {
-        Scanner clavier = new Scanner(in);
-        int nBleus, nJaunes;
+        Scanner clavier = new Scanner(in); // pour Scanner clavier
+        int nBleus, nJaunes; // nbre de points des Bleus et des Jaunes
 
-        out.print(" Résultats des Bleus et des Jaunes ? ");
-        nBleus = clavier.nextInt();
-        nJaunes = clavier.nextInt();
-        out.println();
+        out.print(" Résultats des Bleus et des Jaunes ? "); // invite de saisie
+        nBleus = clavier.nextInt(); // lecture du nbre de points des Bleus
+        nJaunes = clavier.nextInt(); // lecture du nbre de points des Jaunes
+        out.println(); // ligne vide
         
-        if (nJaunes > nBleus) {
+        if (nJaunes > nBleus) { // si les Jaunes ont plus de points
             out.print("Jaunes : ");
             out.println(nJaunes);
             out.print("Bleus : ");
             out.println(nBleus);
-        } else {
+        } else { // si les Bleus ont plus de points ou égalité
             out.print("Bleus : ");
             out.println(nBleus);
             out.print("Jaunes : ");
             out.println(nJaunes);
         }
 
-        clavier.close();
+        clavier.close(); // fermeture du Scanner clavier
     }
 }
