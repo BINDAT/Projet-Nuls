ef0f515202f1454f2c18dd3e3c34eac2ed3374d9
bindat <maximerigaud66@gmail.com>
2025-09-22 21:02:36 +0200

    update commentaire CouiOuNon.java

--- Pour les nuls Java/CouiOuNon.java
+++ Pour les nuls Java/CouiOuNon.java
@@ -1,23 +1,23 @@
-import java.util.Scanner;
-import java.util.Random;
+import java.util.Scanner; //Bibliothèque qui sert à prendre les entrées de saisit
+import java.util.Random; //Bibliothèque qui sert à générer des nombres aléatoires
 
 class CouiOuNon {
     
-    public static void main(String[] args) {
-        Scanner clavier = new Scanner(System.in);
-        Random monGenAlea = new Random();
-        int nAleatoire;
+    public static void main(String[] args) { //fonction public statique type void du nom main
+        Scanner clavier = new Scanner(System.in); //Ici créer une variable clavier qui sert de stock d'entrée de saisit
+        Random monGenAlea = new Random(); //Créer un générateur de nombre aléatoire
+        int nAleatoire; //Créer une variable de type entier
 
         System.out.print("Tapez votre question mon enfant : ");
         clavier.nextLine();
 
-        nAleatoire = monGenAlea.nextInt(10) +1;
+        nAleatoire = monGenAlea.nextInt(10) +1; //La var nAleatoire reprend en mémoire un nombre aléatoire entre 1 et 10 (inclus)
 
-        if (nAleatoire > 5) {
+        if (nAleatoire > 5) { //Si le nombre aléatoire est supérieur à 5 alors
             System.out.println("Oui c'est évident.");
         }else{
             System.out.println("Non. Et ne posez plus la question");
         }
-        clavier.close();
+        clavier.close(); //Ici on ferme le scanner clavier pour éviter les fuites de mémoires
     }
 }
