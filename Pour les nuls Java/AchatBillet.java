c578d2ff0ee90e4f28c83d4c30a5b74f28b2bcc6
bindat <maximerigaud66@gmail.com>
2025-09-24 21:13:26 +0200

    update commentaire AchatBillet.java

--- Pour les nuls Java/AchatBillet.java
+++ Pour les nuls Java/AchatBillet.java
@@ -1,28 +1,28 @@
-import java.util.Scanner;
+import java.util.Scanner; // pour utiliser Scanner
 
 class AchatBillet {
 
     public static void main(String args[]){
-        Scanner clavier = new Scanner(System.in);
-        int age;
-        double dPrix = 0.00;
+        Scanner clavier = new Scanner(System.in); // pour lire au clavier
+        int age; // âge de la personne
+        double dPrix = 0.00; // prix du billet
 
         System.out.print("Quel est votre âge ? ");
-        age = clavier.nextInt();
+        age = clavier.nextInt(); // lire un entier au clavier
 
         if (age >= 12 && age < 65) {
-            dPrix = 9.25;
+            dPrix = 9.25; // prix normal
 
         }
         if (age < 12 || age >= 65){
-            dPrix = 5.25;
+            dPrix = 5.25; // prix réduit
 
         }
         System.out.print("Vous devez ");
-        System.out.print(dPrix);
+        System.out.print(dPrix); // afficher le prix
         System.out.print("€. ");
         System.out.println("Bon spectacle !");
 
-        clavier.close();
+        clavier.close(); // fermer le scanner
     }
 }
