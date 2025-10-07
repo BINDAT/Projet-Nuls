20e3496c95cd003e760dbf5538ae9b7be336f342
bindat <maximerigaud66@gmail.com>
2025-10-07 21:27:19 +0200

    update commentaire de TabloScore.java

--- Pour les nuls Java/TabloScore.java
+++ Pour les nuls Java/TabloScore.java
@@ -1,27 +1,27 @@
-import java.util.Scanner;
-import static java.lang.System.out;
+import java.util.Scanner; // importation de la classe Scanner pour la saisie au clavier
+import static java.lang.System.out; // importation de la classe System pour l'affichage à l'écran
 
-class TabloScore {
-    enum QuiGagne {LOCAUX, VISITEURS, AUCUN}
+class TabloScore { // début de la classe TabloScore
+    enum QuiGagne {LOCAUX, VISITEURS, AUCUN} // énumération pour représenter le résultat du match
 
     public static void main(String args[]) {
-        Scanner clavier = new Scanner(System.in);
-        int canaris, estac;
-        QuiGagne eQui;
+        Scanner clavier = new Scanner(System.in); // création d'un objet Scanner pour la saisie au clavier
+        int canaris, estac; // déclaration des variables pour les scores des équipes
+        QuiGagne eQui; // déclaration de la variable pour le résultat du match
 
         out.print("Résultats des Jaunes et des Bleus ");
-        canaris = clavier.nextInt();
-        estac = clavier.nextInt();
-        out.println();
+        canaris = clavier.nextInt(); // saisie du score des Jaunes
+        estac = clavier.nextInt(); // saisie du score des Bleus
+        out.println(); // saut de ligne
 
         if (canaris > estac) {
-            eQui = QuiGagne.VISITEURS;
+            eQui = QuiGagne.VISITEURS; // les Jaunes gagnent
             out.println("Victoire des Jaunes :-(");
         } else if (estac > canaris){
-            eQui = QuiGagne.LOCAUX;
+            eQui = QuiGagne.LOCAUX; // les Bleus gagnent
             out.println("Les Bleus gagnent :-)");
         } else {
-            eQui = QuiGagne.AUCUN;
+            eQui = QuiGagne.AUCUN; // match nul
             out.println("C'est un match nul :-|");
         }
 
@@ -34,8 +34,8 @@ class TabloScore {
         out.println("SnitSoft vous souhaite un bon match/");
         out.println();
 
-        if (eQui == QuiGagne.LOCAUX) {
-            out.println("C'est une victoire amplement méritée ");
+        if (eQui == QuiGagne.LOCAUX) { 
+            out.println("C'est une victoire amplement méritée "); 
         }
 
         if (eQui == QuiGagne.VISITEURS) {
@@ -47,6 +47,6 @@ class TabloScore {
             out.println("Un bon match nul pour la suite du championnat !");
         }
 
-        clavier.close();
+        clavier.close(); // fermeture de l'objet Scanner pour éviter les fuites de mémoire
     }
 }
