8c0ee1754d15e7a58cb38ca378b7622f7603cf2b
bindat <maximerigaud66@gmail.com>
2025-10-11 21:38:21 +0200

    upload code annexe java

---
+++ Pour les nuls Java/annexe code java/emprunt/apprecia.java
@@ -0,0 +1,14 @@
+String apprecia;
+description = clavier.next();
+
+switch (apprecia) {
+    case "Excellent":
+        System.out.println('A');
+        break;
+    case "Bien":
+        System.out.println('B');
+        break;
+    case "Moyen":
+        System.out.println('C');
+        break;
+}
