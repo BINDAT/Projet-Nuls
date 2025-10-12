38a32e8ee04a5415714a37bb3b97cd5961abee49
bindat <maximerigaud66@gmail.com>
2025-10-12 21:43:21 +0200

    upload code annexe java

---
+++ Pour les nuls Java/annexe code java/emprunt/desAleaAnnex.java
@@ -0,0 +1,9 @@
+int de1, de2;
+
+de1 = monGenAlea.nextInt(6) + 1;
+de2 = monGenAlea.nextInt(6) + 1;
+
+switch (de1 + de2){
+
+}
+//...etc.
---
+++ Pour les nuls Java/annexe code java/emprunt/nAleatoire.java
@@ -0,0 +1,13 @@
+switch (nAleatoire) {
+    case 2 :
+        System.out.println("Non. Et ne me posez plus la question.");
+        break;
+    case 1:
+        System.out.println("Oui. C'est évident.");
+        break;
+    case 3:
+        System.out.print("Oye Oye !");
+        System.out.println("Trois sac pleins.");
+        break;
+    //...etc.
+}
