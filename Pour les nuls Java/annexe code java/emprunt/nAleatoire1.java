7d6cc75f5f10e9a6c51d565c3177c4c131f8dd0a
bindat <maximerigaud66@gmail.com>
2025-10-14 21:34:57 +0200

    upload code annexe

---
+++ Pour les nuls Java/annexe code java/emprunt/nAleatoire1.java
@@ -0,0 +1,9 @@
+switch (nAleatoire) {
+    case 1: 
+        System.out.println("Oui. C'est évident.");
+        break;
+
+    case 5:
+        System.out.println("Pas de chance, Hortense.");
+        break;
+}
