c1ced30386bc3498945843aa332dd2a4bddc1ea6
BINDAT <136336341+BINDAT@users.noreply.github.com>
2025-10-15 18:06:34 +0200

    Update nAleatoire1.java
    Mise à jour de code

--- Pour les nuls Java/annexe code java/emprunt/nAleatoire1.java
+++ Pour les nuls Java/annexe code java/emprunt/nAleatoire1.java
@@ -6,4 +6,16 @@ switch (nAleatoire) {
     case 5:
         System.out.println("Pas de chance, Hortense.");
         break;
+    case 7:
+        System.out.print("Oui, mais seulement si");
+        System.out.println(" vous êtes gentil avec moi.");
+        break;
+    case 10:
+        System.out.print("Non pas avant que");
+        System.out.println(" la marrée soit basse");
+        break;
+    default:
+        System.out.print("Désolé,");
+        System.out.println(" je suis incapable de me décidé");
+        break;
 }
