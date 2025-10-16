d99f6b78fe859fc3e9d881bc763dfa1138023e15
bindat <maximerigaud66@gmail.com>
2025-10-16 17:53:19 +0200

    upload code annexe

---
+++ Pour les nuls Java/annexe code java/emprunt/nAleatoire2.java
@@ -0,0 +1,12 @@
+switch (nAleatoire) {
+    case 1:
+        System.out.println("Oui. C'est évident");
+        break;
+    case 2:
+        System.out.println("Non. Et ne posez plus la question.");
+        break;
+    case 3:
+        System.out.print("Je suis très fatigué.");
+        System.out.println(" Demandez à quelqu'un d'autre.");
+}
+System.out.println("Au revoir.");
