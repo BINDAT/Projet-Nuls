79a08676bf2a2bf8c7e7215ec4c64c4a42f527e5
bindat <maximerigaud66@gmail.com>
2025-10-20 21:16:04 +0200

    update commentaire annexe java

--- Pour les nuls Java/annexe code java/emprunt/Switchex.java
+++ Pour les nuls Java/annexe code java/emprunt/Switchex.java
@@ -1,20 +1,20 @@
 public class Switchex {
-    enum CodeAPE {BOUCHERIE, CHARCUTERIE, CREMERIE, BOULANGERIE, PATISSERIE, PRIMEURS}
+    enum CodeAPE {BOUCHERIE, CHARCUTERIE, CREMERIE, BOULANGERIE, PATISSERIE, PRIMEURS} // Extrait de la nomenclature APE
 
     public static void main(String args[]) {
-        CodeAPE eCAPE;
+        CodeAPE eCAPE; // Extrait de la nomenclature APE
 
         System.out.println("Switch comme expression !");
-        eCAPE = CodeAPE.CREMERIE; // Figé pour exemple
+        eCAPE = CodeAPE.CREMERIE; // Figé pour exemple //
 
-        int bailCommercial = switch (eCAPE) {
-            case BOUCHERIE, CHARCUTERIE -> 1;
-            case CREMERIE               -> 4;
-            case BOULANGERIE, PATISSERIE -> 5;
-            case PRIMEURS               -> 6;
+        int bailCommercial = switch (eCAPE) { // Switch comme expression
+            case BOUCHERIE, CHARCUTERIE -> 1; // Exemple de valeur retournée
+            case CREMERIE               -> 4; // Exemple de valeur retournée
+            case BOULANGERIE, PATISSERIE -> 5; // Exemple de valeur retournée
+            case PRIMEURS               -> 6; // Exemple de valeur retournée
         };
         System.out.print("Catégorie de bail à appliquer :");
-        System.out.println(bailCommercial);
+        System.out.println(bailCommercial); // Affiche 4
         
     }
 }
