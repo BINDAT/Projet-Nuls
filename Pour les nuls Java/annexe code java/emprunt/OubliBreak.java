b109af21ebeb4ccbc7a31ccf529b9e51b7bb1dab
bindat <maximerigaud66@gmail.com>
2025-10-21 21:41:14 +0200

    update partielle code annexe java

--- Pour les nuls Java/annexe code java/emprunt/OubliBreak.java
+++ Pour les nuls Java/annexe code java/emprunt/OubliBreak.java
@@ -20,27 +20,43 @@ public class OubliBreak {
 
         switch (nAleatoire) {
             case 1:
-            ;
+                out.println("Oui. C'est évident.");
+            
             case 2:
-            ;
+                out.println("Non. Et ne posez plus cette question.");
+            
             case 3:
-            ;
+                out.print("Oye, Oye !");
+                out.println(" Trois sacs pleins.");
+
             case 4:
-            ;
+                out.print("Qu'est-ce que vous ne comprenez pas");
+                out.println(" dans le mot 'non' ?");
+                break;
+
             case 5:
-            ;
+                out.println("Pas de chance, Hortense.");
+
             case 6:
-            ;
+                out.println("Sûr et certain.");
+
             case 7:
-            ;
+                out.print("Oui, mais seulement si");
+                out.println(" vous êtes gentil avec moi.");
+
             case 8:
-            ;
+                out.println("Oui (si ça vous fait plaisir).");
+
             case 9:
-            ;
+                out.print("Non pas avant que");
+                out.println(" la marée soit haute.");
+
             case 10:
-            ;
+                out.print("Non pas avant que");
+                out.println(" la marée soit haute.");
+
             default:
-            ;
+                out.;
         }
         
         out.println("Au revoir !");
