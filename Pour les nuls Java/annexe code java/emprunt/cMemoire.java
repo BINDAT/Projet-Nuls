b825fca1f52936059d1c23acb80c857ffe1dab10
bindat <maximerigaud66@gmail.com>
2025-10-25 21:22:05 +0200

    upload code annexe java

---
+++ Pour les nuls Java/annexe code java/emprunt/cMemoire.java
@@ -0,0 +1,9 @@
+c1 = clavier.findWithinHorizon(".",0).charAt(0);
+c2 = clavier.findWithinHorizon(".",0).charAt(0);
+c3 = clavier.findWithinHorizon(".",0).charAt(0);
+c4 = clavier.findWithinHorizon(".",0).charAt(0);
+
+System.out.print(c4);
+System.out.print(c3);
+System.out.print(c2);
+System.out.print(c1);
