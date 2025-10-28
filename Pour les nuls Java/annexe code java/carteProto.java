073e6f7610f61f4597eb46599c87a0f0c93f450e
bindat <maximerigaud66@gmail.com>
2025-10-28 16:38:58 +0100

    update code annexe java

--- Pour les nuls Java/annexe code java/carteProto.java
+++ Pour les nuls Java/annexe code java/carteProto.java
@@ -1,24 +1,22 @@
-//Code préliminaire - NON FINALISÉ
+//Code préliminaire - FINALISÉ
 import java.util.Random;
-int carte, total = 0;
 
 class Deck {
     public static void main (String args[]){
-        int carte, total;
+        int carte, total = 0;
         Random monGenAlea = new Random();
-System.out.println(carte+total);
+
 while (total < 21) {
     carte = monGenAlea.nextInt(10) + 1;
-    
+    total = total += carte;
     System.out.print(carte);
     System.out.print("       ");
     System.out.println(total);
-}
-
-if (total == 21) {
-    System.out.println("Vous gagnez ;-)");
-} else {
-    System.out.println("Vous perdez :-(");
-}
-}
+    }
+    if (total == 21) {
+            System.out.println("Vous gagnez ;-)");
+        } else {
+            System.out.println("Vous perdez :-(");
+        }
+    }
 }
