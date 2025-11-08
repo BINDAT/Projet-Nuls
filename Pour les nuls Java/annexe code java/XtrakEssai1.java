e154071c1dc7ff553d7571da2a9df5afaa537071
bindat <maximerigaud66@gmail.com>
2025-11-08 21:32:55 +0100

    update commentaire annexe java

--- Pour les nuls Java/annexe code java/XtrakEssai1.java
+++ Pour les nuls Java/annexe code java/XtrakEssai1.java
@@ -2,22 +2,23 @@
  *Ce code ne marche PAS, mais je ne suis pas découragé. 
  */
 
-import java.util.Scanner;
+import java.util.Scanner; //import la fonction Scanner pou la saisie clavier
 
 class XtrakEssai1 {
     
     public static void main(String args[]) {
-        Scanner clavier = new Scanner(System.in);
+        Scanner clavier = new Scanner(System.in); //Création d'une variable qui prendra la saisie du clavier
         char cSymb = ' ';
 
         while (cSymb != '@') {
             cSymb = clavier.findWithinHorizon(".", 0).charAt(0);
 
             System.out.print(cSymb);
+            //affichage continue de la saisie jusqu'à temps que @ n'est pas saisit
         }
 
         System.out.print(cSymb);
 
-        clavier.close();
+        clavier.close(); //fermeture du clavier
     }
 }
