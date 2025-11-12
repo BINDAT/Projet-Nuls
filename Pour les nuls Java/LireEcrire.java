1ae6a238c1512bd3ad4469372a19d9299afd7f78
bindat <maximerigaud66@gmail.com>
2025-11-12 21:38:04 +0100

    update commentaire de LireEcrire.java

--- Pour les nuls Java/LireEcrire.java
+++ Pour les nuls Java/LireEcrire.java
@@ -1,25 +1,25 @@
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
+import java.util.Scanner; // Pour la lecture au clavier et dans les fichiers
+import java.io.File; // Pour la lecture de fichiers
+import java.io.FileNotFoundException; // Pour la gestion des exceptions
+import java.io.PrintStream; // Pour l'écriture dans les fichiers
 
 public class LireEcrire {
     
     public static void main(String args []) 
-                        throws FileNotFoundException{
-        Scanner dsc = new Scanner(new File("rawData.txt"));
+                        throws FileNotFoundException{ // Gestion des exceptions
+        Scanner dsc = new Scanner(new File("rawData.txt")); // Lecture du fichier rawData.txt
         PrintStream dsqScripteur = 
-            new PrintStream("cookedData.txt");
-        double dPrixUni, dQuanti, total;
+            new PrintStream("cookedData.txt"); // Écriture dans le fichier cookedData.txt
+        double dPrixUni, dQuanti, total; // Déclaration des variables
 
-        dPrixUni = dsc.nextDouble();
-        dQuanti = dsc.nextInt();
+        dPrixUni = dsc.nextDouble(); // Lecture du prix unitaire
+        dQuanti = dsc.nextInt(); // Lecture de la quantité
 
-        total = dPrixUni * dQuanti;
+        total = dPrixUni * dQuanti; // Calcul du total
 
-        dsqScripteur.println(total);
+        dsqScripteur.println(total); // Écriture du total dans le fichier
 
-        dsc.close();
-        dsqScripteur.close();
+        dsc.close(); // Fermeture du fichier lu
+        dsqScripteur.close(); // Fermeture du fichier écrit
     }
 }
