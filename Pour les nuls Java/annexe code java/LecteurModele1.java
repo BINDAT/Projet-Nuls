365f0e534e5674984c5f4374d43f4658f12f677a
bindat <maximerigaud66@gmail.com>
2025-11-13 21:41:28 +0100

    upload code annexe java

---
+++ Pour les nuls Java/annexe code java/LecteurModele1.java
@@ -0,0 +1,23 @@
+/*
+ * Avant qu'Eclispe puisse compiler ce code,
+ * vous devez remplir les blancs.
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+class ____ {
+    public static void main(String args[]) 
+        throws FileNotFoundException{
+        
+        PrintStream dsqScripteur =
+            new PrintStream("____");
+
+        dsqScripteur.print(____);
+        dsqScripteur.println(____);
+    
+//etc.
+        
+        dsqScripteur.close();
+    }
+}
