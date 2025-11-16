91e48d4d0c8e095ec274d26691b9f870aa2ced4b
bindat <maximerigaud66@gmail.com>
2025-11-16 15:22:21 +0100

    update commentaire de EcrireOK.java

--- Pour les nuls Java/EcrireOK.java
+++ Pour les nuls Java/EcrireOK.java
@@ -1,17 +1,17 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
+import java.io.File; // Pour utiliser la classe File pour créer un fichier
+import java.io.FileNotFoundException; // Pour gérer l'exception si le fichier ne peut pas être créé
+import java.io.PrintStream; // Pour écrire dans le fichier
 
 class EcrireOK {
-    public static void main(String args[]) 
-        throws FileNotFoundException {
+    public static void main(String args[]) // Méthode principale
+        throws FileNotFoundException { // Gestion de l'exception
         
         PrintStream dsqScripteur = 
-            new PrintStream(new File("approuve.txt"));
+            new PrintStream(new File("approuve.txt")); // Création du scripteur pour le fichier "approuve.txt"
 
-        dsqScripteur.print('O');
-        dsqScripteur.println('K');
+        dsqScripteur.print('O'); // Écriture du caractère 'O' dans le fichier
+        dsqScripteur.println('K'); // Écriture du caractère 'K' suivi d'un saut de ligne dans le fichier
 
-        dsqScripteur.close();
+        dsqScripteur.close(); // Fermeture du scripteur pour libérer les ressources
     }
 }
