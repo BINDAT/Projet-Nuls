360472c49c77e657e8a5acf46da06a04c698a1b2
bindat <maximerigaud66@gmail.com>
2025-11-16 21:58:35 +0100

    update commentaire ExtraireNomUnique.java

--- Pour les nuls Java/ExtraireNomUnique.java
+++ Pour les nuls Java/ExtraireNomUnique.java
@@ -1,7 +1,7 @@
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
+import java.util.Scanner; // Importation de la classe Scanner pour la lecture de fichiers
+import java.io.File; // Importation de la classe File pour la gestion des fichiers
+import java.io.FileNotFoundException; // Importation de l'exception FileNotFoundException pour la gestion des erreurs
+import java.io.PrintStream; // Importation de la classe PrintStream pour écrire dans un fichier
 
 class ExtraireNomUnique {
     
@@ -9,20 +9,20 @@ class ExtraireNomUnique {
                         throws FileNotFoundException{
         
         Scanner dsqLecteur = 
-            new Scanner(new File("email.txt"));
+            new Scanner(new File("email.txt")); // Création d'un Scanner pour lire le fichier email.txt
         PrintStream dsqScripteur = 
-            new PrintStream("usernames.txt");
+            new PrintStream("usernames.txt"); // Création d'un PrintStream pour écrire dans le fichier usernames.txt
         char cSymb = 
-            dsqLecteur.findWithinHorizon(".",0).charAt(0);
+            dsqLecteur.findWithinHorizon(".",0).charAt(0); // Lecture du premier caractère du fichier email.txt
         
         while (cSymb != '@') {
             dsqScripteur.print(cSymb);
-            cSymb = dsqLecteur.findWithinHorizon(".",0).charAt(0);
+            cSymb = dsqLecteur.findWithinHorizon(".",0).charAt(0); // Lecture du caractère suivant du fichier email.txt
         }
 
-        dsqScripteur.println();
+        dsqScripteur.println(); // Saut de ligne dans le fichier usernames.txt
 
-        dsqLecteur.close();
-        dsqScripteur.close();
+        dsqLecteur.close(); // Fermeture du Scanner
+        dsqScripteur.close(); // Fermeture du PrintStream
     }
 }
