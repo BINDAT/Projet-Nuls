6e377cd24755cc75866a33cbdc72bcf6e1b0082f
bindat <maximerigaud66@gmail.com>
2025-11-20 21:45:34 +0100

    update json et LesDixMachesChezEdna.java

---
+++ Pour les nuls Java/LesDixMachesChezEdna.java
@@ -0,0 +1,15 @@
+import static java.lang.System.out;
+
+class LesDixMachesChezEdna {
+    
+    public static void main(String args[]) {
+        
+        for (int nTour = 0; nTour < 10; nTour++){
+            out.print("J'ai mâché ");
+            out.print(nTour);
+            out.println(" fois.");
+        }
+        out.println("10 fois ! Hourra !");
+        out.println("Je peut avaler !");
+    }
+}
