0117f978b8253c3f9ea64deb4437abe98e7c70a1
bindat <maximerigaud66@gmail.com>
2025-11-22 21:25:20 +0100

    update commentaire LesDixMachesChezEdna.java

--- Pour les nuls Java/LesDixMachesChezEdna.java
+++ Pour les nuls Java/LesDixMachesChezEdna.java
@@ -1,15 +1,15 @@
-import static java.lang.System.out;
+import static java.lang.System.out; // Pour pouvoir utiliser out directement
 
-class LesDixMachesChezEdna {
+class LesDixMachesChezEdna { // Déclaration de la classe
     
-    public static void main(String args[]) {
-        
-        for (int nTour = 0; nTour < 10; nTour++){
+    public static void main(String args[]) { // Méthode principale
+         
+        for (int nTour = 0; nTour < 10; nTour++){ // Boucle de 0 à 9
             out.print("J'ai mâché ");
-            out.print(nTour);
+            out.print(nTour); // Affiche le nombre de tours
             out.println(" fois.");
         }
-        out.println("10 fois ! Hourra !");
-        out.println("Je peut avaler !");
+        out.println("10 fois ! Hourra !"); // Après la boucle
+        out.println("Je peut avaler !"); // Message final
     }
 }
