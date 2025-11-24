604d211d2252e1809e62825c618e02b2c13f37c9
bindat <maximerigaud66@gmail.com>
2025-11-24 21:42:35 +0100

    update commentaire de DangerPourVosFichier.java

--- Pour les nuls Java/DangerPourVosFichier.java
+++ Pour les nuls Java/DangerPourVosFichier.java
@@ -9,28 +9,28 @@
 * Signez ici : ____________________
 */
 
-import java.io.File;
-import java.util.Scanner;
+import java.io.File; // Pour la gestion des fichiers
+import java.util.Scanner; // Pour la lecture au clavier
 
 class DangerPourVosFichier {
     public static void main(String args[]) {
-        Scanner clavier = new Scanner(System.in);
-        char cDecision;
+        Scanner clavier = new Scanner(System.in); // Pour la lecture au clavier
+        char cDecision; // Pour stocker la décision de l'utilisateur
 
         do {
 
-            System.out.print("Répondez par O ou N...");
-            System.out.print(" Supprimer importantData.txt ? ");
-            cDecision = clavier.findWithinHorizon(".", 0).charAt(0);
-        } while (cDecision !='O' && cDecision != 'N');
+            System.out.print("Répondez par O ou N..."); // Demande à l'utilisateur de répondre par O ou N
+            System.out.print(" Supprimer importantData.txt ? "); // Question posée à l'utilisateur
+            cDecision = clavier.findWithinHorizon(".", 0).charAt(0); // Lecture de la réponse de l'utilisateur
+        } while (cDecision !='O' && cDecision != 'N'); // Répéter tant que la réponse n'est pas O ou N
 
         if (cDecision == 'O') {
-            new File("importantData.txt").delete();
-            System.out.println("Supprimé pour toujours !");
+            new File("importantData.txt").delete(); // Suppression du fichier importantData.txt
+            System.out.println("Supprimé pour toujours !"); // Confirmation de la suppression
         } else {
-            System.out.print("Il n'y a pas de mal à demander !");
+            System.out.print("Il n'y a pas de mal à demander !"); // Message si l'utilisateur a répondu N
         }
 
-        clavier.close();
+        clavier.close(); // Fermeture du scanner pour libérer les ressources
     }
 }
