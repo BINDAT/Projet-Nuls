2ba94c7abb3ef05bb30f9fe637e8364b29fec421
bindat <maximerigaud66@gmail.com>
2025-11-29 21:46:08 +0100

    update commentaire de OccupanceHotel.java

--- Pour les nuls Java/OccupanceHotel.java
+++ Pour les nuls Java/OccupanceHotel.java
@@ -1,22 +1,22 @@
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import static java.lang.System.out;
+import java.util.Scanner; // Importation de la classe Scanner pour la lecture de fichiers
+import java.io.File; // Importation de la classe File pour manipuler les fichiers
+import java.io.FileNotFoundException; // Importation de l'exception FileNotFoundException afin de la gérer
+import static java.lang.System.out; // Importation statique de System.out pour simplifier les appels d'impression
 
 class OccupanceHotel {
 
-    public static void main(String args[]) 
-                        throws FileNotFoundException{
+    public static void main(String args[]) // Méthode principale
+                        throws FileNotFoundException{ // Déclaration que la méthode peut lancer une exception FileNotFoundException
         
-        Scanner dsqLecteur = new Scanner(new File("occupance"));
-        out.print("Chambre\tPersonnes");
+        Scanner dsqLecteur = new Scanner(new File("occupance")); // Création d'un objet Scanner pour lire le fichier "occupance"
+        out.print("Chambre\tPersonnes"); // Impression de l'en-tête du tableau
 
-        for (int nCham = 0; nCham < 10; nCham++) {
-            out.print(nCham);
-            out.print("\t");
-            out.println(dsqLecteur.nextInt());
+        for (int nCham = 0; nCham < 10; nCham++) { // Boucle pour chaque chambre de 0 à 9
+            out.print(nCham); // Impression du numéro de la chambre
+            out.print("\t"); // Impression d'une tabulation pour séparer les colonnes
+            out.println(dsqLecteur.nextInt()); // Lecture et impression du nombre de personnes dans la chambre
         }
 
-        dsqLecteur.close();
+        dsqLecteur.close(); // Fermeture du Scanner pour libérer les ressources
     }
 }
