ce09ec42921bdfd49396f6de6cddad7264819f04
bindat <maximerigaud66@gmail.com>
2025-11-30 17:52:25 +0100

    Ajout de commentaires explicatifs dans BetterShowOneRoom.java

--- Pour les nuls Java/BetterShowOneRoom.java
+++ Pour les nuls Java/BetterShowOneRoom.java
@@ -1,28 +1,28 @@
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import static java.lang.System.out;
+import java.util.Scanner; // Importation de la classe Scanner pour la saisie clavier
+import java.io.File; // Importation de la classe File pour la gestion des fichiers
+import java.io.FileNotFoundException; // Importation de l'exception pour la gestion des fichiers non trouvés
+import static java.lang.System.out; // Importation statique pour simplifier les appels à System.out
 
-public class BetterShowOneRoom {
+public class BetterShowOneRoom { // Déclaration de la classe principale
     
-    public static void main(String args[]) throws FileNotFoundException{
+    public static void main(String args[]) throws FileNotFoundException{ // Début du programme principal avec gestion des exceptions de fichier
         
-        Scanner clavier = new Scanner(System.in);
-        Scanner dsqLecteur = new Scanner(new File("occupance"));
-        int nChoixChambre;
+        Scanner clavier = new Scanner(System.in); // Création d'un objet Scanner pour lire les entrées utilisateur
+        Scanner dsqLecteur = new Scanner(new File("occupance")); // Création d'un objet Scanner pour lire le fichier "occupance"
+        int nChoixChambre; // Variable pour stocker le choix de la chambre de l'utilisateur
 
-        out.print("Quelle chambre ?");
-        nChoixChambre = clavier.nextInt();
+        out.print("Quelle chambre ?"); // Demande à l'utilisateur de choisir une chambre
+        nChoixChambre = clavier.nextInt(); // Lecture du choix de la chambre de l'utilisateur
 
-        for(int nCham = 0; nCham < nChoixChambre && dsqLecteur.hasNext(); nCham++){
-            out.print("La Chambre ");
-            out.print(nChoixChambre);
-            out.print(" a ");
-            out.print(dsqLecteur.nextInt());
-            out.println(" personne(s).");
+        for(int nCham = 0; nCham < nChoixChambre && dsqLecteur.hasNext(); nCham++){ // Boucle pour parcourir les chambres jusqu'à la chambre choisie
+            out.print("La Chambre "); // Affichage du numéro de la chambre
+            out.print(nChoixChambre); // Affichage du numéro de la chambre choisie
+            out.print(" a "); // Affichage du texte intermédiaire
+            out.print(dsqLecteur.nextInt()); // Lecture et affichage du nombre de personnes dans la chambre choisie
+            out.println(" personne(s)."); // Fin de la ligne d'affichage
         }
 
-        clavier.close();
-        dsqLecteur.close();
+        clavier.close(); // Fermeture du scanner pour éviter les fuites de ressources
+        dsqLecteur.close(); // Fermeture du scanner de fichier pour éviter les fuites de ressources
     }
 }
