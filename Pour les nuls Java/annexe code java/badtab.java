f2c1a1e53464fcd24248631f8943a17f1884a02f
bindat <maximerigaud66@gmail.com>
2025-11-30 21:20:57 +0100

    upload code annexe java

---
+++ Pour les nuls Java/annexe code java/badtab.java
@@ -0,0 +1,3 @@
+// Ne faite pas cela :
+int statutCh[];
+statutCh = {1, 4, 0, 2, 2, 1, 4, 3, 0, 2}; // Erreur de compilation
---
+++ Pour les nuls Java/annexe code java/statutChp.java
@@ -0,0 +1,6 @@
+int statutCh[];
+statutCh = new int[10];
+
+for (int nCham = 0; nCham < 10; nCham++){
+    statutCh[nCham] = 0;
+}
