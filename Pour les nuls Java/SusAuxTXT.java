c5f91e62dfaf8e667addfe975dcc5c7a11a59247
bindat <maximerigaud66@gmail.com>
2025-12-01 21:05:27 +0100

    Ajout de commentaires explicatifs dans SusAuxTXT.java pour améliorer la lisibilité du code

--- Pour les nuls Java/SusAuxTXT.java
+++ Pour les nuls Java/SusAuxTXT.java
@@ -1,13 +1,13 @@
-import java.io.File;
+import java.io.File; // Importation de la classe File pour la manipulation des fichiers
 
-class SusAuxTXT {
+class SusAuxTXT { // Déclaration de la classe principale
     
-    public static void main(String args[]) {
+    public static void main(String args[]) { // Méthode principale où l'exécution commence
         
-        File fDossier = new File(".");
-        for (File file : fDossier.listFiles()){
-            if (file.getName().endsWith(".txt")) {
-                file.delete();
+        File fDossier = new File("."); // Création d'un objet File représentant le répertoire courant
+        for (File file : fDossier.listFiles()){ // Boucle à travers tous les fichiers du répertoire
+            if (file.getName().endsWith(".txt")) { // Vérification si le fichier se termine par ".txt"
+                file.delete(); // Suppression du fichier s'il est un fichier texte
             }
         }
     }
