110631c63a4c021bedfae9fcf01fa6115fea3744
bindat <maximerigaud66@gmail.com>
2025-12-02 21:16:31 +0100

    upload annex java

---
+++ Pour les nuls Java/annexe code java/ChaiseProto.java
@@ -0,0 +1,8 @@
+/*
+* Ceci est du vrai code Java, mais
+* il ne peut pas être compilé seul
+*/
+class Chaise{
+    PlaqueHorizontale assise;
+    PlaqueVerticale dossier;
+    PiliersSupport pieds;}
