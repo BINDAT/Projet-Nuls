b9dc48b4a4b4187d260911a066747e406cafec5c
bindat <maximerigaud66@gmail.com>
2025-12-02 21:22:20 +0100

    upload code JazzEcho.java

---
+++ Pour les nuls Java/JazzEcho.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class JazzEcho {
+    
+    public static void main(String args[]) {
+        Scanner clavier = new Scanner(System.in);
+        String sMaChaine;
+
+        System.out.print("sMaChaine");
+        sMaChaine = clavier.nextLine();
+        System.out.println(sMaChaine);
+
+        clavier.close();}}
