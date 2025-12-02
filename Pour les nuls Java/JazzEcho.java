0d9fc9125dc783876ce37582b0ac72ba0187c8f4
bindat <maximerigaud66@gmail.com>
2025-12-02 21:24:24 +0100

    Ajout de commentaires explicatifs dans JazzEcho.java pour améliorer la lisibilité du code

--- Pour les nuls Java/JazzEcho.java
+++ Pour les nuls Java/JazzEcho.java
@@ -1,13 +1,15 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importation de la classe Scanner pour la saisie clavier
 
-class JazzEcho {
+class JazzEcho { // Classe principale pour l'écho de la saisie utilisateur
     
-    public static void main(String args[]) {
-        Scanner clavier = new Scanner(System.in);
-        String sMaChaine;
+    public static void main(String args[]) { // Méthode principale du programme
+        Scanner clavier = new Scanner(System.in); // Création d'un objet Scanner pour lire les entrées utilisateur
+        String sMaChaine; // Variable pour stocker la chaîne de caractères saisie par l'utilisateur
 
-        System.out.print("sMaChaine");
-        sMaChaine = clavier.nextLine();
-        System.out.println(sMaChaine);
+        System.out.print("Une petite dédicace ? : "); // Demande à l'utilisateur de saisir une chaîne
+        sMaChaine = clavier.nextLine(); // Lecture de la chaîne saisie par l'utilisateur
+        System.out.println(sMaChaine); // Affichage de la chaîne saisie
 
-        clavier.close();}}
+        clavier.close(); // Fermeture du scanner pour éviter les fuites de ressources
+    }
+}
