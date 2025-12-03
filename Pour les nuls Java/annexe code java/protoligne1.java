49a64cfcf946fe7518e3e90ac06240021233530e
bindat <maximerigaud66@gmail.com>
2025-12-03 21:31:33 +0100

    upload code annexe java

---
+++ Pour les nuls Java/annexe code java/protoligne1.java
@@ -0,0 +1,5 @@
+sChaine1 = "               l'espace a toussé sur moi " +
+"         et voilà je ne suis plus " +
+"               les cieux roulent des yeux" +
+"des yeux qui ne disent rien et ne savent pas " +
+"   grand'chose";
