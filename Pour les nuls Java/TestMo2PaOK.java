d24d7f373a5462b531eb398e344982c9f609d40f
bindat <maximerigaud66@gmail.com>
2025-12-05 21:41:44 +0100

    Ajout de commentaires explicatifs pour améliorer la lisibilité du code dans TestMo2PaOK.java

--- Pour les nuls Java/TestMo2PaOK.java
+++ Pour les nuls Java/TestMo2PaOK.java
@@ -1,24 +1,24 @@
 /*
 * Ce programme fonctionne !
 */
-import java.util.Scanner;
-import static java.lang.System.out;
+import java.util.Scanner; // Importation de la classe Scanner pour la saisie au clavier
+import static java.lang.System.out; // Importation statique de System.out pour simplifier les appels d'affichage
 
-class TestMo2PaOK {
-    public static void main(String args[]) {
-        Scanner clavier = new Scanner(System.in);
-        String sSecret = "espadon";
-        String sSaisie;
+class TestMo2PaOK { // Déclaration de la classe principale
+    public static void main(String args[]) { // Méthode principale
+        Scanner clavier = new Scanner(System.in); // Création d'un objet Scanner pour la saisie au clavier
+        String sSecret = "espadon"; // Définition du mot de passe secret
+        String sSaisie; // Variable pour stocker la saisie de l'utilisateur
 
-        out.print("Quel est le mot de passe ?");
-        sSaisie = clavier.next();
+        out.print("Quel est le mot de passe ?"); // Invitation à saisir le mot de passe
+        sSaisie = clavier.next(); // Lecture de la saisie utilisateur
 
-        if (sSecret.equals(sSaisie)) {
-            out.println("Mot de passe accepté !");
+        if (sSecret.equals(sSaisie)) { // Comparaison de la saisie avec le mot de passe secret
+            out.println("Mot de passe accepté !"); // Message en cas de succès
         } else {
-            out.println("Demi tour, et vite !");
+            out.println("Demi tour, et vite !"); // Message en cas d'échec
         }
 
-        clavier.close();
+        clavier.close(); // Fermeture du scanner pour libérer les ressources
     }
 }
