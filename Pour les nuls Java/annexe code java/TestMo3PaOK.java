65bb9bf1ab8faf9dfc760be8b5cb9659132e68a3
bindat <maximerigaud66@gmail.com>
2025-12-05 21:46:50 +0100

    upload code archive

---
+++ Pour les nuls Java/annexe code java/TestMo3PaOK.java
@@ -0,0 +1,24 @@
+/*
+* Ce programme fonctionne !
+*/
+import java.util.Scanner;
+import static java.lang.System.out;
+
+class TestMo3PaOK {
+    public static void main(String args[]) {
+        Scanner clavier = new Scanner(System.in);
+        String sSecret = "espadon";
+        String sSaisie;
+
+        out.print("Quel est le mot de passe ? ");
+        sSaisie = clavier.next();
+
+        if (sSecret.equals(sSaisie)) {
+            out.println("Mot de passe accepté ! " + sSaisie + " OK " + sSecret);
+        } else {
+            out.println("Demi tour, et vite ! " + sSaisie + " KO " + sSecret);
+        }
+
+        clavier.close();
+    }
+}
