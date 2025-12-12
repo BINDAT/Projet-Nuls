a29533ac62f86c9cc91a715367dcb480cf44accf
bindat <maximerigaud66@gmail.com>
2025-12-12 21:32:59 +0100

    Amélioration de la méthode afficher dans la classe CompteMieux pour inclure le formatage du solde et l'affichage des détails du compte

--- Pour les nuls Java/CompteMieux.java
+++ Pour les nuls Java/CompteMieux.java
@@ -15,9 +15,19 @@ class CompteMieux {
         (char) (monGenAlea.nextInt(26) + 'A') +
         (char) (monGenAlea.nextInt(26) + 'a') +
         (char) (monGenAlea.nextInt(26) + 'a');
+
+        iId = monGenAlea.nextInt(10000);
+        dSolde = monGenAlea.nextInt(100000);
     }
     
     void afficher(){
+        NumberFormat nfDevise = NumberFormat.getCurrencyInstance();
 
+        out.print("Le compte portant le nom ");
+        out.print(sPatro);
+        out.print(" et dont l'identificateur est");
+        out.print(iId);
+        out.print(" a un solde de ");
+        out.println(nfDevise.format(dSolde));
     }
 }
