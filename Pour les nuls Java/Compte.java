06ae411ef11ce4cfb3f5eb2040f2848249704eb1
bindat <maximerigaud66@gmail.com>
2025-12-14 21:20:07 +0100

    Amélioration des commentaires dans la classe Compte pour clarifier le code

--- Pour les nuls Java/Compte.java
+++ Pour les nuls Java/Compte.java
@@ -1,21 +1,21 @@
-import java.text.NumberFormat;
+import java.text.NumberFormat; // Importe la bibliothèque NumberFormat pour gérer les format devise
 import static java.lang.System.out;
 
 
 class Compte {
-    String sPatro;
-    int iID;
-    double dSolde;
+    String sPatro; // Une var pour le patronyme de l'utilisateur
+    int iID; // var pour l'identificateur
+    double dSolde; // var pour le solde
 
     void afficher(){
         NumberFormat nfDevise =
-            NumberFormat.getCompactNumberInstance();
+            NumberFormat.getCompactNumberInstance(); // gère le format de devise
 
         out.print("Le compte portant le nom ");
-        out.print(sPatro);
+        out.print(sPatro); // affiche le patronyme
         out.print(" et dont l'identificateur est ");
-        out.print(iID);
+        out.print(iID); // affiche l'identificateur
         out.print(" a un solde de ");
-        out.println(nfDevise.format(dSolde));
+        out.println(nfDevise.format(dSolde)); // affiche le solde de compte avec le gestionnaire de devise
     }
 }
