0bfb716405beebe9cda659ecdd061d940c370cd3
bindat <maximerigaud66@gmail.com>
2025-12-23 21:30:47 +0100

    Amélioration des commentaires dans la classe BonCompteAn pour clarifier le code

--- Pour les nuls Java/BonCompteAn.java
+++ Pour les nuls Java/BonCompteAn.java
@@ -1,23 +1,24 @@
-import java.text.NumberFormat;
-import static java.lang.System.out;
+import java.text.NumberFormat; // Pour la mise en forme des devises
+import static java.lang.System.out; // Pour l'affichage à l'écran
 
-class BonCompteAn {
-    String sPatro;
-    int id;
-    double dSolde;
+class BonCompteAn { // Déclaration de la classe
+    String sPatro; // Nom du propriétaire
+    int id;       // Identifiant du compte
+    double dSolde; // Solde du compte
 
     void ajInterets(double dTaux, int iAnnees) {
-        for (int k = 1; k <= iAnnees; k++) {
-            out.print("Ajout de ");
-            out.print(dTaux);
-            out.print(" pour cent por l'année ");
-            out.print(k);
+        for (int k = 1; k <= iAnnees; k++) { // Boucle pour chaque année
+            out.print("Ajout de "); // Affichage du taux d'intérêt
+            out.print(dTaux); // en pour cent
+            out.print(" pour cent pour l'année "); // et de l'année
+            out.print(k); // en cours
 
-            dSolde += dSolde * (dTaux / 100.0);
+            dSolde += dSolde * (dTaux / 100.0); // Calcul des intérêts
         }
     }
-     void afficher() {
-            NumberFormat nfDevise = NumberFormat.getCurrencyInstance();
+    
+    void afficher() { // Méthode pour afficher les informations du compte
+        NumberFormat nfDevise = NumberFormat.getCurrencyInstance(); // Format monétaire pour la devise locale
 
             out.print("Le compte portant le nom ");
             out.print(sPatro);
