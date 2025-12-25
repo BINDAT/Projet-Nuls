83e0a1b16ceaa1ca95eafad926b53403f45a84a0
bindat <maximerigaud66@gmail.com>
2025-12-25 21:18:22 +0100

    Amélioration des commentaires dans la classe TraiteBonsCompteAn pour clarifier le code

--- Pour les nuls Java/TraiteBonsCompteAn.java
+++ Pour les nuls Java/TraiteBonsCompteAn.java
@@ -1,29 +1,29 @@
-import java.util.Random;
+import java.util.Random; // Pour le générateur de nombres aléatoires
 
 class TraiteBonsCompteAn {
     
     public static void main(String args[]) {
-        Random monGenAlea = new Random();
-        BonCompteAn unCompte;
-        double dTxInt;
-        int iAns = 3;
+        Random monGenAlea = new Random(); // Générateur de nombres aléatoires
+        BonCompteAn unCompte; // Référence à un BonCompteAn
+        double dTxInt; // Taux d'intérêts
+        int iAns = 3; // Nombre d'années pour le calcul des intérêts
 
         for (int i= 0; i < 3; i++) {
-            unCompte = new BonCompteAn();
-            unCompte.sPatro = "" +
-            (char) (monGenAlea.nextInt(26) + 'A') +
-            (char) (monGenAlea.nextInt(26) + 'a') +
-            (char) (monGenAlea.nextInt(26) + 'a');
-        unCompte.id = monGenAlea.nextInt(10000);
-        unCompte.dSolde = monGenAlea.nextInt(10000);
+            unCompte = new BonCompteAn(); // Création d'un BonCompteAn
+            unCompte.sPatro = "" + // Trois caractères aléatoires
+            (char) (monGenAlea.nextInt(26) + 'A') + // Majuscules
+            (char) (monGenAlea.nextInt(26) + 'a') + // Minuscules
+            (char) (monGenAlea.nextInt(26) + 'a'); // Minuscules
+        unCompte.id = monGenAlea.nextInt(10000); // Identifiant aléatoire
+        unCompte.dSolde = monGenAlea.nextInt(10000); // Solde aléatoire
 
-        unCompte.afficher();
+        unCompte.afficher(); // Affichage avant intérêts
 
-        dTxInt = monGenAlea.nextInt(5);
+        dTxInt = monGenAlea.nextInt(5); // Taux d'intérêts aléatoire entre 0 et 4 %
         unCompte.ajInterets(dTxInt, iAns); // Deux params !
 
-        unCompte.afficher();
-        System.out.println();
+        unCompte.afficher(); // Affichage après intérêts
+        System.out.println(); // Saut de ligne
         }
     }
 }
