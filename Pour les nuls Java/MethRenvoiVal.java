b9db13ebb5cdf81345c723d266a6002f068e4c9c
bindat <maximerigaud66@gmail.com>
2025-12-29 21:34:43 +0100

    Amélioration des commentaires dans la classe MethRenvoiVal pour clarifier le code

--- Pour les nuls Java/MethRenvoiVal.java
+++ Pour les nuls Java/MethRenvoiVal.java
@@ -1,30 +1,30 @@
-import java.util.Random;
-import java.text.NumberFormat;
+import java.util.Random; // Importation de la classe Random
+import java.text.NumberFormat; // Importation de la classe NumberFormat
 
 class MethRenvoiVal {
     public static void main(String args[]) {
-        Random monGenAlea = new Random();
-        ClasseMethRenVal unCompte;
-        double dTxInt;
-        double dIntAnnuels;
+        Random monGenAlea = new Random(); // Création d'un générateur de nombres aléatoires
+        ClasseMethRenVal unCompte; // Déclaration d'une variable de type ClasseMethRenVal
+        double dTxInt; // Taux d'intérêt
+        double dIntAnnuels; // Intérêts annuels
 
         for (int i = 0; i < 3; i++){
-            unCompte = new ClasseMethRenVal();
+            unCompte = new ClasseMethRenVal(); // Création d'un objet de type ClasseMethRenVal
 
-            unCompte.sPatro = "" +
+            unCompte.sPatro = "" + // Génération d'un nom aléatoire
             (char) (monGenAlea.nextInt(26) + 'A') +
             (char) (monGenAlea.nextInt(26) + 'a') +
             (char) (monGenAlea.nextInt(26) + 'a');
-        unCompte.iId = monGenAlea.nextInt(10000);
-        unCompte.dSolde = monGenAlea.nextInt(10000);
+        unCompte.iId = monGenAlea.nextInt(10000); // Génération d'un identifiant aléatoire
+        unCompte.dSolde = monGenAlea.nextInt(10000); // Génération d'un solde aléatoire
 
-        unCompte.afficher();
+        unCompte.afficher(); // Affichage des informations du compte
 
-        dTxInt = monGenAlea.nextInt(5);
-        dIntAnnuels = unCompte.lireInterets(dTxInt);
+        dTxInt = monGenAlea.nextInt(5); // Génération d'un taux d'intérêt aléatoire
+        dIntAnnuels = unCompte.lireInterets(dTxInt); // Calcul des intérêts annuels
         
         System.out.print("Montant des intérêts annuels : ");
-        System.out.println(dIntAnnuels);
+        System.out.println(dIntAnnuels); // Affichage des intérêts annuels
         System.out.println();
         } 
     }
