014f7eae834c9bf305ba8bd1bbe0f6758bf7b1d2
bindat <maximerigaud66@gmail.com>
2026-01-03 21:04:01 +0100

    Amélioration des commentaires dans la classe ShowPicture pour clarifier le code

--- Pour les nuls Java/ShowPicture.java
+++ Pour les nuls Java/ShowPicture.java
@@ -1,17 +1,17 @@
-import javax.swing.JFrame;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import javax.swing.JFrame; // Importation des classes JFrame, ImageIcon et JLabel
+import javax.swing.ImageIcon; // pour créer une fenêtre graphique et afficher une image
+import javax.swing.JLabel; // Importation de la classe JLabel pour afficher l'image dans la fenêtre
 
 class ShowPicture {
     
     public static void main(String args[]) {
-        JFrame frame = new JFrame();
-        ImageIcon icon = new ImageIcon("pourlesnuls.jpg");
-        JLabel label = new JLabel(icon);
+        JFrame frame = new JFrame(); // Création d'une nouvelle fenêtre JFrame
+        ImageIcon icon = new ImageIcon("pourlesnuls.jpg"); // Chargement de l'image "pourlesnuls.jpg" dans un objet ImageIcon
+        JLabel label = new JLabel(icon); // Création d'un JLabel pour contenir l'image
 
-        frame.add(label);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        frame.add(label); // Ajout du JLabel à la fenêtre JFrame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Définition de l'opération de fermeture de la fenêtre
+        frame.pack(); // Ajustement de la taille de la fenêtre en fonction de la taille de l'image
+        frame.setVisible(true); // Rendre la fenêtre visible
     }
 }
