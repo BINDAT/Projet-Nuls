8904dec77bc95b17656e3442e3c8f1eaa20969d6
bindat <maximerigaud66@gmail.com>
2026-01-05 21:13:46 +0100

    Ajout de la configuration de lancement pour la classe ShowPicture dans launch.json

--- Pour les nuls Java/annexe code java/JFrame.java
+++ Pour les nuls Java/annexe code java/JFrame.java
@@ -1,16 +1,16 @@
 public class JFrame {
-    int height;
-    int width;
-    public Component add() ...
-    public void setDefaultCloseOperation() ...
-    public void pack() ...
-    public void setVisible() ...
+    int height; // hauteur de la fenêtre
+    int width; // largeur de la fenêtre
+    public Component add() ... // ajoute un composant à la fenêtre
+    public void setDefaultCloseOperation() ... // définit l'opération de fermeture
+    public void pack() ... // ajuste la taille de la fenêtre en fonction des composants
+    public void setVisible() ... // rend la fenêtre visible
     ...
     /** 
      * Construit un nouvel objet Frame invisible par défaut.
     */
    public JFrame(){
     ...
-   }
+   } // constructeur
    ...
 }
