b13642bfc78d284a5e68d6a85ea408009d3c6fa3
bindat <maximerigaud66@gmail.com>
2026-01-18 21:09:38 +0100

    Ajout de plusieurs fichiers C affichant des messages de bienvenue

---
+++ Pour les nuls C/Livre2C/annexe code c/an0302.c
@@ -0,0 +1,10 @@
+/* Auteur : Dan Gookin
+ Programme affichant un message (commentaire multiligne) */
+
+#include <stdio.h>  /* Requis pour pouvoir utilser puts() */
+
+int main()
+{
+    puts("Salut humain."); /* Affiche le texte */
+    return 0;
+}
---
+++ Pour les nuls C/Livre2C/annexe code c/an0303.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("Salut humain."); // Affiche le texte (monoligne)
+    return 0;
+}
