9ef6e8ed4e8289dd64113727dde6bfe0cc1a70b8
bindat <maximerigaud66@gmail.com>
2026-01-19 21:18:39 +0100

    Ajout de commentaires explicatifs dans le fichier ex0201.c pour clarifier le code

--- Pour les nuls C/Livre2C/ex0201.c
+++ Pour les nuls C/Livre2C/ex0201.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> // Inclusion de la bibliothèque standard d'entrée/sortie
 
-int main()
+int main() // Fonction principale du programme
 {
-    puts("Bienvenue aux humains.");
-    return(0);
+    puts("Bienvenue aux humains."); // Affiche un message de bienvenue
+    return(0); // Indique que le programme s'est terminé avec succès
 }
