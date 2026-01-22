9d1952daa51c7daa63a4ef338b0827f08d279a72
bindat <maximerigaud66@gmail.com>
2026-01-22 21:03:05 +0100

    Ajout de commentaires explicatifs dans le fichier ex0401.c pour clarifier le code

--- Pour les nuls C/Livre2C/ex0401.c
+++ Pour les nuls C/Livre2C/ex0401.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> // Inclusion de la bibliothèque standard pour les entrées/sorties
 
 int main()
 {
-    puts("Ne viens pas m'importuner maintenant. J'ai deu travail.");
-    return(0);
+    puts("Ne viens pas m'importuner maintenant. J'ai deu travail."); // Affiche un message à l'écran
+    return(0); // Indique que le programme s'est terminé avec succès
 }
