bc534841afb4ad9382710314bd09d84b3883dbcd
bindat <maximerigaud66@gmail.com>
2026-01-24 21:02:23 +0100

    Ajout du fichier ex0404.c avec une fonction main affichant un poème Ajout du fichier ex0405.c

---
+++ Pour les nuls C/Livre2C/ex0404.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Inclusion de la bibliothèque standard d'entrée/sortie
+
+int main() // Début de la fonction principale
+{
+    puts("Une souris verte,"); // Affiche la première ligne
+    puts("qui courait dans l'herbe."); // Affiche la deuxième ligne
+    puts("Je l'attrape par la queue,"); // Affiche la troisième ligne
+    puts("Je la montre à ces messieurs."); // Affiche la quatrième ligne
+    return(0); // Indique que le programme s'est terminé avec succès
+}
