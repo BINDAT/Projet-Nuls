2b6e950c7b4db17bae7d5dc06720cc5ad56cdd41
bindat <maximerigaud66@gmail.com>
2026-01-24 21:10:42 +0100

    Correction des commentaires et ajout d'un point-virgule manquant dans le fichier ex0408.c

--- Pour les nuls C/Livre2C/ex0408.c
+++ Pour les nuls C/Livre2C/ex0408.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> // Inclusion de la bibliothèque standard d'entrée/sortie
 
-int main()
+int main() // Fonction principale du programme
 {
-    puts("Ce programme fait BOUM !")
-    return(0);
+    puts("Ce programme fait BOUM !"); // Affiche le message à l'écran 
+    return(0); // Indique que le programme s'est terminé avec succès
 }
