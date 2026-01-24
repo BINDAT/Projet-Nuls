c496491bf881cdb4c74b6187adefb510417a4dd8
bindat <maximerigaud66@gmail.com>
2026-01-24 21:07:49 +0100

    Ajout des fichiers ex0406.c et ex0407.c avec des fonctions main affichant un message et un poème

---
+++ Pour les nuls C/Livre2C/ex0406.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Inclusion de la bibliothèque standard d'entrée/sortie
+
+int main() // Début de la fonction principale
+{
+    puts("Le mot de passe secret est :"); // Affiche la première ligne
+    puts("Spatula."); // Affiche la deuxième ligne
+    return(0); // Indique que le programme s'est terminé avec succès
+}
