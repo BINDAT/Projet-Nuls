b36636b9d66f0de220e064e60964de680e09e60c
bindat <maximerigaud66@gmail.com>
2026-01-25 21:13:56 +0100

    Ajout des fichiers ex0409.c et ex0410.c avec une fonction main affichant un message

---
+++ Pour les nuls C/Livre2C/ex0409.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Inclusion de la bibliothèque standard d'entrée/sortie
+
+int main() // Fonction principale du programme
+{
+    puts("Ce programme fait BOUM !") 
+    return(0); // Indique que le programme s'est terminé avec succès
+}
