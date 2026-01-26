cb7109ff76f3395cb7ee68095a8986744d92ae47
bindat <maximerigaud66@gmail.com>
2026-01-26 21:23:19 +0100

    Ajout de commentaires explicatifs dans le fichier ex4011.c

--- Pour les nuls C/Livre2C/ex4011.c
+++ Pour les nuls C/Livre2C/ex4011.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> // entête standard pour les entrées/sorties
 
-int main()
+int main() // fonction principale
 {
-    printf("Étranger dans son propre pays.");
-    return(0);
+    printf("Étranger dans son propre pays."); // affichage du message à l'écran
+    return(0); // valeur de retour de la fonction principale
 }
