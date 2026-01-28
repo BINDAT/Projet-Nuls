e40fc20420806cb0ab89c8c21dabd7815e2825c3
bindat <maximerigaud66@gmail.com>
2026-01-28 13:14:40 +0100

    Ajout de commentaires explicatifs dans le fichier ex0501.c

--- Pour les nuls C/Livre2C/ex0501.c
+++ Pour les nuls C/Livre2C/ex0501.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include <stdio.h> // Inclusion de la bibliothèque standard d'entrée/sortie
 
 int main()
 {
-    printf("La valeur %d est un entier\n",986);
-    printf("La valeur %f est un flottant\n",98.6);
-    return(0);
+    printf("La valeur %d est un entier\n",986); // Affiche un entier
+    printf("La valeur %f est un flottant\n",98.6); // Affiche un flottant
+    return(0); // Indique que le programme s'est terminé avec succès
 }
