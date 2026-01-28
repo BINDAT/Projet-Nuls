a79d5359d402a68a0df0483927271b9e738141e9
bindat <maximerigaud66@gmail.com>
2026-01-28 13:09:34 +0100

    Correction d'une erreur de syntaxe dans le fichier ex0416.c : remplacement de 'pritf' par 'printf'.

--- Pour les nuls C/Livre2C/ex0416.c
+++ Pour les nuls C/Livre2C/ex0416.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> // Inclusion de la bibliothèque standard d'entrée/sortie
 
-int main()
+int main() // Fonction principale du programme
 {
-    pritf("Ceci est une erreur pour Monsieur le lieur.");
-    return(0);
+    pritf("Ceci est une erreur pour Monsieur le lieur."); // Ligne avec une erreur de syntaxe (faut de frappe dans 'printf')
+    return(0); // Retourne 0 pour indiquer que le programme s'est terminé avec succès
 }
