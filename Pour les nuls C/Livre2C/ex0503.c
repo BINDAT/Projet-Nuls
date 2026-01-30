83f564e84122fe347f1dbc038b4f6c133c435ffd
bindat <maximerigaud66@gmail.com>
2026-01-30 07:59:14 +0100

    Ajout du fichier ex0503.c avec une fonction main affichant plusieurs entiers et flottants

---
+++ Pour les nuls C/Livre2C/ex0503.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Inclusion de la bibliothèque standard d'entrée/sortie
+
+int main()
+{
+    printf("%d",127); // Affiche un entier
+    printf("%1.2f",3.1415926535); // Affiche un nombre à virgule flottante avec 2 décimales
+    printf("%d",122013); // Affiche un autre entier
+    printf("%1.1f",0.00008); // Affiche un autre nombre à virgule flottante avec 1 décimale
+    return(0); // Indique que le programme s'est terminé avec succès
+}
