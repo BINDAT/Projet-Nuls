ed41f0533de5b0d094725a7950ad9b39d5a2caba
bindat <maximerigaud66@gmail.com>
2026-01-30 10:29:54 +0100

    Ajout du fichier ex0504.c avec une fonction main effectuant des opérations arithmétiques

---
+++ Pour les nuls C/Livre2C/ex0504.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("Valeur initiales : 8 et 2");
+    printf("Addition = %d\n",8+2);
+    printf("Soustraction = %d\n",8-2);
+    printf("Multiplication = %d\n",8*2);
+    printf("Division = %d\n",8/2);
+    return(0);
+}
