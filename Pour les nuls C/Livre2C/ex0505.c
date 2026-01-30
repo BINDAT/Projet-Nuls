e1d15726a7a061033f0cfaea7f48eade60529b9a
bindat <maximerigaud66@gmail.com>
2026-01-30 10:32:45 +0100

    Ajout du fichier ex0505.c avec une fonction main effectuant des opérations arithmétiques et affichant des résultats

---
+++ Pour les nuls C/Livre2C/ex0505.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("Valeur initiales : 8 et 2");
+    printf("Addition = %d\n",8+2);
+    printf("Soustraction = %d\n",8-2);
+    printf("Multiplication = %d\n",8*2);
+    printf("Division = %d\n",8/2);
+    printf("Les flottants 456.98 et 213.4 font\n %f", 456.98+213.4);
+    return(0);
+}
