0ef0d4b4a55f023a02dda9b9fba5948b3444bde2
bindat <maximerigaud66@gmail.com>
2026-01-30 10:37:26 +0100

    Ajout des fichiers ex0506.c et ex0507.c avec des fonctions main effectuant des opérations arithmétiques et affichant des résultats

---
+++ Pour les nuls C/Livre2C/ex0506.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("Valeur initiales : 8 et 2");
+    printf("Addition = %d\n",8+2);
+    printf("Soustraction = %d\n",8-2);
+    printf("Multiplication = %d\n",8*2);
+    printf("Division = %d\n",8/2);
+    printf("Les flottants 456.98 et 213.4 font\n %f", 456.98+213.4);
+    printf("Les entiers 8 4 et 25 multipliés font\n %d",8*4*25);
+    return(0);
+}
---
+++ Pour les nuls C/Livre2C/ex0507.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("L'équation mathématique 0+50*1-60-60*0+10 fait %d",0+50*1-60-60*0+10);
+    return(0);
+}
