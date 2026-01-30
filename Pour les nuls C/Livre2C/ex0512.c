8886b1e93dae7e68574210cae2f65b2ed91f927a
bindat <maximerigaud66@gmail.com>
2026-01-30 10:47:08 +0100

    Ajout du fichier ex0512.c avec une fonction main affichant des valeurs entières

---
+++ Pour les nuls C/Livre2C/ex0512.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("La valeur est %d\n", 5);
+    printf("et %d est bien la valeur.\n", 5);
+    printf("Ce n'est pas %d,\n", 5+1);
+    printf("ni %d non plus.", 5-1);
+    printf("Non, la valeur rest %d.\n", 5);
+    return(0);
+}
