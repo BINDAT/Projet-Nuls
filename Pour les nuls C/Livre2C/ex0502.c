b7b4df4a2962675d3f4c22940245e2739f4ca292
bindat <maximerigaud66@gmail.com>
2026-01-30 07:56:37 +0100

    Ajout de commentaires explicatifs dans le fichier ex0502.c pour clarifier les affichages

--- Pour les nuls C/Livre2C/ex0502.c
+++ Pour les nuls C/Livre2C/ex0502.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include <stdio.h> // Inclusion de la bibliothèque standard d'entrée/sortie
 
 int main()
 {
-    printf("%d",127);
-    printf("%f",3.1415926535);
-    printf("%d",122013);
-    printf("%f",0.00008);
+    printf("%d",127); // Affiche un entier
+    printf("%f",3.1415926535); // Affiche un nombre à virgule flottante
+    printf("%d",122013); // Affiche un autre entier
+    printf("%f",0.00008); // Affiche un autre nombre à virgule flottante
+    return(0); // Indique que le programme s'est terminé avec succès
 }
