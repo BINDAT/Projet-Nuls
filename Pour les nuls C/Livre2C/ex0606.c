c5db28fce609c10160c644cb693b2341d84c59d4
bindat <maximerigaud66@gmail.com>
2026-02-03 13:04:25 +0100

    Ajout du fichier ex0606.c avec une fonction main affichant des calculs basés sur les constantes blorf et GLORKUS

---
+++ Pour les nuls C/Livre2C/ex0606.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define blorf 22
+#define GLORKUS 16
+
+int main()
+{
+    printf("La valeur de blorf est %d.\n",blorf);
+    printf("La valeur de blorf +16 est %d.\n",blorf + GLORKUS);
+    printf("La valeur de blorf puissance 2 est %d.\n",blorf *blorf);
+}
