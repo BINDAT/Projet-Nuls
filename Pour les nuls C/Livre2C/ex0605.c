6aec7396581f5f02b0e2d45d8cfec360ee456d39
bindat <maximerigaud66@gmail.com>
2026-02-04 21:21:28 +0100

    Ajout de commentaires explicatifs pour clarifier l'utilisation de la constante blorf dans ex0605.c

--- Pour les nuls C/Livre2C/ex0605.c
+++ Pour les nuls C/Livre2C/ex0605.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <stdio.h> // pour printf et les autres fonctions d'entrée/sortie
 
-#define blorf 22
+#define blorf 22 // blorf est défini comme 22, mais le symbole de division (/) est présent après le nombre, ce qui peut causer des problèmes lors de l'utilisation de blorf dans des expressions.
 
 int main()
 {
-    printf("La valeur de blorf est %d.\n",blorf);
-    printf("La valeur de blorf +16 est %d.\n",blorf + 16);
-    printf("La valeur de blorf puissance 2 est %d.\n",blorf *blorf);
+    printf("La valeur de blorf est %d.\n",blorf); // Affiche la valeur de blorf, qui est 22.
+    printf("La valeur de blorf +16 est %d.\n",blorf + 16); // Affiche la valeur de blorf plus 16, ce qui est 38.
+    printf("La valeur de blorf puissance 2 est %d.\n",blorf *blorf); // Affiche la valeur de blorf multipliée par elle-même, ce qui est 484.
 }
