a2f95fbeae6bdcb961350aa059c04dddbe0f64ce
bindat <maximerigaud66@gmail.com>
2026-02-05 17:25:05 +0100

    Ajout de commentaires explicatifs pour clarifier les constantes blorf et GLORKUS dans ex0606.c

--- Pour les nuls C/Livre2C/ex0606.c
+++ Pour les nuls C/Livre2C/ex0606.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> // pour printf et les autres fonctions d'entrée/sortie
 
-#define blorf 22
-#define GLORKUS 16
+#define blorf 22 // blorf est défini comme une constante entière avec la valeur 22
+#define GLORKUS 16 // GLORKUS est défini comme une constante entière avec la valeur 16
 
 int main()
 {
-    printf("La valeur de blorf est %d.\n",blorf);
-    printf("La valeur de blorf +16 est %d.\n",blorf + GLORKUS);
-    printf("La valeur de blorf puissance 2 est %d.\n",blorf *blorf);
+    printf("La valeur de blorf est %d.\n",blorf); // Affiche la valeur de blorf
+    printf("La valeur de blorf +16 est %d.\n",blorf + GLORKUS); // Affiche la valeur de blorf plus GLORKUS (22 + 16)
+    printf("La valeur de blorf puissance 2 est %d.\n",blorf *blorf); // Affiche la valeur de blorf multipliée par elle-même (22 * 22)
 }
