2541fd42268261aee51a8f601192d0febeeb801c
bindat <maximerigaud66@gmail.com>
2026-02-06 20:50:38 +0100

    upload code ex0608.c

---
+++ Pour les nuls C/Livre2C/ex0608.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int Ananias, Azarias, Misael;
+
+    Ananias = 701;
+    Azarias = 709;
+    Misael = 719;
+
+    printf("Ananias est %d\nAzarias est %d\nMisael est %d\n",Ananias,Azarias,Misael);
+    return(0);
+}
