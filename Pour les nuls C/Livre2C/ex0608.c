8dbbe331fe3f7500229a524eddea4f8843af8dc9
bindat <maximerigaud66@gmail.com>
2026-02-07 21:03:40 +0100

    update commentaire ex0608.c

--- Pour les nuls C/Livre2C/ex0608.c
+++ Pour les nuls C/Livre2C/ex0608.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> // en-tête de la bibliothèque standard d'entrée/sortie
 
 int main()
 {
-    int Ananias, Azarias, Misael;
+    int Ananias, Azarias, Misael; // déclaration de trois variables de type entier
 
-    Ananias = 701;
-    Azarias = 709;
-    Misael = 719;
+    Ananias = 701; // affectation de la valeur 701 à la variable Ananias
+    Azarias = 709; // affectation de la valeur 709 à la variable Azarias
+    Misael = 719; // affectation de la valeur 719 à la variable Misael
 
-    printf("Ananias est %d\nAzarias est %d\nMisael est %d\n",Ananias,Azarias,Misael);
-    return(0);
+    printf("Ananias est %d\nAzarias est %d\nMisael est %d\n",Ananias,Azarias,Misael); // affichage des valeurs des variables Ananias, Azarias et Misael à l'aide de la fonction printf
+    return(0); // retour de la fonction main, indiquant que le programme s'est terminé avec succès
 }
