ee0d7dd2587e8df4e1c9312c79663ae493271dfe
bindat <maximerigaud66@gmail.com>
2026-02-11 17:42:52 +0100

    update code ex710.c et ex0711.c

---
+++ Pour les nuls C/Livre2C/ex0710.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // en-tête de la bibliothèque standard d'entrée/sortie
+
+int main()
+{
+    char prompt[] = "Frappez Entrée pour exploser :"; // déclaration et initialisation d'un tableau de caractères pour le message d'invite
+
+    printf("%s",prompt); // affichage du message d'invite
+    getchar(); // attente de l'appui sur la touche Entrée
+    return(0); // indique que le programme s'est terminé avec succès
+}
