99befac6ce45539356c8e868b7436d7c6b5862c0
bindat <maximerigaud66@gmail.com>
2026-02-15 21:11:26 +0100

    update commentaire ex0804.c

--- Pour les nuls C/Livre2C/ex0804.c
+++ Pour les nuls C/Livre2C/ex0804.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
+#include <stdio.h> // pour les fonctions d'entrée/sortie
 
 int main()
 {
-    int premier, second;
+    int premier, second; // déclaration de deux variables de type entier
 
     printf("Indiquez la valeur de premier : ");
-    scanf("%d", &second);
+    scanf("%d", &premier); // lecture de la valeur de 'premier' à partir de l'entrée standard et stockage dans la variable 'premier'
     printf("Indiquez la valeur de second : ");
-    scanf("%d", &second);
+    scanf("%d", &second); // lecture de la valeur de 'second' à partir de l'entrée standard et stockage dans la variable 'second'
 
     puts("Evaluation en cours...");
-    if (premier < second)
+    if (premier < second) // condition pour vérifier si 'premier' est plus petit que 'second'
     {
         printf("%d est plus petit que %d\n", premier, second);
     }
-    if (premier > second)
+    if (premier > second) // condition pour vérifier si 'premier' est plus grand que 'second'
     {
         printf("%d est plus grand que %d\n", premier, second);
     }
