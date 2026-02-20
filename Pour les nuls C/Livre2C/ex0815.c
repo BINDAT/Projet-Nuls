f7cd621674cca1cec36e284034344773bb1010dc
bindat <maximerigaud66@gmail.com>
2026-02-20 21:40:16 +0100

    ajout du fichier ex0815.c avec un exemple de condition pour vérifier la position d'une cible

---
+++ Pour les nuls C/Livre2C/ex0815.c
@@ -0,0 +1,19 @@
+#include <stdio.h> // pour les fonctions d'entrée/sortie
+
+int main()
+{
+    int coordonnee; // déclaration d'une variable de type entier pour stocker la coordonnée
+
+    printf("Coordonnée de la cible : ");
+    scanf("%d", &coordonnee); // lecture de la valeur de 'coordonnee' à partir de l'entrée standard et stockage dans la variable 'coordonnee'
+    
+    if( coordonnee > 5 || coordonnee < -5) // condition pour vérifier si la coordonnée est en dehors de l'intervalle [-5, 5]
+    {
+        puts("Assez proche !");
+    }
+    else // condition pour vérifier si la coordonnée est en dehors de l'intervalle [-5, 5]
+    {
+        puts("La cible est encore loin !");
+    }
+    return(0); // fin du programme
+}
