7f68218492782ef6ed25aa2ece5b3b9c550303e5
bindat <maximerigaud66@gmail.com>
2026-02-25 21:55:45 +0100

    ajout de commentaires explicatifs dans ex0818.c pour clarifier le code de gestion des erreurs

--- Pour les nuls C/Livre2C/ex0818.c
+++ Pour les nuls C/Livre2C/ex0818.c
@@ -2,27 +2,27 @@
 
 int main()
 {
-    char code;
+    char code; // Déclaration d'une variable de type caractère pour stocker le code d'erreur
 
-    printf("Indiquez le code erreur (a-c): ");
-    scanf("%c", &code);
+    printf("Indiquez le code erreur (a-c): "); // Affichage d'une invite à l'utilisateur pour entrer un code d'erreur
+    scanf("%c", &code); // Lecture du caractère entré par l'utilisateur et stockage dans la variable 'code'
 
-    switch (code)
+    switch (code) // Utilisation d'une structure de contrôle 'switch' pour traiter le code d'erreur
     {
     case 'a':
-    case 'A':
+    case 'A': // Cas pour les codes 'a' ou 'A'
         puts("Erreur disque, vous n'y pouvez rien.");
         break;
     
     case 'b':
-    case 'B': 
+    case 'B': // Cas pour les codes 'b' ou 'B'
         puts("Format invalide, appelez votre avocat.");
         break;
     case 'c':
-    case 'C':
+    case 'C': // Cas pour les codes 'c' ou 'C'
         puts("Nom de fichier incorrect, spank it.");
         break;
-    default:
+    default: // Cas par défaut si le code d'erreur ne correspond à aucun des cas précédents
         puts("Haha, ni A, ni B, ni C ?");
         break;
     }
