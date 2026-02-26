20c414fb7bd96d6661294b79642df5df88d60562
bindat <maximerigaud66@gmail.com>
2026-02-26 18:54:42 +0100

    ajout du fichier ex0820.c avec un exemple de gestion des codes d'erreur et mise à jour des messages d'invite

--- Pour les nuls C/Livre2C/ex0820.c
+++ Pour les nuls C/Livre2C/ex0820.c
@@ -4,23 +4,24 @@ int main()
 {
     char code; // Déclaration d'une variable de type caractère pour stocker le code d'erreur
 
-    printf("Indiquez le code erreur (A-C): "); // Affichage d'une invite à l'utilisateur pour entrer un code d'erreur
+    printf("Indiquez le code erreur (a-c): "); // Affichage d'une invite à l'utilisateur pour entrer un code d'erreur
     scanf("%c", &code); // Lecture du caractère entré par l'utilisateur et stockage dans la variable 'code'
 
     switch (code) // Utilisation d'une structure de contrôle 'switch' pour traiter le code d'erreur
     {
-    case 'A': // Cas pour les codes 'A'
+    case 'a':
+    case 'A': // Cas pour les codes 'a' ou 'A'
         puts("Erreur disque, vous n'y pouvez rien.");
         break;
     
-    case 'B': // Cas pour les codes 'B'
+    case 'b':
+    case 'B': // Cas pour les codes 'b' ou 'B'
         puts("Format invalide, appelez votre avocat.");
         break;
-    
-    case 'C': // Cas pour les codes 'C'
+    case 'c':
+    case 'C': // Cas pour les codes 'c' ou 'C'
         puts("Nom de fichier incorrect, spank it.");
         break;
-    
     default: // Cas par défaut si le code d'erreur ne correspond à aucun des cas précédents
         puts("Haha, ni A, ni B, ni C ?");
         break;
