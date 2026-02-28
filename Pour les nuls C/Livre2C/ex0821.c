85b811d8ae2ab446833407d4b954aaf2d1810c6b
bindat <maximerigaud66@gmail.com>
2026-02-28 20:49:31 +0100

    ajout de commentaires explicatifs dans ex0821.c pour clarifier le code

--- Pour les nuls C/Livre2C/ex0821.c
+++ Pour les nuls C/Livre2C/ex0821.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
+#include <stdio.h> // inclusion de la bibliothèque standard d'entrée/sortie
 
-int main()
+int main() // fonction principale
 {
-    int a, b, leplusgrand;
+    int a, b, leplusgrand; // déclaration des variables
     
     printf("Indiquez une valeur A: ");
-    scanf("%d", &a);
+    scanf("%d", &a); // lecture de la première valeur
     printf("Indiquez une autre valeur B: ");
-    scanf("%d", &b);
+    scanf("%d", &b); // lecture de la deuxième valeur
 
-    leplusgrand = (a > b) ? a : b;
+    leplusgrand = (a > b) ? a : b; // opérateur ternaire pour trouver le plus grand
     printf("La valeur %d est plus grande.\n", leplusgrand);
-    return(0);
+    return(0); // fin du programme
 }
