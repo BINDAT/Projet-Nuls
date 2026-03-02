28412c6975fcd00b0d756701bc4a3c2ce1a6f68d
bindat <maximerigaud66@gmail.com>
2026-03-02 15:02:52 +0100

    ajout du fichier ex0901.c avec un exemple de boucle et affichage de messages

---
+++ Pour les nuls C/Livre2C/ex0901.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // inclusion de la bibliothèque standard d'entrée/sortie
+
+int main()
+{
+    int x;
+
+    for (x = 0; x < 10; x++) // boucle qui s'exécute 10 fois
+    {
+        puts("Ne vous l'ais-je pas encore dit ?"); // affichage de la phrase à chaque itération
+    }
+    return(0); // fin du programme
+}
