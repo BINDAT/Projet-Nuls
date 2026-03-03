fdd2c1cf1d6860ca9646089a3dbc7eddf7df0caa
bindat <maximerigaud66@gmail.com>
2026-03-03 21:50:17 +0100

    correction du commentaire dans ex0907.c pour refléter la logique de la boucle

--- Pour les nuls C/Livre2C/ex0907.c
+++ Pour les nuls C/Livre2C/ex0907.c
@@ -3,7 +3,7 @@
 int main()
 {
     int compteur;
-    for (compteur = 25; compteur >= 0; compteur -= 1) // boucle qui s'exécute de 3 à 100, en incrémentant de 3 à chaque itération
+    for (compteur = 25; compteur >= 0; compteur -= 1) // boucle qui s'exécute de 25 à 0, en décrémentant de 1 à chaque itération
     {
         printf("%d\t", compteur); // affichage de la valeur de compteur à chaque itération, suivi d'un saut de ligne
     }
