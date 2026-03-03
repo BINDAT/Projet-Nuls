90efea7447571f3dd5836af920a1baac9faa1d11
bindat <maximerigaud66@gmail.com>
2026-03-03 21:49:59 +0100

    correction de la variable dans ex0906.c et ajout du fichier ex0907.c avec un exemple de boucle décomptant de 25 à 0

--- Pour les nuls C/Livre2C/ex0906.c
+++ Pour les nuls C/Livre2C/ex0906.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    int duo;
-    for (duo = 3; duo <= 100; duo += 3) // boucle qui s'exécute de 3 à 100, en incrémentant de 3 à chaque itération
+    int trio;
+    for (trio = 3; trio <= 100; trio += 3) // boucle qui s'exécute de 3 à 100, en incrémentant de 3 à chaque itération
     {
-        printf("%d\t", duo); // affichage de la valeur de duo à chaque itération, suivi d'un saut de ligne
+        printf("%d\t", trio); // affichage de la valeur de trio à chaque itération, suivi d'un saut de ligne
     }
     putchar('\n'); // saut de ligne après chaque affichage
     return(0); // fin du programme
