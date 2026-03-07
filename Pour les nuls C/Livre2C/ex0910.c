899ce80efec19bb9cf31ac644efa6882ef7924c6
bindat <maximerigaud66@gmail.com>
2026-03-07 20:48:48 +0100

    ajout du fichier ex0910.c avec un exemple de boucle affichant les lettres de l'alphabet de Z à A

---
+++ Pour les nuls C/Livre2C/ex0910.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // Pour le fonctionnement de printf et putchar, qui sont utilisés pour afficher du texte à l'écran et pour insérer des caractères, respectivement.
+
+int main()
+{
+    char alphabet; // déclaration d'une variable de type caractère pour stocker les lettres de l'alphabet
+    for (alphabet = 'z'; alphabet >= 'a'; alphabet--) // boucle qui s'exécute de 'z' à 'a', en décrémentant de 1 à chaque itération
+    {
+        printf("%c", alphabet); // affichage de la lettre actuelle de l'alphabet à chaque itération
+    }
+    putchar('\n'); // saut de ligne après l'affichage de toutes les lettres de l'alphabet
+    return(0); // termine le programme et retourne 0 pour indiquer que tout s'est bien passé
+}
