15b4177462d5e5aa1dcb1e7311dc26c349ec0ac3
bindat <maximerigaud66@gmail.com>
2026-03-15 22:45:30 +0100

    upload code an&ex1004.c

---
+++ Pour les nuls C/Livre2C/ex1004.c
@@ -0,0 +1,29 @@
+#include <stdio.h> // pour entrée/sortie
+
+void busy();
+/* Fonction prompt() */
+
+void prompt()
+{
+    printf("C:\\DOS> ");
+}
+
+int main()
+{
+    busy();
+    return(0);
+}
+
+void busy()
+{
+    int loop;
+    char input[32];
+
+    loop=0;
+    while (loop < 5)
+    {
+        prompt();
+        fgets(input, 31, stdin);
+        loop = loop+1;
+    }
+}
