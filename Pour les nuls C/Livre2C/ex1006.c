2cacd3d48047bd7264f0bb619f939997d9e5aca6
bindat <maximerigaud66@gmail.com>
2026-03-17 21:06:16 +0100

    update commentaire de ex1006.c

--- Pour les nuls C/Livre2C/ex1006.c
+++ Pour les nuls C/Livre2C/ex1006.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
+#include <stdio.h> // pour printf et putchar
 
-void graph(int cmptr);
+void graph(int cmptr); // prototype de la fonction graph
 
-int main()
+int main() // fonction principale
 {
-    int valeur;
+    int valeur; // déclaration de la variable valeur
 
-    valeur = 2;
+    valeur = 2; // initialisation de la variable valeur
 
-    while (valeur <= 64)
+    while (valeur <= 64) // boucle tant que valeur est inférieure ou égale à 64
     {
-        graph(valeur);
-        printf("La valeur est %d\n", valeur);
-        valeur = valeur * 2;
+        graph(valeur); // appel de la fonction graph avec valeur comme argument
+        printf("La valeur est %d\n", valeur); // affichage de la valeur actuelle
+        valeur = valeur * 2; // mise à jour de la variable valeur en la multipliant par 2
     }
     return(0);
 }
 
-void graph(int cmptr)
+void graph(int cmptr) // définition de la fonction graph qui prend un entier cmptr en argument
 {
-    int x;
-    for (x = 0; x<cmptr; x=x+1)
-        putchar('*');
-    putchar('\n');
+    int x; // déclaration de la variable x
+    for (x = 0; x<cmptr; x=x+1) // boucle for qui s'exécute tant que x est inférieur à cmptr, en incrémentant x de 1 à chaque itération
+        putchar('*'); // affichage d'un caractère '*' à chaque itération de la boucle
+    putchar('\n'); // affichage d'un caractère de nouvelle ligne après la boucle for pour passer à la ligne suivante
 }
