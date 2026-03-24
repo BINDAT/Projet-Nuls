08a507c9050652c3766e9593aa17a0ac82808980
bindat <maximerigaud66@gmail.com>
2026-03-24 21:23:31 +0100

    upload code ex1101.c et ex1102.c

---
+++ Pour les nuls C/Livre2C/ex1101.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int boucle = 0; boucle <= 10; boucle++)
+    {
+        puts("Ne marchez pa sur ma pelouse!");
+    }
+    return 0;
+}
---
+++ Pour les nuls C/Livre2C/ex1102.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int boucle = 0;
+
+    while(boucle <= 10)
+    {
+        puts("Ne marchez pa sur ma pelouse!");
+        boucle++;
+    }
+    return 0;
+}
