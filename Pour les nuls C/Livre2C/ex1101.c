81ae8b8e41768b50ef24f8b6df1db2c7c8418610
bindat <maximerigaud66@gmail.com>
2026-03-25 20:51:30 +0100

    update commentaire de ex1101.c

--- Pour les nuls C/Livre2C/ex1101.c
+++ Pour les nuls C/Livre2C/ex1101.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <stdio.h> // Bibliothèque pour faire fonction puts
 
 int main()
 {
-    int boucle;
+    int boucle; // Déclaration d'une variable entière boucle
 
-    for(boucle = 0; boucle <= 10; boucle++)
+    for(boucle = 0; boucle <= 10; boucle++) //Utilisation de la variable boucle pour 10 executions
     {
-        puts("Ne marchez pa sur ma pelouse!");
+        puts("Ne marchez pa sur ma pelouse!"); //affichage du message fois 10 par la boucle
     }
-    return 0;
+    return 0; //renvoi 0 dans le terminal pour indiquer que cela sais terminé sans soucis.
 }
