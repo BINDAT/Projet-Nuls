04dcfecbac874b13e34e29fb852d2c3c871f15a0
bindat <maximerigaud66@gmail.com>
2026-03-26 21:06:11 +0100

    update comment de ex1102.c

--- Pour les nuls C/Livre2C/ex1102.c
+++ Pour les nuls C/Livre2C/ex1102.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> //bibliothèque pour gerer l'affichage
 
-int main()
+int main() // fonction principale
 {
-    int boucle = 0;
+    int boucle = 0; // déclaration de la variable et sa valeur
 
-    while(boucle <= 10)
+    while(boucle <= 10) // une boucle qui utilise la variable
     {
         puts("Ne marchez pa sur ma pelouse!");
-        boucle++;
+        boucle++; // incrémentation de la boucle
     }
     return 0;
 }
