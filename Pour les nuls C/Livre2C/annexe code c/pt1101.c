ee2993b569f36d871c146b1b7c0d64a1e58e782e
bindat <maximerigaud66@gmail.com>
2026-03-27 21:10:29 +0100

    update commentaire de pt1101.c

--- Pour les nuls C/Livre2C/annexe code c/pt1101.c
+++ Pour les nuls C/Livre2C/annexe code c/pt1101.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+#include <stdio.h> // Permet un affiche de puts
 
 int main()
 {
-    for(int boucle = 0; boucle <= 10; boucle++, puts("Ne marchez pa sur ma pelouse!"));
-    return 0;
+    for(int boucle = 0; boucle <= 10; boucle++, puts("Ne marchez pa sur ma pelouse!")); // En paramètre des instructions sont entrées autre que celle de boucle.
+    return 0; //retourne 0 à l'affichage à la fin du programme.
 }
+
+/*
+Observation : Ce code tourne 10 fois par l'effet de la boucle pour afficher 10 fois un message. Ont peut remarquer que la variable et sa valeur
+sont initialisées directement dans la boucle for.
+*/
