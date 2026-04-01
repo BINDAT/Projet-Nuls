d100e90f24e7b99958339fe61d9bcfaf3225f2ff
bindat <maximerigaud66@gmail.com>
2026-04-01 21:10:13 +0200

    ajout de commentaires explicatifs dans ex1106.c pour clarifier le code

--- Pour les nuls C/Livre2C/ex1106.c
+++ Pour les nuls C/Livre2C/ex1106.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <stdio.h> // pour les fonctions d'entrée/sortie
 
 int main()
 {
-    int a,b;
+    int a,b; // déclaration de deux variables entières
 
-    b=16;
+    b=16; // affectation de la valeur 16 à la variable b
     printf("Avant, a ne change pas encore et b = %d\n", b);
-    a= ++b;
-    printf("Après, a = %d et b = %d\n", a, b);
-    return(0);
+    a= ++b; // pré-incrémentation : b est d'abord incrémenté, puis sa valeur est affectée à a
+    printf("Après, a = %d et b = %d\n", a, b); // affichage des valeurs de a et b après l'opération
+    return(0); // fin du programme, retourne 0 pour indiquer que tout s'est bien passé
 }
