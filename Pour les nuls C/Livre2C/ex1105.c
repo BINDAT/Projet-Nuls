d0989e2093c2a69c321be41a6cc15a5bc1496270
bindat <maximerigaud66@gmail.com>
2026-04-02 21:05:29 +0200

    ajout de commentaires explicatifs dans ex1105.c pour clarifier le code

--- Pour les nuls C/Livre2C/ex1105.c
+++ Pour les nuls C/Livre2C/ex1105.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <stdio.h> // pour les fonctions d'entrée/sortie
 
-int main()
+int main() // fonction principale du programme
 {
-    int a,b;
+    int a,b; // déclaration de deux variables entières a et b
 
-    b=16;
-    printf("Avant, a ne change pas encore et b = %d\n", b);
-    a=b++;
-    printf("Après, a = %d et b = %d\n", a, b);
-    return(0);
+    b=16; // affectation de la valeur 16 à la variable b
+    printf("Avant, a ne change pas encore et b = %d\n", b); // affichage de la valeur de b avant l'opération d'incrémentation
+    a=b++; // affectation de la valeur de b à a, puis incrémentation de b de 1 (post-incrémentation)
+    printf("Après, a = %d et b = %d\n", a, b); // affichage des valeurs de a et b après l'opération d'incrémentation
+    return(0); // fin de la fonction principale, retour de la valeur 0 pour indiquer que le programme s'est terminé avec succès
 }
