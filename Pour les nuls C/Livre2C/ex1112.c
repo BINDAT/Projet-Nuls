a6c702ee09b4b2478af57de830cae0ff7e635884
bindat <maximerigaud66@gmail.com>
2026-04-07 19:34:28 +0200

    update code ex1112.c

---
+++ Pour les nuls C/Livre2C/ex1112.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+    double nombre, valeur, boucle;
+    nombre = 2;
+    for (boucle = 0; boucle <=10; boucle++)
+    {
+        printf("Nombre = %.2f, pow = %.2f cumul = %.2f", nombre, valeur, nombre = pow(valeur[boucle]));
+    }
+   return(0); 
+}
