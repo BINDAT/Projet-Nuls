fb3c402b21a3fb2dd60cd49f325c65ecfdbc831e
bindat <maximerigaud66@gmail.com>
2026-04-07 21:21:16 +0200

    mise à jours task

--- Pour les nuls C/Livre2C/ex1112.c
+++ Pour les nuls C/Livre2C/ex1112.c
@@ -9,7 +9,7 @@ int main ()
     for (boucle = 0; boucle <=10; boucle++)
     {
         valeur = pow(nombre, boucle);
-printf("Nombre %2.f tour %2.f de boucle de base voici vos puissance %2.f\n",nombre, boucle ,valeur);
+        printf("Nombre %2.f tour %2.f de boucle de base voici vos puissance %2.f\n",nombre, boucle ,valeur);
     }
    return(0); 
 }
