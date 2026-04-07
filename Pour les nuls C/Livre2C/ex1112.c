8a836680b6207ff9fdc81ea852ee94e896036767
BINDAT <136336341+BINDAT@users.noreply.github.com>
2026-04-07 20:29:40 +0200

    Update ex1112.c
    Mise à jour 2 à revoir.

--- Pour les nuls C/Livre2C/ex1112.c
+++ Pour les nuls C/Livre2C/ex1112.c
@@ -1,14 +1,15 @@
+
 #include <stdio.h>
 #include <math.h>
 
 int main ()
 {
-    double nombre, valeur, boucle;
+    float nombre, valeur, boucle;
     nombre = 2;
     for (boucle = 0; boucle <=10; boucle++)
     {
         valeur = pow(nombre, boucle);
-printf("Voici vos puissance %2.f", valeur);
+printf("Nombre %2.f tour %2.f de boucle de base voici vos puissance %2.f\n",nombre, boucle ,valeur);
     }
    return(0); 
 }
