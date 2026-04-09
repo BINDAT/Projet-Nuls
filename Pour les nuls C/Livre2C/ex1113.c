942220b02d763d7ba3c0a75d0feae3d9e0108eb9
bindat <maximerigaud66@gmail.com>
2026-04-09 21:33:13 +0200

    update commentaire de ex1113.c

--- Pour les nuls C/Livre2C/ex1113.c
+++ Pour les nuls C/Livre2C/ex1113.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <stdio.h> //Pour les entrée est sorties
 
 int main()
 {
-    float degres,radians;
+    float degres,radians; //Déclaration de deux variable float
 
-    printf("");
-    scanf("%f", &degres);
-    radians = 0.01745352925*degres;
-    printf("%.2f degres valent %.2f radians.\n", degres, radians);
+    printf("Indiquez un angle en degrés : ");
+    scanf("%f", &degres); //Prend la saisit et l'enregistre dans la var
+    radians = 0.01745352925*degres; //radians sers de recipient à calcul en prenant une val fixe est une en mémoire
+    printf("%.2f degrés valent %.2f radians.\n", degres, radians); //Affiche les degrés saisit et le radians
     return(0);
 }
