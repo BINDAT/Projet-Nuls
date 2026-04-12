862e9557f0aa26ce07f5622b3dee610594392e0c
bindat <maximerigaud66@gmail.com>
2026-04-12 21:54:42 +0200

    Ajout de commentaires explicatifs dans ex1115.c et ex1116.c, et création du fichier ex1119.c avec les inclusions nécessaires.

--- Pour les nuls C/Livre2C/ex1115.c
+++ Pour les nuls C/Livre2C/ex1115.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-#include <math.h>
+#include <stdio.h> //Pour les entrées/sorties standard
+#include <math.h> //Pour les fonctions mathématiques, notamment sin()
 
-#define PI 3.14159
-#define LONGUEURONDE 70
-#define PERIODE .1
+#define PI 3.14159 //Définition de la constante PI
+#define LONGUEURONDE 70 //Définition de la longueur d'onde pour l'affichage du graphique
+#define PERIODE .1 //Définition de la période pour l'incrémentation de graph
 
 int main()
 {
-    float graph,s,x;
+    float graph,s,x; //Déclaration des variables : graph pour l'angle, s pour la valeur du sinus, x pour le compteur de caractères
 
-    for (graph = 0; graph<PI; graph+=PERIODE)
+    for (graph = 0; graph<PI; graph+=PERIODE) //Boucle pour faire varier graph de 0 à PI avec un pas de PERIODE
     {
-        s = sin(graph);
-        for (x = 0; x < s*LONGUEURONDE; x++)
-            putchar('*');
-        putchar('\n');
+        s = sin(graph); //Calcul de la valeur du sinus de graph et stockage dans s
+        for (x = 0; x < s*LONGUEURONDE; x++) //Boucle pour afficher des '*' en fonction de la valeur de s multipliée par LONGUEURONDE
+            putchar('*'); //Affichage d'un '*' pour chaque itération de la boucle
+        putchar('\n'); //Affichage d'un saut de ligne après chaque série de '*' pour passer à la ligne suivante du graphique
     }
-    return(0);
+    return(0); //Retourne 0 pour indiquer que le programme s'est terminé avec succès
 }
--- Pour les nuls C/Livre2C/ex1116.c
+++ Pour les nuls C/Livre2C/ex1116.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-#include <math.h>
+#include <stdio.h> //Pour les entrées/sorties standard
+#include <math.h> //Pour les fonctions mathématiques comme sin()
 
-#define PI 1
-#define LONGUEURONDE 70
-#define PERIODE .1
+#define PI 1 //Valeur de pi (approximative)
+#define LONGUEURONDE 70 //Longueur maximale de l'onde à afficher
+#define PERIODE .1 //Période de l'onde (plus petit = plus de points)
 
-int main()
+int main() //Point d'entrée du programme
 {
-    float graph,s,x;
+    float graph,s,x; //Variables pour le graphique, la valeur de sin et la position x
 
-    for (graph = 0; graph<PI; graph+=PERIODE)
+    for (graph = 0; graph<PI; graph+=PERIODE) //Boucle pour parcourir les valeurs de graph de 0 à pi avec un pas de PERIODE
     {
-        s = sin(graph);
-        for (x = 0; x < s*LONGUEURONDE; x++)
-            putchar('*');
-        putchar('\n');
+        s = sin(graph); //Calcul de la valeur de sin pour la position actuelle de graph
+        for (x = 0; x < s*LONGUEURONDE; x++) //Boucle pour afficher les étoiles en fonction de la valeur de sin
+            putchar('*'); //Affiche une étoile pour chaque unité de longueur correspondant à la valeur de sin
+        putchar('\n'); //Affiche une nouvelle ligne après chaque valeur de graph pour séparer les différentes lignes du graphique
     }
-    return(0);
+    return(0); //Retourne 0 pour indiquer que le programme s'est terminé avec succès
 }
