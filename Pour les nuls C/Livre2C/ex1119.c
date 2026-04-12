d23a53bfb4085215557c81206f5d29b5842bd884
bindat <maximerigaud66@gmail.com>
2026-04-12 22:03:29 +0200

    Ajout du fichier ex1119.c avec un programme générant des nombres aléatoires à partir d'une graine fournie par l'utilisateur.

--- Pour les nuls C/Livre2C/ex1119.c
+++ Pour les nuls C/Livre2C/ex1119.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main()
+{
+    unsigned graine;
+    int r, a, b;
+
+    printf("Indiquez une valeur int pou la graine : ");
+    scanf("%u", &graine);
+    srand(graine);
+    for (a=0; a<20; a++)
+    {
+        for (b=0; b<5; b++)
+        {
+            r=rand();
+            printf("%d\t", r);
+        }
+        putchar('\n');
+    }
+    return(0);
+}
