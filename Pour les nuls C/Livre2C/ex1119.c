bd040fc16f2ae8a6f6df58e47a8a0ce37fca7a3a
bindat <maximerigaud66@gmail.com>
2026-04-13 21:39:06 +0200

    Ajout de commentaires explicatifs dans ex1119.c pour clarifier le code.

--- Pour les nuls C/Livre2C/ex1119.c
+++ Pour les nuls C/Livre2C/ex1119.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> // pour printf et scanf
+#include <stdlib.h> // pour rand et srand
 
 int main()
 {
-    unsigned graine;
-    int r, a, b;
+    unsigned graine; // la graine doit être un entier non signé
+    int r, a, b; // r pour stocker le nombre aléatoire, a et b pour les boucles
 
     printf("Indiquez une valeur int pou la graine : ");
-    scanf("%u", &graine);
-    srand(graine);
-    for (a=0; a<20; a++)
+    scanf("%u", &graine); // lire la graine de l'utilisateur
+    srand(graine); // initialiser le générateur de nombres aléatoires avec la graine
+    for (a=0; a<20; a++) // boucle pour générer 20 lignes
     {
-        for (b=0; b<5; b++)
+        for (b=0; b<5; b++) // boucle pour générer 5 nombres par ligne
         {
-            r=rand();
-            printf("%d\t", r);
+            r=rand(); // générer un nombre aléatoire
+            printf("%d\t", r); // afficher le nombre suivi d'une tabulation pour l'alignement
         }
-        putchar('\n');
+        putchar('\n'); // après chaque ligne de 5 nombres, passer à la ligne suivante
     }
-    return(0);
+    return(0); // terminer le programme
 }
