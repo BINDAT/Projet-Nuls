afd81bee329b8fee5387f9fd55c7ab4eb136dfe3
bindat <maximerigaud66@gmail.com>
2026-04-14 13:02:17 +0200

    Ajout du fichier pt1118.c avec un programme générant 100 nombres aléatoires entre 1 et 20.

---
+++ Pour les nuls C/Livre2C/annexe code c/pt1118.c
@@ -0,0 +1,19 @@
+#include <stdio.h> //Pour printf et puts
+#include <stdlib.h> //Pour rand et srand
+
+int main()
+{
+    int r, a, b; //r pour le nombre aléatoire, a et b pour les boucles
+
+        puts("100 chiffres au hasard");
+        for (a=0; a<20; a++) //20 lignes
+        {
+            for (b=0; b<5; b++) //5 chiffres par ligne
+            {
+                r=rand() %20 +1; //r prend une valeur entre 1 et 20
+                printf("%d\t", r); //Affiche r suivi d'une tabulation pour espacer les chiffres
+            }
+            putchar('\n'); //Affiche un saut de ligne après chaque ligne de 5 chiffres
+        }
+        return(0);
+}
