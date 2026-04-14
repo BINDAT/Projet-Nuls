2f8ffb3baf9a25c170d834f21dc24045daf43ee4
bindat <maximerigaud66@gmail.com>
2026-04-14 12:58:43 +0200

    Ajout de commentaires explicatifs dans ex1117.c et ex1118.c pour clarifier le code.

--- Pour les nuls C/Livre2C/ex1117.c
+++ Pour les nuls C/Livre2C/ex1117.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> //Pour printf et puts
+#include <stdlib.h> //Pour rand et srand
 
 int main()
 {
-    int r, a, b;
+    int r, a, b; //r pour le nombre aléatoire, a et b pour les boucles
 
         puts("100 chiffres au hasard");
-        for (a=0; a<20; a++)
+        for (a=0; a<20; a++) //20 lignes
         {
-            for (b=0; b<5; b++)
+            for (b=0; b<5; b++) //5 chiffres par ligne
             {
-                r=rand();
-                printf("%d\t", r);
+                r=rand(); //r prend une valeur aléatoire
+                printf("%d\t", r); //Affiche r suivi d'une tabulation pour espacer les chiffres
             }
-            putchar('\n');
+            putchar('\n'); //Affiche un saut de ligne après chaque ligne de 5 chiffres
         }
         return(0);
 }
