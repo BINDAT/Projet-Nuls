7be882a03782dd1df60d03d395f34ff280144750
bindat <maximerigaud66@gmail.com>
2026-04-15 21:53:36 +0200

    Ajout de commentaires explicatifs dans ex1120.c pour clarifier le code.

--- Pour les nuls C/Livre2C/ex1120.c
+++ Pour les nuls C/Livre2C/ex1120.c
@@ -4,17 +4,17 @@
 
 int main()
 {
-    int r, a, b;
+    int r, a, b; // r pour le nombre aléatoire, a et b pour les boucles
 
-    srand((unsigned)time(NULL));
-    for (a=0; a<20; a++)
+    srand((unsigned)time(NULL)); // initialisation de la graine pour les nombres aléatoires
+    for (a=0; a<20; a++) // boucle pour générer 20 lignes
     {
-        for (b=0; b<5; b++)
+        for (b=0; b<5; b++) // boucle pour générer 5 nombres par ligne
         {
-            r=rand();
-            printf("%d\t", r);
+            r=rand(); // génération d'un nombre aléatoire
+            printf("%d\t", r); // affichage du nombre aléatoire suivi d'une tabulation pour l'alignement
         }
-        putchar('\n');
+        putchar('\n'); // saut de ligne après chaque ligne de 5 nombres aléatoires
     }
-    return(0);
+    return(0); // fin du programme
 }
