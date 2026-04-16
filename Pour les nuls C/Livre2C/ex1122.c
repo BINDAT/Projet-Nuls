8b33c9abf427d8971ab79c86f8b364b0fdf58b99
bindat <maximerigaud66@gmail.com>
2026-04-16 22:51:08 +0200

    Ajout des fichiers ex1122.c et ex1123.c avec des programmes affichant des calculs simples.

---
+++ Pour les nuls C/Livre2C/ex1122.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Inclusion de la bibliothèque standard d'entrée/sortie
+
+int main()
+{
+    printf("%d\n", 20-5*2+42/6);
+    return(0);
+}
---
+++ Pour les nuls C/Livre2C/ex1123.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Inclusion de la bibliothèque standard d'entrée/sortie
+
+int main()
+{
+    printf("%d\n", 12/3/2);
+    return(0);
+}
