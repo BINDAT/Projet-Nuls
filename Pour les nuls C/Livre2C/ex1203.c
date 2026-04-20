23837e1a7c4ba4c0a01c6c8699ac7d6b4f74f737
bindat <maximerigaud66@gmail.com>
2026-04-20 21:16:00 +0200

    Refactor ex1203.c pour lire et afficher les scores individuellement, ajout d'ex1204.c avec une implémentation similaire.

--- Pour les nuls C/Livre2C/ex1203.c
+++ Pour les nuls C/Livre2C/ex1203.c
@@ -1,19 +1,23 @@
 #include <stdio.h> // Entête pour les fonctions d'entrée/sortie
 
-int main()
+int main() // Fonction principale du programme
 {
     int scoremax[4]; // Déclaration d'un tableau de 4 entiers pour stocker les scores
-    int x; // Déclaration d'une variable entière pour les boucles
 
-    for (x=0; x<4; x++) // Boucle pour lire les scores
-    {
-        printf("Votre score #%d : ", x+1); // Affichage d'un message pour demander le score
-        scanf("%d", &scoremax[x]); // Lecture du score et stockage dans le tableau
-    }
-    
+    printf("Votre meilleur score: ");
+    scanf("%d",&scoremax[0]); // Lecture du premier score et stockage dans la variable scoremax[0]
+    printf("Second meilleur score: ");
+    scanf("%d",&scoremax[1]); // Lecture du second score et stockage dans la variable scoremax[1]
+    printf("Troisième meilleur score: ");
+    scanf("%d",&scoremax[2]); // Lecture du troisième score et stockage dans la variable scoremax[2]
+    printf("Quatrième meilleur score: ");
+    scanf("%d",&scoremax[3]); // Lecture du quatrième score et stockage dans la variable scoremax[3]
+
     puts("Voici vos meilleurs scores"); // Affichage d'un message pour introduire les scores
-    for (x=0; x<4; x++) // Boucle pour afficher les scores
-        printf("#%d %d\n", x+1, scoremax[x]); // Affichage du score avec un format spécifique
-    
-    return 0; // Retourne 0 pour indiquer que le programme s'est terminé avec succès
+    printf("#1 %d\n",scoremax[0]); // Affichage du premier score avec un format spécifique
+    printf("#2 %d\n",scoremax[1]); // Affichage du second score avec un format spécifique
+    printf("#3 %d\n",scoremax[2]); // Affichage du troisième score avec un format spécifique
+    printf("#4 %d\n",scoremax[3]); // Affichage du quatrième score avec un format spécifique
+
+    return(0); // Retourne 0 pour indiquer que le programme s'est terminé avec succès
 }
