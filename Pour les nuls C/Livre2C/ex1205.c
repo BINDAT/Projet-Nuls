d80f50b78ef55d843553f0534b9cd4783d9fbe4e
bindat <maximerigaud66@gmail.com>
2026-04-21 21:18:19 +0200

    Ajout du fichier ex1205.c avec un programme pour saisir et afficher les meilleurs scores.

---
+++ Pour les nuls C/Livre2C/ex1205.c
@@ -0,0 +1,19 @@
+#include <stdio.h> // Entête pour les fonctions d'entrée/sortie
+
+int main()
+{
+    int scoremax[10]; // Déclaration d'un tableau de 10 entiers pour stocker les scores
+    int x; // Déclaration d'une variable entière pour les boucles
+
+    for (x=0; x<10; x++) // Boucle pour lire les scores
+    {
+        printf("Votre score #%d : ", x+1); // Affichage d'un message pour demander le score
+        scanf("%d", &scoremax[x]); // Lecture du score et stockage dans le tableau
+    }
+    
+    puts("Voici vos meilleurs scores"); // Affichage d'un message pour introduire les scores
+    for (x=0; x<10; x++) // Boucle pour afficher les scores
+        printf("#%d %d\n", x+1, scoremax[x]); // Affichage du score avec un format spécifique
+    
+    return 0; // Retourne 0 pour indiquer que le programme s'est terminé avec succès
+}
