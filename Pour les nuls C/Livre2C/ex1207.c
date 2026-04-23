dd96368feb12d9d37038c7510049863667a37167
bindat <maximerigaud66@gmail.com>
2026-04-23 14:00:24 +0200

    Amélioration des commentaires dans ex1207.c et ex1209.c pour clarifier le code

--- Pour les nuls C/Livre2C/ex1207.c
+++ Pour les nuls C/Livre2C/ex1207.c
@@ -6,13 +6,13 @@ int main()
     int copie; // Déclaration d'une variable entière pour les boucles
     int taBourseFin[] = {};
 
-    for (copie = 0; copie<=tabBourseDebut[copie]; copie++)
+    for (copie = 0; copie<=tabBourseDebut[copie]; copie++) // Boucle pour copier les valeurs du tableau de début dans le tableau de fin
     {
-        tabBourseDebut[copie] = taBourseFin[copie];
+        tabBourseDebut[copie] = taBourseFin[copie]; // Copie de la valeur du tableau de début dans le tableau de fin
     }
-    for (int affichage = 0; affichage <= 5; affichage++)
+    for (int affichage = 0; affichage <= 5; affichage++) // Boucle pour afficher les valeurs du tableau de fin
     {
-        printf("%d: %d\n", affichage +1, taBourseFin[affichage]);
+        printf("%d: %d\n", affichage +1, taBourseFin[affichage]); // Affiche l'index +1 et la valeur correspondante du tableau de fin
     }
     return 0; // Retourne 0 pour indiquer que le programme s'est terminé avec succès
 }
--- Pour les nuls C/Livre2C/ex1209.c
+++ Pour les nuls C/Livre2C/ex1209.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    char prenom[16];
+    char prenom[16]; // On déclare une variable de type char qui peut contenir jusqu'à 15 caractères (plus le caractère de fin de chaîne '\0')
 
-    printf("Quel est votre pénom ? ");
-    fgets(prenom, 16, stdin);
-    printf("Ravi de vous rencontrez, %s\n", prenom);
+    printf("Quel est votre pénom ? "); // On affiche un message demandant à l'utilisateur de saisir son prénom
+    fgets(prenom, 16, stdin); // On lit le prénom de l'utilisateur et on le stocke dans la variable prenom
+    printf("Ravi de vous rencontrez, %s\n", prenom); // On affiche un message de bienvenue en utilisant le prénom de l'utilisateur
     return(0);
 }
