2720888da159c7ac99e3782ca48b54dd6445fea3
bindat <maximerigaud66@gmail.com>
2026-04-23 13:36:29 +0200

    Amélioration des commentaires dans ex1207.c pour clarifier le code

--- Pour les nuls C/Livre2C/ex1207.c
+++ Pour les nuls C/Livre2C/ex1207.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include <stdio.h> // Affiche une phrase caractère par caractère
 
-int main()
+int main() // Affiche une phrase caractère par caractère
 {
-    char phrase[] = "Texte insignifiant";
-    int index;
+    char phrase[] = "Texte insignifiant"; // Phrase à afficher
+    int index; // Index de caractère
 
-    index = 0;
-    while (phrase[index] != '\0')
+    index = 0; // Initialisation de l'index
+    while (phrase[index] != '\0') // Tant que le caractère n'est pas le caractère de fin de chaîne
     {
-        putchar(phrase[index]);
-        index++;
+        putchar(phrase[index]); // Affiche le caractère à l'index actuel
+        index++; // Incrémente l'index pour passer au caractère suivant
     }
-    putchar('\n');
-    return(0);
+    putchar('\n'); // Affiche une nouvelle ligne après avoir affiché la phrase
+    return(0); // Indique que le programme s'est terminé avec succès
 }
