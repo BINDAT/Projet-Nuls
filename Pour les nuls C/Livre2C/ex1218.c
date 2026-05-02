4f33041a8dbfe0b053af873a3ee28ce7096b767f
bindat <maximerigaud66@gmail.com>
2026-05-02 21:26:32 +0200

    Amélioration des commentaires dans le fichier ex1218.c pour clarifier le code d'affichage des présidents.

--- Pour les nuls C/Livre2C/ex1218.c
+++ Pour les nuls C/Livre2C/ex1218.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> // pour printf et putchar
 
-#define TAILLE 3
+#define TAILLE 3 // nombre de présidents
 
 int main()
 {
@@ -8,18 +8,18 @@ int main()
         "Sarkozy",
         "Coty",
         "Grevy"
-    };
-    int x,index;
+    }; // tableau de chaînes de caractères
+    int x,index; // variables d'indexation
 
     for (x=0; x<TAILLE; x++)
     {
-        index = 0;
-        while (president[x][index] != '\0')
+        index = 0; // réinitialisation de l'index pour chaque président
+        while (president[x][index] != '\0') // tant que le caractère courant n'est pas le caractère de fin de chaîne
         {
-            putchar(president[x][index]);
-            index++;
+            putchar(president[x][index]); // affichage du caractère courant
+            index++; // passage au caractère suivant
         }
-        putchar('\n');
+        putchar('\n'); // saut de ligne après chaque nom
     }
     return(0);
 }
