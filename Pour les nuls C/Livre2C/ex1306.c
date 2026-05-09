8b3d8d6776681659d6c8fc4f1df52c6efeba1a45
bindat <maximerigaud66@gmail.com>
2026-05-09 21:16:06 +0200

    Correction de la logique de conversion des lettres dans le fichier ex1306.c pour assurer un traitement correct des majuscules et minuscules.

--- Pour les nuls C/Livre2C/ex1306.c
+++ Pour les nuls C/Livre2C/ex1306.c
@@ -4,24 +4,31 @@
 
 int main()
 {
-    char texte[100], minuscule[100], majuscule[100];
+    char texte[100], lettre[100];
     int pointeur;
+
     printf("Saisissez du texte : ");
     fgets(texte,sizeof(texte),stdin);
     
     for(pointeur = 0; texte[pointeur] !='\0'; pointeur++)
     {
-        if(isupper(texte[pointeur])!='\0')
+        if(isupper(texte[pointeur]))
         {
-            minuscule[pointeur] = tolower(texte[pointeur]);
+            lettre[pointeur] = tolower(texte[pointeur]);
         }
-        else if (islower(texte[pointeur]))
+        else if(islower(texte[pointeur]))
         {
-            majuscule[pointeur] = toupper(texte[pointeur]);
+            lettre[pointeur] = toupper(texte[pointeur]);
         }
+        else
+        {
+            lettre[pointeur] = texte[pointeur];
+        }
+        
     }
-    
-    printf("Voici votre saisit : %c \n", texte[pointeur]);
+    lettre[pointeur] ='\0';
+
+    printf("Voici votre saisit : %s\n", lettre);
 
     return(0);
 }
