8f42564da277c60d1d7f29e02d0e91bd0114a635
bindat <maximerigaud66@gmail.com>
2026-05-10 21:02:28 +0200

    Amélioration des commentaires dans les fichiers ex1304.c et ex1305.c pour clarifier le code et faciliter la compréhension.

--- Pour les nuls C/Livre2C/ex1304.c
+++ Pour les nuls C/Livre2C/ex1304.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <ctype.h>
+#include <stdio.h> // pour printf et scanf
+#include <ctype.h> // pour toupper
 
 int main()
 {
-    char reponse;
+    char reponse; // pour stocker la réponse de l'utilisateur
     printf("Voulez-vous faire exploser la lune ?");
-    scanf("%c",&reponse);
-    reponse = toupper(reponse);
-    if (reponse == 'O')
+    scanf("%c",&reponse); // lire la réponse de l'utilisateur
+    reponse = toupper(reponse); // convertir la réponse en majuscule
+    if (reponse == 'O') // si la réponse est 'O' (oui)
         puts("BOUM!");
     else
         puts("La lune ne craint rien.");
--- Pour les nuls C/Livre2C/ex1305.c
+++ Pour les nuls C/Livre2C/ex1305.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-#include <ctype.h>
+#include <stdio.h> // pour printf et scanf
+#include <ctype.h> // pour toupper
 
 int main()
 {
-    char reponse;
+    char reponse; // pour stocker la réponse de l'utilisateur
     printf("Voulez-vous faire exploser la lune ?");
-    scanf("%c",&reponse);
-    reponse = toupper(reponse);
-    if (reponse == 'O')
+    scanf("%c",&reponse); // lire la réponse de l'utilisateur
+    reponse = toupper(reponse); // convertir la réponse en majuscule
+    if (reponse == 'O') // si la réponse est 'O' (oui)
         puts("BOUM!");
-    else if (reponse == 'N')
+    else if (reponse == 'N') // si la réponse est 'N' (non)
         puts("La lune ne craint rien.");
-    else
+    else // si la réponse n'est ni 'O' ni 'N'
         puts("Votre réponse n'est ni oui ni non");
     return(0);
 }
