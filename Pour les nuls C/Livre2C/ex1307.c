7015fc374d25147196a7926b455a5789c1a358dc
bindat <maximerigaud66@gmail.com>
2026-05-12 21:06:44 +0200

    Ajout du fichier ex1307.c pour implémenter un système de vérification de mot de passe.

---
+++ Pour les nuls C/Livre2C/ex1307.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char motpasse[] = "taco";
+    char tabsaisie[15];
+    int match;
+
+    printf("Le mot de passe ?");
+    scanf("%s", tabsaisie);
+
+    match = strcmp(tabsaisie,motpasse);
+    if (match == 0)
+        puts("Mot de passe correct");
+    else
+        puts("Mauvais mot de passe. Alertez la DGSE.");
+    
+    return(0);
+}
