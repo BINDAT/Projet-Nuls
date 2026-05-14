3e45721ef225e25005bd54b852d78f4ad1e93b54
bindat <maximerigaud66@gmail.com>
2026-05-14 21:21:15 +0200

    Ajout du fichier ex1309.c pour implémenter un système de vérification de mot de passe insensible à la casse.

---
+++ Pour les nuls C/Livre2C/ex1309.c
@@ -0,0 +1,18 @@
+#include <stdio.h> // strcmp() est dans string.h
+#include <string.h> // strcmp() compare deux chaînes de caractères et retourne 0 si elles sont identiques
+
+int main()
+{
+    char motpasse[] = "taco"; // mot de passe à trouver
+    char tabsaisie[15]; // tableau de caractères pour stocker la saisie de l'utilisateur
+
+    printf("Le mot de passe ?");
+    scanf("%s", tabsaisie); // lit une chaîne de caractères depuis l'entrée standard et la stocke dans tabsaisie
+
+    if (strcasecmp(tabsaisie,motpasse) == 0) // si match est égal à 0, cela signifie que les deux chaînes de caractères sont identiques, en ignorant la casse 
+        puts("Mot de passe correct");
+    else
+        puts("Mauvais mot de passe. Alertez la DGSE.");
+    
+    return(0);
+}
