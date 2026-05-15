a29507624e1a1fe0e46b4cf78cd286f010e00f07
bindat <maximerigaud66@gmail.com>
2026-05-15 21:24:20 +0200

    Ajout du fichier ex1310.c pour implémenter un programme de concaténation de prénom et nom de famille.

---
+++ Pour les nuls C/Livre2C/ex1310.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char primo[40];
+    char secundo[20];
+
+    printf("Indiquez votre prénom : ");
+    scanf("%s", primo);
+    printf("Et votre nom de famille : ");
+    scanf("%s", secundo);
+    strcat(primo, secundo);
+    printf("Ravi de vous voir, %s!\n", primo);
+    return(0);
+}
