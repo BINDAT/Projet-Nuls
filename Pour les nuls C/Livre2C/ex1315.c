fe2d3a2b383652a77cfeb4434a6e014f8f6a8c58
BINDAT <136336341+BINDAT@users.noreply.github.com>
2026-05-18 15:00:10 +0200

    Create ex1315.c

---
+++ Pour les nuls C/Livre2C/ex1315.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("%-9smoi\n", "Parle -");
+  printf("%-8smoi\n", "Parle -");
+  printf("%-7smoi\n", "Parle -");
+  printf("%-6smoi\n", "Parle -");
+  printf("%-5smoi\n", "Parle -");
+  printf("%-4smoi\n", "Parle -");
+}
