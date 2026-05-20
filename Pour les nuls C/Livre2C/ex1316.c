9fef466a47dbb78391faa187a7bcf2e031a31924
bindat <maximerigaud66@gmail.com>
2026-05-20 21:00:49 +0200

    Ajout de commentaires explicatifs dans le fichier ex1316.c pour clarifier le fonctionnement du programme affichant les présidents américains.

--- Pour les nuls C/Livre2C/ex1316.c
+++ Pour les nuls C/Livre2C/ex1316.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> //pour printf et scanf
 
-#define taille 4
+#define taille 4 //taille du tableau
 
 int main()
 {
@@ -10,10 +10,10 @@ int main()
         {"John", "Adams"}, 
         {"Thomas", "Jefferson"},
         {"James", "Monroe"}
-    };
+    }; //tableau de 4 lignes et 2 colonnes
     for (int boucle = 0; boucle < taille; boucle++)
     {
-        printf("%-10s %s\n",President[boucle][0],President[boucle][1]);
+        printf("%-10s %s\n",President[boucle][0],President[boucle][1]); //affiche les prénoms et noms des présidents
     }
-    return(0);
+    return(0); //fin du programme
 }
