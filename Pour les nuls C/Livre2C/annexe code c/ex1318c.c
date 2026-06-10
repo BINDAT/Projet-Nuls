3340e0264dac5f8d0eaa536168b9c845be9c5a65
bindat <maximerigaud66@gmail.com>
2026-06-10 22:02:14 +0200

    Ajout des fichiers ex1317.c et ex1318.c pour implémenter des programmes de lecture et d'affichage des initiales.

---
+++ Pour les nuls C/Livre2C/annexe code c/ex1318c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char primo, secundo;
+
+    printf("Initiale de votre prénom : ");
+    primo = getchar();
+    printf("Initiale de votre nom de famille : ");
+    secundo = getchar();
+    printf("Vos initiales sont '%c et '%c\n",primo,secundo);
+    return(0);
+}
