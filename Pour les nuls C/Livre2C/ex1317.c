31eab38526b488b5445728437ec556e1d14df7ef
bindat <maximerigaud66@gmail.com>
2026-06-11 18:02:20 +0200

    Ajout de commentaires explicatifs dans les fichiers ex1317.c et ex1318.c pour clarifier le fonctionnement des programmes de lecture et d'affichage des caractères.

--- Pour les nuls C/Livre2C/ex1317.c
+++ Pour les nuls C/Livre2C/ex1317.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
+#include <stdio.h> // pour getchar() et putchar()
 
 int main()
 {
-    char i;
+    char i; // i est de type char pour stocker les caractères saisis
 
     do
     {
-        i = getchar();
-        putchar(i);
-    } while (i != '.');
+        i = getchar(); // lire un caractère depuis l'entrée standard (clavier)
+        putchar(i); // afficher le caractère lu à l'écran
+    } while (i != '.'); // continuer à lire et afficher les caractères jusqu'à ce que le caractère '.' soit saisi
     
-    putchar('\n');
+    putchar('\n'); // ajouter une nouvelle ligne après la boucle pour une meilleure lisibilité
     return(0);
 }
--- Pour les nuls C/Livre2C/ex1318.c
+++ Pour les nuls C/Livre2C/ex1318.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> // pour printf et getchar
 
 int main()
 {
-    char primo, secundo;
+    char primo, secundo; // primo et secundo sont des variables de type char
 
     printf("Initiale de votre prénom : ");
-    primo = getchar();
+    primo = getchar(); // on lit un caractère et on le stocke dans la variable primo
     printf("Initiale de votre nom de famille : ");
-    second = getchar();
-    printf("Vos initiales sont '%c et '%c\n",primo,secundo);
+    second = getchar(); // on lit un caractère et on le stocke dans la variable secundo
+    printf("Vos initiales sont '%c et '%c\n",primo,secundo); // on affiche les initiales en utilisant le format %c pour les caractères
     return(0);
 }
