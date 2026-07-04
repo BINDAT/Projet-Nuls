1e5bdd8c948eaf13b5fdee85c59f4959dd743282
bindat <maximerigaud66@gmail.com>
2026-07-04 20:40:14 +0200

    Ajout de commentaires explicatifs pour clarifier la structure et les variables dans le fichier ex1401.c

--- Pour les nuls C/Livre2C/ex1401.c
+++ Pour les nuls C/Livre2C/ex1401.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
+#include <stdio.h> //Pour printf et scanf pour l'affichage et la saisie
 
 int main()
 {
-    struct joueur
+    struct joueur // déclaration d'une structure joueur
     {
-        char nomj[32];
-        int scoremax;
+        char nomj[32]; // déclaration d'un tableau de caractères pour le nom du joueur
+        int scoremax; // déclaration d'un entier pour le meilleur score du joueur
     };
-    struct joueur xbox;
+    struct joueur xbox; // déclaration d'une variable de type struct joueur
 
     printf("Nom du joueur : ");
-    scanf("%s", xbox.nomj);
+    scanf("%s", xbox.nomj); // saisie du nom du joueur grâce à la structure
     printf("Meilleur score : ");
-    scanf("%d", &xbox.scoremax);
+    scanf("%d", &xbox.scoremax); // saisie du meilleur score du joueur grâce à la structure
 
-    printf("Meilleur score de %s : %d\n", xbox.nomj, xbox.scoremax);
+    printf("Meilleur score de %s : %d\n", xbox.nomj, xbox.scoremax); // affichage du meilleur score du joueur grâce à la structure
     return(0);
 }
