d0016fb9defe85b7484454dfce709cbbb8c52a41
bindat <maximerigaud66@gmail.com>
2026-07-06 20:58:52 +0200

    Ajout de commentaires explicatifs pour clarifier la structure et les variables dans le fichier ex1403.c

--- Pour les nuls C/Livre2C/ex1403.c
+++ Pour les nuls C/Livre2C/ex1403.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
+#include <stdio.h> // pour printf
 
 int main()
 {
-    struct president
+    struct president // déclaration de la structure
     {
-        char nomPres[40];
-        int anneeNomi;
+        char nomPres[40]; // nom du président
+        int anneeNomi; // année de nomination
     };
     struct president pres1 = {
         "George Washington",
-        1789
+        1789 // initialisation de la structure
     };
 
-    printf("Le premier président est %s\n", pres1.nomPres);
-    printf("Il a pris ses fonctions en %d\n", pres1.anneeNomi);
+    printf("Le premier président est %s\n", pres1.nomPres); // affichage du nom du président
+    printf("Il a pris ses fonctions en %d\n", pres1.anneeNomi); // affichage de l'année de nomination
 
     return(0);
 }
