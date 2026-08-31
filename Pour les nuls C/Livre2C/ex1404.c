178d8805c127105c28c98329efc75e4cdd1d9ab3
bindat <maximerigaud66@gmail.com>
2026-08-31 21:08:36 +0200

    Ajout du fichier ex1404.c pour implémenter un programme affichant le nom et l'année de nomination du premier président des États-Unis.

---
+++ Pour les nuls C/Livre2C/ex1404.c
@@ -0,0 +1,18 @@
+#include <stdio.h> // pour printf
+
+int main()
+{
+    struct president // déclaration de la structure
+    {
+        char nomPres[40]; // nom du président
+        int anneeNomi; // année de nomination
+    } pres1 = {
+        "George Washington",
+        1789 // initialisation de la structure
+    };
+
+    printf("Le premier président est %s\n", pres1.nomPres); // affichage du nom du président
+    printf("Il a pris ses fonctions en %d\n", pres1.anneeNomi); // affichage de l'année de nomination
+
+    return(0);
+}
